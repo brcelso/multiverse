659eab9d45b639556ba9bde4625acb610cd636d8
celso <celso.sjunior@outlook.com>
2025-06-21 16:37:12 -0300

    Create coffee.ts

---
+++ 2k38/cycle/src/data/products/coffee.ts
@@ -0,0 +1,81 @@
+export const coffee = {
+  launchDate: '00-00-00',
+
+  Brazil: {
+    'June 2025': [
+      {
+        edition: 'Coffee 500g',
+        price: 34.99,
+        discount: 25,
+        currency: 'R$'
+      },
+    {
+        edition: 'Coffee Index',
+        price: 311,
+        discount: 28.5,
+        currency: '$'
+      },
+    ],
+    'May 2025': [
+      {
+        edition: 'Coffee 500g',
+        price: 47.99,
+        increase: 500,
+        currency: 'R$'
+      },
+      {
+        edition: 'Coffee 1kg',
+        price: 125.90,
+        increase: 0,
+        currency: 'R$'
+      },
+      {
+        edition: 'Coffee Index',
+        price: 398.37,
+        discount: 8.5,
+        currency: '$'
+      },
+    ],
+    'February 2025': [
+      {
+        edition: 'Coffee 500g',
+        price: 34.99,
+        increase: 300,
+        currency: 'R$'
+      },
+      {
+        edition: 'Coffee Index',
+        price: 435.92,
+        increase: 400,
+        currency: '$'
+      },
+    ],
+    'January 2022': [
+      {
+        edition: 'Coffee 500g',
+        price: 22.25,
+        increase: 170,
+        currency: 'R$'
+      },
+      {
+        edition: 'Coffee Index',
+        price: 260.14,
+        increase: 200,
+        currency: '$'
+      },
+    ],
+    'April 2019': [
+      {
+        edition: 'Coffee 500g',
+        price: 8.48,
+        increase: 0,
+        currency: 'R$'
+      },
+      {
+        edition: 'Coffee Index',
+        price: 87.47,
+        currency: '$'
+      },
+    ],
+  },
+};
