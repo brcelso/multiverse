5ca28e4987486001bf6030560a843aa5cd2a6cd7
Celso <celso.sjunior@outlook.com>
2025-06-22 13:59:21 -0300

    added gta + ubi

--- 2k38/cycle/src/data/subs/GTA+.ts
+++ 2k38/cycle/src/data/subs/GTA+.ts
@@ -5,43 +5,43 @@ export const gtaplus = {
       { price: 42.90, increase: 35, currency: 'R$',
         basePrice: 5.99, exchangeTax2: 5.36,
         realPrice: 45.38, exchangeTax3: 5.68, 
-        edition: "GTA+ 1 Year" },
+        edition: "GTA+ Monthly" },
       ],
     'March 2022': [
       { price: 31.90, currency: 'R$',
         basePrice: 5.99, exchangeTax2: 5.32,
         realPrice: 28.39, exchangeTax3: 4.74, 
-        edition: "GTA+ 1 Year" },
+        edition: "GTA+ Monthly" },
       ],
       },
   Europe: {
     'April 2025': [
       { price: 7.99, increase: 33, currency: '€', 
-        edition: "GTA+ 1 Year" },
+        edition: "GTA+ Monthly" },
       ],
     'March 2022': [
       { price: 5.99, currency: '€',
-        edition: "GTA+ 1 Year" },
+        edition: "GTA+ Monthly" },
       ],
       },
   UnitedKingdom: {
     'April 2025': [
       { price: 6.99, increase: 40, currency: '£', 
-        edition: "GTA+ 1 Year" },
+        edition: "GTA+ Monthly" },
       ],
     'March 2022': [
       { price: 4.99, currency: '£',
-        edition: "GTA+ 1 Year" },
+        edition: "GTA+ Monthly" },
       ],
       },
   UnitedStates: {
     'April 2025': [
       { price: 7.99, increase: 33, currency: '$', 
-        edition: "GTA+ 1 Year" },
+        edition: "GTA+ Monthly" },
       ],
     'March 2022': [
       { price: 5.99, currency: '$',
-        edition: "GTA+ 1 Year" },
+        edition: "GTA+ Monthly" },
       ],
       },
     }
