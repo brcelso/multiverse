4761fdf85d802449a07066190677cf06958ac8cc
celso <celso.sjunior@outlook.com>
2025-06-26 00:30:17 -0300

    Update PSgiftcard.ts

--- 2k38/cycle/src/data/products/PSgiftcard.ts
+++ 2k38/cycle/src/data/products/PSgiftcard.ts
@@ -7,7 +7,8 @@ export const psgiftcard = {
         edition: 'PS Gift Card R$530',
         price: 424,
         discount: 20,
-        currency: 'R$', 
+        currency: 'R$',
+        basePrice2: 200,
         exchangeTax2: 2.12,
         realPrice: 1100,
         exchangeTax3: 5.50
@@ -15,20 +16,105 @@ export const psgiftcard = {
       {
         edition: 'PS Gift Card R$530',
         price: 530,
-        currency: 'R$', 
+        currency: 'R$',
         basePrice2: 250,
         exchangeTax2: 2.12,
         realPrice: 1375,
         exchangeTax3: 5.50
       },
+      {
+        edition: 'PS Gift Card R$400',
+        price: 400,
+        currency: 'R$',
+        basePrice2: 200,
+        exchangeTax2: 2.00,
+      },
+      {
+        edition: 'PS Gift Card R$300',
+        price: 300,
+        currency: 'R$',
+        basePrice2: 150,
+        exchangeTax2: 2.00,
+      },
+      {
+        edition: 'PS Gift Card R$250',
+        price: 250,
+        currency: 'R$',
+        basePrice2: 125,
+        exchangeTax2: 2.00,
+      },
+      {
+        edition: 'PS Gift Card R$150',
+        price: 150,
+        currency: 'R$',
+        basePrice2: 75,
+        exchangeTax2: 2.00,
+      },
+      {
+        edition: 'PS Gift Card R$100',
+        price: 100,
+        currency: 'R$',
+        basePrice2: 50,
+        exchangeTax2: 2.00,
+      },
+      {
+        edition: 'PS Gift Card R$60',
+        price: 60,
+        currency: 'R$',
+        basePrice2: 25,
+        exchangeTax2: 2.40,
+      },
+      {
+        edition: 'PS Gift Card R$35',
+        price: 35,
+        currency: 'R$',
+        basePrice2: 10,
+        exchangeTax2: 3.50,
+      },
     ],
   },
+
   UnitedStates: {
     'June 2025': [
       {
         edition: 'PS Gift Card $250',
         price: 250,
-        currency: '$', 
+        currency: '$',
+      },
+      {
+        edition: 'PS Gift Card $200',
+        price: 200,
+        currency: '$',
+      },
+      {
+        edition: 'PS Gift Card $150',
+        price: 150,
+        currency: '$',
+      },
+      {
+        edition: 'PS Gift Card $100',
+        price: 100,
+        currency: '$',
+      },
+      {
+        edition: 'PS Gift Card $75',
+        price: 75,
+        currency: '$',
+      },
+      {
+        edition: 'PS Gift Card $50',
+        price: 50,
+        currency: '$',
+      },
+      {
+        edition: 'PS Gift Card $25',
+        price: 25,
+        currency: '$',
+      },
+      {
+        edition: 'PS Gift Card $10',
+        price: 10,
+        currency: '$',
       },
     ],
   },
