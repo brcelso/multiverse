ff8152fda1133c72e05e713ae1914f786920b876
celso <celso.sjunior@outlook.com>
2025-06-26 00:18:14 -0300

    Update PSgiftcard.ts

--- 2k38/cycle/src/data/products/PSgiftcard.ts
+++ 2k38/cycle/src/data/products/PSgiftcard.ts
@@ -7,7 +7,28 @@ export const psgiftcard = {
         edition: 'PS Gift Card R$530',
         price: 424,
         discount: 20,
-        currency: 'R$'
+        currency: 'R$', 
+        exchangeTax2: 2.12
+        realPrice: 1100,
+        exchangeTax3: 5.50
+      },
+      {
+        edition: 'PS Gift Card R$530',
+        price: 530,
+        currency: 'R$', 
+        basePrice2: 250,
+        exchangeTax2: 2.12
+        realPrice: 1375,
+        exchangeTax3: 5.50
+      },
+    ],
+  },
+  UnitedStates: {
+    'June 2025': [
+      {
+        edition: 'PS Gift Card $250',
+        price: 250,
+        currency: '$', 
       },
     ],
   },
