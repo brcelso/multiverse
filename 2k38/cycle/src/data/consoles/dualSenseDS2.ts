6cf8f28f4da07fe5aa65931c1f8f67fe810be1cf
celso <celso.sjunior@outlook.com>
2025-06-26 10:24:05 -0300

    Create dualSenseDS2.ts

---
+++ 2k38/cycle/src/data/consoles/dualSenseDS2.ts
@@ -0,0 +1,23 @@
+export const dualsenseds2 = {
+      launchDate: '2025-06-25',
+
+  Brazil: {
+    'June 2025': [
+      {
+        edition: 'Dual Sense DS2',
+        price: 599,
+        currency: 'R$',
+      },
+      ],
+  },
+
+  UnitedStates: {
+    'April 2025': [
+      {
+        edition: 'Dual Sense DS2',
+        price: 84,
+        currency: '$',
+      },
+    ],
+  },
+};
