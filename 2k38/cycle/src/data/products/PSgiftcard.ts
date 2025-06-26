12a570ef891b432e087cf77e36d7e45ce9e5e816
celso <celso.sjunior@outlook.com>
2025-06-25 23:53:52 -0300

    Create PSgiftcard.ts

---
+++ 2k38/cycle/src/data/products/PSgiftcard.ts
@@ -0,0 +1,14 @@
+export const psgiftcard = {
+  launchDate: '2025-06-2025',
+
+  Brazil: {
+    'June 2025': [
+      {
+        edition: 'PS Gift Card R$530',
+        price: 424,
+        discount: 20,
+        currency: 'R$'
+      },
+    ],
+  },
+};
