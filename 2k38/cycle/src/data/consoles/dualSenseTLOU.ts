a05cef4a80eb312093234fdc5c0339d9ebf6e10c
celso <celso.sjunior@outlook.com>
2025-06-26 10:13:06 -0300

    Create dualSenseTLOU.ts

---
+++ 2k38/cycle/src/data/consoles/dualSenseTLOU.ts
@@ -0,0 +1,31 @@
+export const dualsensetlou = {
+      launchDate: '2025-04-28',
+
+  Brazil: {
+    'June 2025': [
+      {
+        edition: 'Dual Sense TLOU',
+        price: 1499,
+        currency: 'R$',
+        increase: 150,
+      },
+      ],
+    'April 2025': [
+      {
+        edition: 'Dual Sense TLOU',
+        price: 599,
+        currency: 'R$',
+      },
+    ],
+  },
+
+  UnitedStates: {
+    'April 2025': [
+      {
+        edition: 'Dual Sense TLOU',
+        price: 84,
+        currency: '$',
+      },
+    ],
+  },
+};
