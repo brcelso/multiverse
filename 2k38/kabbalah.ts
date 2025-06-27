1558a020bd47b6598668da58452dff1c6a884521
celso <celso.sjunior@outlook.com>
2025-06-27 20:25:24 -0300

    Create kabbalah.ts

---
+++ 2k38/kabbalah.ts
@@ -0,0 +1,34 @@
+27-05-2025
+
+status celso
+
+UP 1 2
+ON 3 5 10
+OFF 1 2 4 6 7 8 9
+
+status ana
+
+UP 8
+ON 2 3 7 9 10
+OFF 1 4 5 6 8
+
+26-06-2025
+
+identificado que para passar a proxima fase 
+precisa ligar todos disjuntores,
+porem nem todos ficam ligados, pode ter algum
+prejudicando ou interferindo no funcionamento
+geral para avanco da fase.
+----
+
+1 Ancestrais
+2 Avos Maternos
+3 Avos Paternos
+4 Mae
+5 Pai
+6 Jesus
+7 Feminino
+8 Masculino
+9 Corpo
+10 Existencia
+
