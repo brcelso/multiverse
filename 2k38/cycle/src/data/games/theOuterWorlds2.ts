7b75c95b2d9b724c81b405064f1b8d5516c4f4f4
celso <celso.sjunior@outlook.com>
2025-06-28 22:46:17 -0300

    Criar theOuterWorlds2.ts

---
+++ 2k38/cycle/src/data/games/theOuterWorlds2.ts
@@ -0,0 +1,30 @@
+export const theouterworlds2 = {
+  launchDate: '2025-10-29',
+
+  Brazil: {
+    'June 2025': [
+      { edition: 'Premium', price: 499.50, increase: 18, currency: 'R$', exchangeTax: 4.99 },
+      { edition: 'Standard', price: 455.90, increase: 14, currency: 'R$', exchangeTax: 5.69 },
+      { edition: 'Premium', price: 424.00, currency: 'R$', exchangeTax: 4.24 },
+      { edition: 'Standard', price: 399.90, currency: 'R$', exchangeTax: 4.99 }
+],
+  },
+  Europe: {
+    'June 2025': [
+      { edition: 'Premiun', price: 99.99, currency: '€' },
+      { edition: 'Standard', price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'June 2025': [
+      { edition: 'Premium', price: 89.99, currency: '£' },
+      { edition: 'Standard', price: 69.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'June 2025': [
+      { edition: 'Premium', price: 99.99, currency: '$' },
+      { edition: 'Standard', price: 79.99, currency: '$' },
+    ],
+  },
+};
