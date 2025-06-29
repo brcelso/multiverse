f59b66c351d2e6f95a7ec8b0277647abe4d42336
celso <celso.sjunior@outlook.com>
2025-06-28 22:56:12 -0300

    Criar MetalGearSnakeEaterRmake.ts

---
+++ 2k38/cycle/src/data/games/MetalGearSnakeEaterRmake.ts
@@ -0,0 +1,40 @@
+export const mgs3 = {
+  launchDate: '2025-08-28',
+
+  Japan: {
+    'June 2025': [
+      { edition: "Deluxe Edition", price: 9980, currency: '¥' },
+      { edition: "Standard Edition", price: 8980, currency: '¥' },
+    ],
+  },
+
+  Brazil: {
+    'June 2025': [
+      { edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax: 5.69 },
+      { edition: "Standard Edition", price: 399.50, currency: 'R$', exchangeTax: 5.70 },
+      { edition: "Deluxe Edition", price: 399.50, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Standard Edition", price: 349.9, currency: 'R$', exchangeTax: 4.99 },
+    ],
+  },
+
+  UnitedStates: {
+    'June 2025': [
+      { edition: "Deluxe Edition", price: 79.99, currency: '$' },
+      { edition: "Standard Edition", price: 69.99, currency: '$' },
+    ],
+  },
+
+  Europe: {
+    'June 2025': [
+      { edition: "Deluxe Edition", price: 89.99, currency: '€' },
+      { edition: "Standard Edition", price: 79.99, currency: '€' },
+    ],
+  },
+
+  UnitedKingdom: {
+    'June 2025': [
+      { edition: "Deluxe Edition", price: 79.99, currency: '£' },
+      { edition: "Standard Edition", price: 69.99, currency: '£' },
+    ],
+  },
+};
