4e66b4c7911b763e9be7307394cd4f601fabe02a
celso <celso.sjunior@outlook.com>
2025-06-30 18:22:53 -0300

    Atualizar o DeathStranding2.ts

--- 2k38/cycle/src/data/games/DeathStranding2.ts
+++ 2k38/cycle/src/data/games/DeathStranding2.ts
@@ -11,6 +11,7 @@ export const deathStranding2OnTheBeach = {
 
   Brazil: {
     'June 2025': [
+      { edition: "Standard Edition", price: 245.46, discount: 30, currency: 'R$', exchangeTax: 3.59 },
       { edition: "Deluxe Edition", price: 449.90, increase: 12.5, currency: 'R$', exchangeTax: 5.62 },
       { edition: "Standard Edition", price: 399.90, increase: 14.3, currency: 'R$', exchangeTax: 5.71 },
       { edition: "Standard Edition", price: 251.93, discount: 28, currency: 'R$', exchangeTax: 3.59 },
