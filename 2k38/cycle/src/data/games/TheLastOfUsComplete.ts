73a88b803187c4daaa27483133ff249723666465
celso <celso.sjunior@outlook.com>
2025-07-01 21:55:37 -0300

    Criar TheLastOfUsComplete.ts

---
+++ 2k38/cycle/src/data/games/TheLastOfUsComplete.ts
@@ -0,0 +1,34 @@
+export const tloucomplete = {
+  launchDate: '2025-04-10',
+
+  Japan: {
+    'April 2025': [
+      { edition: "Complete Edition", price: 11980, currency: '¥' },
+    ],
+  },
+  Brazil: {
+    'July 2025': [
+      { edition: "Complete Edition", price: 569.90, increase: 14, currency: 'R$', exchangeTax: 5.69 },
+
+    ],
+    'April 2025': [
+      { edition: "Complete Edition", price: 499.50, currency: 'R$', exchangeTax: 4.99 },
+    ],
+  },
+  Europe: {
+    'April 2025': [
+      { edition: "Complete Edition", price: 109.99, currency: '€' },
+    ],
+  },
+
+  UnitedKingdom: {
+    'April 2025': [
+      { edition: "Complete Edition", price: 94.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'April 2025': [
+      { edition: "Complete Edition", price: 99.99, currency: '$' },
+    ],
+  },
+};
