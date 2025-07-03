b3c4621c61ee9ff9310e776013f07d2ccb3cc0df
Celso <celso.sjunior@outlook.com>
2025-07-02 21:59:23 -0300

    update gtav

--- 2k38/cycle/src/data/games/GrandTheftAutoVI.ts
+++ 2k38/cycle/src/data/games/GrandTheftAutoVI.ts
@@ -5,10 +5,12 @@ export const gtavi = {
     'May 2026': [
       { edition: 'Super Deluxe Edition', price: 649.90, currency: 'R$', exchangeTax: 4.99 },
       { edition: 'Deluxe Edition', price: 499.90, currency: 'R$', exchangeTax: 5.00 },
-      { edition: 'Standard Edition', price: 379.90, currency: 'R$', exchangeTax: 5.42 },
+      { edition: 'Standard Edition', price: 665.90, currency: 'R$', basePrice: 99.99, exchangeTax2: 6.66 },
+      { edition: 'Standard Edition', price: 599.90, currency: 'R$', basePrice: 89.99, exchangeTax2: 6.66 },
+      { edition: 'Standard Edition', price: 532.90, currency: 'R$', basePrice: 79.99, exchangeTax2: 6.66 },
+      { edition: 'Standard Edition', price: 465.90, currency: 'R$', basePrice: 69.99, exchangeTax2: 6.66 },
     ],
   },
-
   Europe: {
     'May 2026': [
       { edition: 'Super Deluxe Edition', price: 129.99, currency: '€' },
