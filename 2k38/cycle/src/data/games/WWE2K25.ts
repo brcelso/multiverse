2b87d0ff856607dff4f03818ea05b3ec7763c670
celso <celso.sjunior@outlook.com>
2025-07-04 20:35:52 -0300

    Criar WWE2K25.ts

---
+++ 2k38/cycle/src/data/games/WWE2K25.ts
@@ -0,0 +1,48 @@
+export const nflCollegeFootball2026 = {
+  launchDate: '2025-03-13',
+
+  Brazil: {
+    'July 2025': [
+      { edition: 'Deadline', price: 428.93, discount: 34, currency: 'R$', exchangeTax2: 5.69 },
+      { edition: 'Standard', price: 227.94, discount: 40, currency: 'R$', exchangeTax2: 4.99 },
+    ],
+    'March 2025': [
+      { edition: 'Deadline', price: 649.90, currency: 'R$', exchangeTax2: 4.99 },
+      { edition: 'Deadman', price: 499.9, currency: 'R$', exchangeTax2: 5 },
+      { edition: 'Standard', price: 379.90, currency: 'R$', exchangeTax2: 4.99 },
+    ],
+  },
+  Europe: {
+    'July 2025': [
+      { edition: 'Deadline', price: 85.79, discount: 34, currency: '€' },
+      { edition: 'Standard', price: 44.99, discount: 40, currency: '€' },
+    ],
+    'March 2025': [
+      { edition: 'Deadline', price: 129.99, currency: '€' },
+      { edition: 'Deadman', price: 99.99, currency: '€' },
+      { edition: 'Standard', price: 74.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'July 2025': [
+      { edition: 'Deadline', price: 79.19, discount: 34, currency: '£' },
+      { edition: 'Standard', price: 38.99, discount: 40, currency: '£' },
+    ],
+    'March 2025': [
+      { edition: 'Deadline', price: 119.99, currency: '£' },
+      { edition: 'Deadman', price: 89.99, currency: '£' },
+      { edition: 'Standard', price: 64.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'July 2025': [
+      { edition: 'Deadline', price: 85.79, discount: 34, currency: '$' },
+      { edition: 'Standard', price: 41.99, discount: 40, currency: '$' },
+    ],
+    'March 2025': [
+      { edition: 'Deadline', price: 129.99, currency: '$' },
+      { edition: 'Deadman', price: 99.99, currency: '$' },
+      { edition: 'Standard', price: 69.99, currency: '$' },
+    ],
+  },
+};
