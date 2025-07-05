58ffc502c82ac28ce46a240dc0972d407f0dc130
celso <celso.sjunior@outlook.com>
2025-07-05 10:29:50 -0300

    Atualizar o DeathStranding2.ts

--- 2k38/cycle/src/data/games/DeathStranding2.ts
+++ 2k38/cycle/src/data/games/DeathStranding2.ts
@@ -1,30 +1,22 @@
 export const deathStranding2OnTheBeach = {
   launchDate: '2025-06-22',
 
-  Japan: {
-    'June 2025': [
-      { edition: "Collector's Edition", price: 31980, currency: '¥' },  
-      { edition: "Deluxe Edition", price: 9980, currency: '¥' },
-      { edition: "Standard Edition", price: 8980, currency: '¥' },
+  Argentina: {
+    'July 2025': [
+      { edition: "Standard Edition", price: 60490, discount: 12, currency: 'ARS' },
+      { edition: "Standard Edition", price: 68490, currency: 'ARS', exchangeTax2: 978.56 },
+
     ],
   },
 
   Brazil: {
     'June 2025': [
-      { edition: "Standard Edition", price: 245.46, discount: 30, currency: 'R$', exchangeTax2: 3.59 },
+      { edition: "Physical Media", price: 245.46, discount: 30, currency: 'R$', exchangeTax2: 3.59 },
       { edition: "Deluxe Edition", price: 449.90, increase: 12.5, currency: 'R$', exchangeTax2: 5.62 },
       { edition: "Standard Edition", price: 399.90, increase: 14.3, currency: 'R$', exchangeTax2: 5.71 },
-      { edition: "Standard Edition", price: 251.93, discount: 28, currency: 'R$', exchangeTax2: 3.59 },
+      { edition: "Physical Media", price: 251.93, discount: 28, currency: 'R$', exchangeTax2: 3.59 },
       { edition: "Deluxe Edition", price: 399.5, currency: 'R$', exchangeTax2: 4.99 },
       { edition: "Standard Edition", price: 349.9, currency: 'R$', exchangeTax2: 4.99 },
-    ],
-  },
-
-  UnitedStates: {
-    'June 2025': [
-      { edition: "Collector's Edition", price: 229.99, currency: '$' },
-      { edition: "Deluxe Edition", price: 79.99, currency: '$' },
-      { edition: "Standard Edition", price: 69.99, currency: '$' },
     ],
   },
 
@@ -36,11 +28,27 @@ export const deathStranding2OnTheBeach = {
     ],
   },
 
+  Japan: {
+    'June 2025': [
+      { edition: "Collector's Edition", price: 31980, currency: '¥' },  
+      { edition: "Deluxe Edition", price: 9980, currency: '¥' },
+      { edition: "Standard Edition", price: 8980, currency: '¥' },
+    ],
+  },
+
   UnitedKingdom: {
     'June 2025': [
       { edition: "Collector's Edition", price: 229.99, currency: '£' },
       { edition: "Deluxe Edition", price: 79.99, currency: '£' },
       { edition: "Standard Edition", price: 69.99, currency: '£' },
+    ],
+  },
+
+  UnitedStates: {
+    'June 2025': [
+      { edition: "Collector's Edition", price: 229.99, currency: '$' },
+      { edition: "Deluxe Edition", price: 79.99, currency: '$' },
+      { edition: "Standard Edition", price: 69.99, currency: '$' },
     ],
   },
 };
