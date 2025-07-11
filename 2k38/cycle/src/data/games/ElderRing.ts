bd98643ce283ba1b728553f143cf7f9318a219e7
celso <celso.sjunior@outlook.com>
2025-07-11 14:52:25 -0300

    Atualizar o ElderRing.ts

--- 2k38/cycle/src/data/games/ElderRing.ts
+++ 2k38/cycle/src/data/games/ElderRing.ts
@@ -2,6 +2,9 @@ export const eldenRing = {
   launchDate: '2022-02-25',
 
   Brazil: {
+    'July 2025': [
+      { edition: 'Standard Edition', price: 179.94, discount: 40, currency: 'R$' },
+    ],
     'February 2022': [
       { edition: 'Deluxe Edition', price: 399.5, currency: 'R$' },
       { edition: 'Standard Edition', price: 299.9, currency: 'R$' },
