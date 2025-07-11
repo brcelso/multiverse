a9c3f9bde6779a054d5346991464888a5f3f3b5f
celso <celso.sjunior@outlook.com>
2025-07-10 21:46:48 -0300

    Criar thecallistoprotocol.ts

---
+++ 2k38/cycle/src/data/games/thecallistoprotocol.ts
@@ -0,0 +1,41 @@
+export const thecallistoprotocol = {
+  launchDate: '2022-12-02',
+
+  Brazil: {
+    'July 2025': [
+      { edition: "Deluxe Edition PS5", price: 72.43, discount: 85, currency: 'R$', exchangeTax: 3.69 },
+      { edition: "Dexuxe Edition PS4", price: 64.48, discount: 85, currency: 'R$', exchangeTax: 3.69 },
+    ],
+    'December 2022': [
+      { edition: "Deluxe Edition PS5", price: 482.50, increase: 7.5, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Deluxe Edition PS5", price: 449.50, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Deluxe Edition PS4", price: 429.90, increase: 7.5, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Deluxe Edition PS4", price: 399.50, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Standard Edition PS5", price: 374.90, increase: 7, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Standard Edition PS5", price: 349.90, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Standard Edition PS4", price: 319.90, increase: 7, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Standard Edition PS4", price: 299.90, currency: 'R$', exchangeTax: 4.99 },
+    ],
+  },
+
+  UnitedStates: {
+    'June 2025': [
+      { edition: "Deluxe Edition", price: 79.99, currency: '$' },
+      { edition: "Standard Edition", price: 69.99, currency: '$' },
+    ],
+  },
+
+  Europe: {
+    'June 2025': [
+      { edition: "Deluxe Edition", price: 89.99, currency: '€' },
+      { edition: "Standard Edition", price: 79.99, currency: '€' },
+    ],
+  },
+
+  UnitedKingdom: {
+    'June 2025': [
+      { edition: "Deluxe Edition", price: 79.99, currency: '£' },
+      { edition: "Standard Edition", price: 69.99, currency: '£' },
+    ],
+  },
+};
