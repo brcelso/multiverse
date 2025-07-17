6b04412a902012a69a7898030c8a8932a361fb93
celso <celso.sjunior@outlook.com>
2025-07-16 21:28:55 -0300

    Atualizar o GhostofYotei.ts

--- 2k38/cycle/src/data/games/GhostofYotei.ts
+++ 2k38/cycle/src/data/games/GhostofYotei.ts
@@ -2,6 +2,9 @@ export const ghostOfYotei = {
   launchDate: '2025-10-02',
 
   Brazil: {
+    'July 2025': [
+      { edition: 'Standard Edition', price: 279.92, discount: 30, currency: 'R$', exchangeTax: 3.99 },
+    ],
     'June 2025': [
       { edition: 'Standard Edition', price: 287.93, discount: 28, currency: 'R$', exchangeTax: 4.62 },
       { edition: 'Deluxe Edition', price: 449.9, currency: 'R$', exchangeTax: 5.62 },
