aeb69572cf5fafd8815a1f9246c34fea72a2ce3f
celso <celso.sjunior@outlook.com>
2025-07-16 21:39:21 -0300

    Criar FC26.ts

---
+++ 2k38/cycle/src/data/games/FC26.ts
@@ -0,0 +1,31 @@
+export const fc26 = {
+  launchDate: '2025-09-26',
+
+  Brazil: {
+    'July 2025': [
+      { edition: 'Ultimate Edition', price: 499.50, currency: 'R$' },
+      { edition: 'Standard Edition ', price: 349.90, currency: 'R$' },
+    ],
+  },
+
+  UnitedStates: {
+    'July 2025': [
+      { edition: 'Ultimate Edition', price: 99.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+
+  Europe: {
+    'July 2025': [
+      { edition: 'Ultimate Edition', price: 109.99, currency: '€' },
+      { edition: 'Standard Edition', price: 79.99, currency: '€' },
+    ],
+  },
+
+  UnitedKingdom: {
+    'July 2025': [
+      { edition: 'Ultimate Edition', price: 99.99, currency: '£' },
+      { edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
+};
