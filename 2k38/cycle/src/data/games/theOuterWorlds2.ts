a9f9306959ef467fe487d25aae9bea543ccb5c91
celso <celso.sjunior@outlook.com>
2025-07-23 14:54:09 -0300

    Atualizar o theOuterWorlds2.ts

--- 2k38/cycle/src/data/games/theOuterWorlds2.ts
+++ 2k38/cycle/src/data/games/theOuterWorlds2.ts
@@ -14,12 +14,18 @@ export const theouterworlds2 = {
 ],
   },
   Europe: {
+    'July 2025': [
+      { edition: 'Standard', price: 69.99, discount: 12.5, currency: '€' },
+    ],
     'June 2025': [
-      { edition: 'Premiun', price: 99.99, currency: '€' },
+      { edition: 'Premium', price: 99.99, currency: '€' },
       { edition: 'Standard', price: 79.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
+    'July 2025': [
+      { edition: 'Standard', price: 59.99, discount: 14.5, currency: '£' },
+    ],
     'June 2025': [
       { edition: 'Premium', price: 89.99, currency: '£' },
       { edition: 'Standard', price: 69.99, currency: '£' },
