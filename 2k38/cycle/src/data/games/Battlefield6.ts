c443aa2ffcf4b8032a46df11f7dcd1bae237f41c
celso <celso.sjunior@outlook.com>
2025-07-23 21:52:04 -0300

    Criar Battlefield6.ts

---
+++ 2k38/cycle/src/data/games/Battlefield6.ts
@@ -0,0 +1,31 @@
+export const bf6 = {
+  launchDate: '2025-10-10',
+
+  Brazil: {
+    'July 2025': [
+      { edition: 'Phantom Edition', price: 625.00, currency: 'R$', exchangeTax: 5.71 },
+      { edition: 'Standard Edition', price: 455.00, currency: 'R$', exchangeTax: 5.71 },
+    ],
+  },
+
+  UnitedStates: {
+    'July 2025': [
+      { edition: 'Phantom Edition', price: 109.99, currency: '$' },
+      { edition: 'Standard Edition', price: 79.99, currency: '$' },
+    ],
+  },
+
+  Europe: {
+    'July 2025': [
+      { edition: 'Phantom Edition', price: 109.99, currency: '€' },
+      { edition: 'Standard Edition', price: 79.99, currency: '€' },
+    ],
+  },
+
+  UnitedKingdom: {
+    'July 2025': [
+      { edition: 'Phantom Edition', price: 99.99, currency: '£' },
+      { edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
+};
