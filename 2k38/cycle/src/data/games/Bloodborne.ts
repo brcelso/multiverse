b65c103a439ee220c49984cb72f3f2dea3676dbb
Celso <celso.sjunior@outlook.com>
2025-08-13 21:36:21 -0300

    added games

--- 2k38/cycle/src/data/games/Bloodborne.ts
+++ 2k38/cycle/src/data/games/Bloodborne.ts
@@ -3,39 +3,63 @@ export const bloodborne = {
 
   Brazil: {
     'August 2025': [
-      { edition: 'Standard Edition', price: 57.45, discount: 50, currency: 'R$' },
+      { edition: 'Standard Edition', price: 57.45, discount: 50, currency: 'R$', exchangeTax: 5.74},
+    ],
+    'June 2025': [
+      { edition: 'Standard Edition', price: 114.90, currency: 'R$'},
+    ],
+    'June 2021': [
+      { edition: 'Standard Edition', price: 99.50, currency: 'R$'},
     ],
     'March 2015': [
-      { edition: 'GOTY Edition', price: 199.50, currency: 'R$'},
-      { edition: 'Standard Edition', price: 114.90, currency: 'R$'},
+      { edition: 'Complete + DLC Edition', price: 153.50, currency: 'R$'},
+      { edition: 'Standard Edition', price: 179.99, currency: 'R$', exchangeTax: 3},
     ],
   },
 
   UnitedStates: {
-    'August 2025': [
+      'August 2025': [
         { edition: 'Standard Edition', price: 9.99, discount: 50, currency: '$' },
         ],
-        'March 2015': [
+        'June 2021': [
         { edition: 'GOTY Edition', price: 34.99, currency: '$' },
         { edition: 'Standard Edition', price: 19.99, currency: '$' },
+        ],
+        'June 2015': [
+        { edition: 'Standard Edition', price: 39.99, currency: '$' },
+        ],
+        'March 2015': [
+        { edition: 'Standard Edition', price: 59.99, currency: '$' },
         ],
   },
     Europe: {
         'August 2025': [
         { edition: 'Standard Edition', price: 9.99, discount: 50, currency: '€' },
         ],
-        'March 2015': [
+        'June 2021': [
         { edition: 'GOTY Edition', price: 34.99, currency: '€' },
-        { edition: 'Standard Edition', price: 19.99, currency: '€' },
+        { edition: 'Standard Edition', price: 15.99, currency: '€' },
+        ],
+        'June 2015': [
+        { edition: 'Standard Edition', price: 39.99, currency: '€' },
+        ],
+        'March 2015': [
+        { edition: 'Standard Edition', price: 59.99, currency: '€' },
         ],
     },
     UnitedKingdom: {
         'August 2025': [
         { edition: 'Standard Edition', price: 7.99, discount: 50, currency: '£' },
         ],
-        'March 2015': [
+        'June 2021': [
         { edition: 'GOTY Edition', price: 29.99, currency: '£' },
         { edition: 'Standard Edition', price: 15.99, currency: '£' },
+        ],
+        'June 2015': [
+        { edition: 'Standard Edition', price: 34.99, currency: '£' },
+        ],
+        'March 2015': [
+        { edition: 'Standard Edition', price: 49.99, currency: '£' },
         ],
     },
 };
