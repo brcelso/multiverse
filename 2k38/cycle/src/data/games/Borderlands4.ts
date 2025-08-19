30d0026ad42530bfc461e21af8c5059a0d770dd4
celso <celso.sjunior@outlook.com>
2025-08-19 15:23:14 -0300

    Atualizar o Borderlands4.ts

--- 2k38/cycle/src/data/games/Borderlands4.ts
+++ 2k38/cycle/src/data/games/Borderlands4.ts
@@ -2,7 +2,10 @@ export const borderlands4 = {
   launchDate: '2025-09-12',
 
   Brazil: {
-    'September 2025': [
+    'August 2025': [
+      { edition: 'Standard Edition', price: 306.99, discount: 19, currency: 'R$', exchangeTax: 3.83 },
+    ],
+    'June 2025': [
       { edition: 'Super Deluxe Edition', price: 649.90, currency: 'R$', exchangeTax: 4.99 },
       { edition: 'Deluxe Edition', price: 499.90, currency: 'R$', exchangeTax: 5.00 },
       { edition: 'Standard Edition', price: 379.90, currency: 'R$', exchangeTax: 5.42 },
@@ -10,7 +13,7 @@ export const borderlands4 = {
   },
 
   Europe: {
-    'September 2025': [
+    'June 2025': [
       { edition: 'Super Deluxe Edition', price: 129.99, currency: '€' },
       { edition: 'Deluxe Edition', price: 99.99, currency: '€' },
       { edition: 'Standard Edition', price: 79.99, currency: '€' },
@@ -18,7 +21,7 @@ export const borderlands4 = {
   },
   
   UnitedKingdom: {
-    'September 2025': [
+    'June 2025': [
       { edition: 'Super Deluxe Edition', price: 119.99, currency: '£' },
       { edition: 'Deluxe Edition', price: 89.99, currency: '£' },
       { edition: 'Standard Edition', price: 69.99, currency: '£' },
@@ -26,7 +29,7 @@ export const borderlands4 = {
   },
 
   UnitedStates: {
-    'September 2025': [
+    'June 2025': [
       { edition: 'Super Deluxe Edition', price: 129.99, currency: '$' },
       { edition: 'Deluxe Edition', price: 99.99, currency: '$' },
       { edition: 'Standard Edition', price: 69.99, currency: '$' },
