3928f0a1100fec427b150138bf0ea27b363a5876
celso <celso.sjunior@outlook.com>
2025-08-19 15:09:36 -0300

    Criar COD7.ts

---
+++ 2k38/cycle/src/data/games/COD7.ts
@@ -0,0 +1,31 @@
+export const cod7 = {
+  launchDate: '2025-11-14',
+
+  Brazil: {
+    'August 2025': [
+      { edition: 'Vault Edition', price: 479.90, currency: 'R$', exchangeTax: 5 },
+      { edition: 'Standard Edition', price: 349.90, currency: 'R$', exchangeTax: 5 },
+    ],
+  },
+
+  UnitedStates: {
+    'August 2025': [
+      { edition: 'Vault Edition', price: 99.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+
+  Europe: {
+    'August 2025': [
+      { edition: 'Vault Edition', price: 109.99, currency: '€' },
+      { edition: 'Standard Edition', price: 79.99, currency: '€' },
+    ],
+  },
+
+  UnitedKingdom: {
+    'August 2025': [
+      { edition: 'Vault Edition', price: 99.99, currency: '£' },
+      { edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
+};
