4a65e099b585ebfb1ad15c6d9d00d3bb9a6d479b
celso <celso.sjunior@outlook.com>
2025-08-20 02:52:58 -0300

    Atualizar o FC26.ts

--- 2k38/cycle/src/data/games/FC26.ts
+++ 2k38/cycle/src/data/games/FC26.ts
@@ -2,6 +2,9 @@ export const fc26 = {
   launchDate: '2025-09-26',
 
   Brazil: {
+    'August 2025': [
+      { edition: 'Ultimate Edition', price: 449.50, discount: 10, currency: 'R$' },
+    ],
     'July 2025': [
       { edition: 'Ultimate Edition', price: 499.50, currency: 'R$' },
       { edition: 'Standard Edition', price: 349.90, currency: 'R$' },
