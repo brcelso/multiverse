cf8741ddbbf577a12c0ff800765e0e8881ec30a6
celso <celso.sjunior@outlook.com>
2025-08-21 08:59:27 -0300

    Atualizar o COD7.ts

--- 2k38/cycle/src/data/games/COD7.ts
+++ 2k38/cycle/src/data/games/COD7.ts
@@ -7,25 +7,22 @@ export const cod7 = {
       { edition: 'Standard Edition', price: 349.90, currency: 'R$', exchangeTax: 5 },
     ],
   },
-
-  UnitedStates: {
-    'August 2025': [
-      { edition: 'Vault Edition', price: 99.99, currency: '$' },
-      { edition: 'Standard Edition', price: 69.99, currency: '$' },
-    ],
-  },
-
   Europe: {
     'August 2025': [
       { edition: 'Vault Edition', price: 109.99, currency: '€' },
       { edition: 'Standard Edition', price: 79.99, currency: '€' },
     ],
   },
-
   UnitedKingdom: {
     'August 2025': [
       { edition: 'Vault Edition', price: 99.99, currency: '£' },
       { edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'August 2025': [
+      { edition: 'Vault Edition', price: 99.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
     ],
   },
 };
