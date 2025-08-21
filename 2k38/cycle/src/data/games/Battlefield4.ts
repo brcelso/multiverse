a82a5b54eea9b358ad7689f138b0f00b44ff7e69
celso <celso.sjunior@outlook.com>
2025-08-21 13:20:08 -0300

    Atualizar o Battlefield4.ts

--- 2k38/cycle/src/data/games/Battlefield4.ts
+++ 2k38/cycle/src/data/games/Battlefield4.ts
@@ -11,17 +11,6 @@ export const bf4 = {
       { edition: 'Standard Edition', price: 99.50, currency: 'R$', exchangeTax: 5 },
     ],
   },
-
-  UnitedStates: {
-    'August 2025': [
-      { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '$' },
-    ],
-    'November 2013': [
-      { edition: 'Premium Edition', price: 39.99, currency: '$' },
-      { edition: 'Standard Edition', price: 19.99, currency: '$' },
-    ],
-  },
-
   Europe: {
     'August 2025': [
       { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '€' },
@@ -31,14 +20,22 @@ export const bf4 = {
       { edition: 'Standard Edition', price: 19.99, currency: '€' },
     ],
   },
-
   UnitedKingdom: {
     'August 2025': [
       { edition: 'Standard Edition', price: 3.99, discount: 75, currency: '£' },
     ],
     'November 2013': [
-      { edition: 'Premium Edition', price: 15.99, currency: '£' },
-      { edition: 'Standard Edition', price: 34.99, currency: '£' },
+      { edition: 'Premium Edition', price: 34.99, currency: '£' },
+      { edition: 'Standard Edition', price: 15.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'August 2025': [
+      { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '$' },
+    ],
+    'November 2013': [
+      { edition: 'Premium Edition', price: 39.99, currency: '$' },
+      { edition: 'Standard Edition', price: 19.99, currency: '$' },
     ],
   },
 };
