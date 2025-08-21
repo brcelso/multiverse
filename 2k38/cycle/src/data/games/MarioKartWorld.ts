d27869e6223012b5fc04f694915a97e1f740a6dc
celso <celso.sjunior@outlook.com>
2025-08-21 07:56:41 -0300

    Atualizar o MarioKartWorld.ts

--- 2k38/cycle/src/data/games/MarioKartWorld.ts
+++ 2k38/cycle/src/data/games/MarioKartWorld.ts
@@ -6,10 +6,22 @@ export const marioKartWorldSW2 = {
       { price: 499.9, currency: 'R$', exchangeTax: 6.24 },
     ],
   },
-
+  Europe: {
+    'June 2025': [
+      { price: 89.99, currency: '€' },
+      { price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'June 2025': [
+      { price: 74.99, currency: '£' },
+      { price: 66.99, currency: '£' },
+    ],
+  },
   UnitedStates: {
     'June 2025': [
       { price: 79.99, currency: '$' },
+      { price: 69.99, currency: '$' },
     ],
   },
 };
