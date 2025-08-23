05b767a54ca938e9eeb6bf96c7557178458e9ba2
celso <celso.sjunior@outlook.com>
2025-08-22 21:51:29 -0300

    Atualizar o TheLastOfUsPart2.ts

--- 2k38/cycle/src/data/games/TheLastOfUsPart2.ts
+++ 2k38/cycle/src/data/games/TheLastOfUsPart2.ts
@@ -33,6 +33,9 @@ export const theLastOfUsPart2 = {
     'November 2023': [
       { edition: 'Standard Edition', price: 19.99, currency: '$' , discount: 66 },
     ],
+    'July 2023': [
+      { edition: 'Standard Edition', price: 39.99, currency: '$' , discount: 33 },
+    ],
     'June 2020': [
       { edition: 'Ellie Edition', price: 229.99, currency: '$' },
       { edition: "Collector's Edition", price: 169.99, currency: '$' },
