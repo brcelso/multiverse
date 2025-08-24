8a0b2e275044e40c4ef5c54b6462f61fdd197175
Celso <celso.sjunior@outlook.com>
2025-08-24 14:01:57 -0300

    updated apple

--- 2k38/cycle/src/data/subs/AppleTV.ts
+++ 2k38/cycle/src/data/subs/AppleTV.ts
@@ -2,20 +2,20 @@ export const appletv = {
   launchDate: '2019-11-01',
   Brazil: {
     'August 2025': [
-      { price: 29.90, increase: 30, totalIncrease: 200,
-        currency: 'R$', exchangeTax: 2.30, edition: "Apple TV+" },
+      { price: 29.90, increase: 30, exchangeTax: 2.30, exchangeTax2: 5.50, realPrice: 71.44,
+        currency: 'R$', edition: "Apple TV+", totalIncrease: 200, },
       ],
     'October 2023': [
-      { price: 21.90, increase: 25, totalIncrease: 122,
-        currency: 'R$', exchangeTax: 2.19, edition: "Apple TV+" },
+      { price: 21.90, increase: 25, exchangeTax: 2.19, exchangeTax2: 5.08, realPrice: 50.74,
+        currency: 'R$', edition: "Apple TV+", totalIncrease: 122, },
       ],
     'October 2022': [
-      { price: 14.90, increase: 40, currency: 'R$', exchangeTax: 2.13, 
-        edition: "Apple TV+" },
+      { price: 14.90, increase: 40, exchangeTax: 2.13, exchangeTax2: 5.40, realPrice: 37.74,
+        currency: 'R$', edition: "Apple TV+" },
       ],
     'November 2019': [
-      { price: 9.90, currency: 'R$', exchangeTax: 2, 
-        edition: "Apple TV+" },
+      { price: 9.90,  exchangeTax: 2, exchangeTax2: 4.23, realPrice: 21.10,
+        currency: 'R$', edition: "Apple TV+" },
       ],
       },
     Europe: {
