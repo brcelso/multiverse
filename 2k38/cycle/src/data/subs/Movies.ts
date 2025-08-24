7e0acee53c98492988300faf7c3cdf9645cb686a
celso <celso.sjunior@outlook.com>
2025-08-24 10:52:41 -0300

    Criar Movies.ts

---
+++ 2k38/cycle/src/data/subs/Movies.ts
@@ -0,0 +1,23 @@
+export const movies = {
+  launchDate: '2025-08-23',
+  Brazil: {
+    'August 2025': [
+      { price: 69.90, currency: 'R$', exchangeTax: 2.79, 
+        edition: "Buy" },
+      ],
+    'August 2025': [
+      { price: 49.90, currency: 'R$', exchangeTax: 2.50, 
+        edition: "Rent" },
+      ],
+      },
+  UnitedStates: {
+    'August 2025': [
+      { price: 25, currency: '$', 
+        edition: "Buy" },
+      ],
+    'August 2025': [
+      { price: 20, currency: '$', 
+        edition: "Rent" },
+      ],
+      },
+    }
