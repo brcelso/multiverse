fe85114a9d2dba02365f06a942f3e0146d8c0d93
Celso <celso.sjunior@outlook.com>
2025-08-24 13:30:09 -0300

    updated apple

--- 2k38/cycle/src/data/subs/Movies.ts
+++ 2k38/cycle/src/data/subs/Movies.ts
@@ -2,9 +2,9 @@ export const movies = {
   launchDate: '2025-08-23',
   Brazil: {
     'August 2025': [
-      { price: 69.90, currency: 'R$', exchangeTax: 2.79, 
+      { price: 69.90, currency: 'R$', exchangeTax: 2.79, exchangeTax2: 5.5, realPrice: 137.44,
         edition: "F1 2025 Buy" },
-      { price: 49.90, currency: 'R$', exchangeTax: 2.50, 
+      { price: 49.90, currency: 'R$', exchangeTax: 2.50, exchangeTax2: 5.5, realPrice: 109.94,
         edition: "F1 2025 Rent" },
       ],
       },
