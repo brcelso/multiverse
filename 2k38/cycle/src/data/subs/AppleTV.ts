b7f8f35c86d1708262fac47e4e903404c64d991d
celso <celso.sjunior@outlook.com>
2025-08-24 10:59:52 -0300

    Atualizar o AppleTV.ts

--- 2k38/cycle/src/data/subs/AppleTV.ts
+++ 2k38/cycle/src/data/subs/AppleTV.ts
@@ -5,15 +5,15 @@ export const appletv = {
       { price: 29.90, increase: 30, currency: 'R$', exchangeTax: 2.30, 
         edition: "Apple TV+" },
       ],
-    'Outubro 2023': [
+    'October 2023': [
       { price: 21.90, increase: 25, currency: 'R$', exchangeTax: 2.19, 
         edition: "Apple TV+" },
       ],
-    'Outubro 2022': [
+    'October 2022': [
       { price: 14.90, increase: 40, currency: 'R$', exchangeTax: 2.13, 
         edition: "Apple TV+" },
       ],
-    'Novembro 2019': [
+    'November 2019': [
       { price: 9.90, currency: 'R$', exchangeTax: 2, 
         edition: "Apple TV+" },
       ],
@@ -23,15 +23,15 @@ export const appletv = {
       { price: 12.99, increase: 30, currency: '$', 
         edition: "Apple TV+" },
       ],
-    'Outubro 2023': [
+    'October 2023': [
       { price: 9.99, increase: 25, currency: '$', 
         edition: "Apple TV+" },
       ],
-    'Outubro 2022': [
+    'October 2022': [
       { price: 6.99, increase: 40, currency: '$', 
         edition: "Apple TV+" },
       ],
-    'Novembro 2019': [
+    'November 2019': [
       { price: 4.99, currency: '$', 
         edition: "Apple TV+" },
       ],
