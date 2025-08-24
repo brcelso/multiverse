871ed0e2fde623414b604443246595389a168051
Celso <celso.sjunior@outlook.com>
2025-08-24 12:26:37 -0300

    updated apple

--- 2k38/cycle/src/data/subs/AppleTV.ts
+++ 2k38/cycle/src/data/subs/AppleTV.ts
@@ -18,6 +18,42 @@ export const appletv = {
         edition: "Apple TV+" },
       ],
       },
+    Europe: {
+    'August 2025': [
+      { price: 12.99, increase: 30, totalIncrease: 160,
+        currency: '€', edition: "Apple TV+" },
+      ],
+    'October 2023': [
+      { price: 9.99, increase: 25, totalIncrease: 50,
+        currency: '€', edition: "Apple TV+" },
+      ],
+    'October 2022': [
+      { price: 6.99, increase: 40, currency: '€', 
+        edition: "Apple TV+" },
+      ],
+    'November 2019': [
+      { price: 4.99, currency: '€', 
+        edition: "Apple TV+" },
+      ],
+      },
+    UnitedKingdom: {
+    'August 2025': [
+      { price: 9.99, increase: 11, totalIncrease: 100,
+        currency: '£', edition: "Apple TV+" },
+      ],
+    'October 2023': [
+      { price: 8.99, increase: 29, totalIncrease: 80,
+        currency: '£', edition: "Apple TV+" },
+      ],
+    'October 2022': [
+      { price: 6.99, increase: 20, currency: '£', 
+        edition: "Apple TV+" },
+      ],
+    'November 2019': [
+      { price: 4.99, currency: '£', 
+        edition: "Apple TV+" },
+      ],
+      },
   UnitedStates: {
     'August 2025': [
       { price: 12.99, increase: 30, totalIncrease: 160,
