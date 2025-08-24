72ba0d9e8150839ab2f6802b85338248f816ee01
celso <celso.sjunior@outlook.com>
2025-08-24 09:43:39 -0300

    Criar AppleTV.ts

---
+++ 2k38/cycle/src/data/subs/AppleTV.ts
@@ -0,0 +1,39 @@
+export const appletv = {
+  launchDate: '2025-08-23',
+  Brazil: {
+    'August 2025': [
+      { price: 29.90, increase: 30, currency: 'R$', exchangeTax: 2.30, 
+        edition: "Apple TV+" },
+      ],
+    'Outubro 2023': [
+      { price: 21.90, increase: 25, currency: 'R$', exchangeTax: 2.19, 
+        edition: "Apple TV+" },
+      ],
+    'Outubro 2022': [
+      { price: 14.90, increase: 40, currency: 'R$', exchangeTax: 2.13, 
+        edition: "Apple TV+" },
+      ],
+    'Novembro 2019': [
+      { price: 9.90, currency: 'R$', exchangeTax: 2, 
+        edition: "Apple TV+" },
+      ],
+      },
+  UnitedStates: {
+    'August 2025': [
+      { price: 12.99, increase: 30, currency: '$', 
+        edition: "Apple TV+" },
+      ],
+    'Outubro 2023': [
+      { price: 9.99, increase: 25, currency: '$', 
+        edition: "Apple TV+" },
+      ],
+    'Outubro 2022': [
+      { price: 6.99, increase: 40, currency: '$', 
+        edition: "Apple TV+" },
+      ],
+    'Novembro 2019': [
+      { price: 4.99, currency: '$', 
+        edition: "Apple TV+" },
+      ],
+      },
+    }
