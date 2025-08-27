28a86c906b21fc237ba2553827ddbbe55deaa9f3
celso <celso.sjunior@outlook.com>
2025-08-26 22:19:31 -0300

    Criar AllPrices.ts

---
+++ 2k38/cycle/src/data/games/AllPrices.ts
@@ -0,0 +1,18 @@
+export const allprices {
+launchDate: '2013-06-14',
+
+  Brazil: {
+    'June 2013': [
+      { price: 149.99, currency: 'R$', exchangeTax: 2.5 },
+      { price: 119.99, currency: 'R$', exchangeTax: 2 },
+      { price: 99.99, currency: 'R$', exchangeTax: 5 },
+    ],
+  },
+
+  UnitedStates: {
+    'June 2013': [
+      { price: 59.99, currency: '$' },
+      { price: 19.99, currency: '$' },
+    ],
+  },
+};
