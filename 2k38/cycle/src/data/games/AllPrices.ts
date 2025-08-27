50a9704fb6ac5015343d0ea6fb3f4dd9a00da78b
celso <celso.sjunior@outlook.com>
2025-08-26 22:47:10 -0300

    Atualizar o AllPrices.ts

--- 2k38/cycle/src/data/games/AllPrices.ts
+++ 2k38/cycle/src/data/games/AllPrices.ts
@@ -2,14 +2,20 @@ export const allprices = {
   launchDate: '2013-06-14',
 
   Brazil: {
+    'November 2020': [
+      { edition: "PS5", price: 349.90, currency: 'R$', exchangeTax: 2.5 },
+    ],
     'June 2013': [
-      { edition: "PS3", price: 149.99, currency: 'R$', exchangeTax: 2.5 },
-      { edition: "PS3", price: 119.99, currency: 'R$', exchangeTax: 2 },
+      { edition: "PS3", price: 149.99, increase: 25, totalIncrease: 50, currency: 'R$', exchangeTax: 2.5 },
+      { edition: "PS3", price: 119.99, increase: 20, currency: 'R$', exchangeTax: 2 },
       { edition: "PS3", price: 99.99, currency: 'R$', exchangeTax: 5 },
     ],
   },
 
   UnitedStates: {
+    'November 2020': [
+      { edition: "PS5", price: 69.99, increase: 17, currency: '$' },
+    ],
     'June 2013': [
       { edition: "PS3", price: 59.99, currency: '$' },
       { edition: "PS3", price: 19.99, currency: '$' },
