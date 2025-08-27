e1386e61626273b8bbab44cf8ecf61f2389622e0
celso <celso.sjunior@outlook.com>
2025-08-27 16:54:47 -0300

    Criar DemonSouls.ts

---
+++ 2k38/cycle/src/data/games/DemonSouls.ts
@@ -0,0 +1,47 @@
+export const demonsouls = {
+  launchDate: '2020-11-11',
+
+  Brazil: {
+    'August 2025': [
+      { edition: 'Standard Edition', price: 167.95, discount: 58, currency: 'R$', exchangeTax2: 4.99 },
+    ],
+    'June 2025': [
+      { edition: 'Deluxe Edition', price: 512.90, increase: 14, currency: 'R$', exchangeTax2: 5.69 },
+      { edition: 'Standard Edition', price: 399.90, increase: 14, currency: 'R$', exchangeTax2: 5.71 },
+    ],
+    'November 2020': [
+      { edition: 'Deluxe Edition', price: 449.90, currency: 'R$', exchangeTax2: 4.99 },
+      { edition: 'Standard Edition', price: 349.90, currency: 'R$', exchangeTax2: 4.99 },
+    ],
+  },
+
+  UnitedStates: {
+    'August 2025': [
+      { edition: 'Standard Edition', price: 28.39, discount: 58, currency: '$' },
+    ],
+    'November 2020': [
+      { edition: 'Deluxe Edition', price: 89.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+
+  Europe: {
+    'August 2025': [
+      { edition: 'Standard Edition', price: 39.99, discount: 50, currency: '€' },
+    ],
+    'November 2020': [
+      { edition: 'Deluxe Edition', price: 99.99, currency: '€' },
+      { edition: 'Standard Edition', price: 79.99, currency: '€' },
+    ],
+  },
+
+  UnitedKingdom: {
+    'August 2025': [
+      { edition: 'Standard Edition', price: 34.99, discount: 50, currency: '£' },
+    ],
+    'November 2020': [
+      { edition: 'Deluxe Edition', price: 89.99, currency: '£' },
+      { edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
+};
