102c95398f502849abd844412fb601bc8fcd5d62
celso <celso.sjunior@outlook.com>
2025-08-26 22:34:31 -0300

    Atualizar o AllPrices.ts

--- 2k38/cycle/src/data/games/AllPrices.ts
+++ 2k38/cycle/src/data/games/AllPrices.ts
@@ -1,18 +1,17 @@
+
 export const allprices = {
-launchDate: '2013-06-14',
+  launchDate: '2025-06-05',
 
   Brazil: {
-    'June 2013': [
-      { price: 149.99, currency: 'R$', exchangeTax: 2.5 },
-      { price: 119.99, currency: 'R$', exchangeTax: 2 },
-      { price: 99.99, currency: 'R$', exchangeTax: 5 },
+    'June 2025': [
+      { price: 408.27, discount: 7, currency: 'R$', exchangeTax: 6.28 },
+      { price: 439.90, currency: 'R$', exchangeTax: 6.28 },
     ],
   },
 
   UnitedStates: {
-    'June 2013': [
-      { price: 59.99, currency: '$' },
-      { price: 19.99, currency: '$' },
+    'June 2025': [
+      { price: 69.99, currency: '$' },
     ],
   },
 };
