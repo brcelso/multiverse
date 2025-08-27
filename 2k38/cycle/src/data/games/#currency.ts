e956f5cb5c62720ad9dc46012733ce95ce22d09b
celso <celso.sjunior@outlook.com>
2025-08-26 22:25:05 -0300

    Atualizar o #currency.ts

--- 2k38/cycle/src/data/games/#currency.ts
+++ 2k38/cycle/src/data/games/#currency.ts
@@ -1,16 +1,19 @@
 export const currency = {
-  launchDate: '2025-06-05',
+  launchDate: '2025-02-28',
 
   Brazil: {
-    'June 2025': [
-      { price: 408.27, discount: 7, currency: 'R$', exchangeTax: 6.28 },
-      { price: 439.90, currency: 'R$', exchangeTax: 6.28 },
+    'February 2025': [
+      { edition: 'Deluxe Edition', price: 248.91, currency: 'R$', discount: 17 },
+      { edition: 'Deluxe Edition', price: 297.41, currency: 'R$', discount: 15 },
+      { edition: 'Standard Edition', price: 349.9, currency: 'R$' },
+      { edition: 'Standard Edition', price: 299.9, currency: 'R$' },
     ],
   },
 
   UnitedStates: {
-    'June 2025': [
-      { price: 69.99, currency: '$' },
+    'February 2025': [
+      { edition: 'Standard Edition', price: 49.79, currency: '$', discount: 17 },
+      { edition: 'Standard Edition', price: 59.99, currency: '$' },
     ],
   },
 };
