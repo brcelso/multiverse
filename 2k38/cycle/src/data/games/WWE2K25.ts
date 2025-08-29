018dd62d625165ded7ba7494aa0250ac32c471d4
celso <celso.sjunior@outlook.com>
2025-08-29 15:56:47 -0300

    Atualizar o WWE2K25.ts

--- 2k38/cycle/src/data/games/WWE2K25.ts
+++ 2k38/cycle/src/data/games/WWE2K25.ts
@@ -2,6 +2,9 @@ export const wwe2k25 = {
   launchDate: '2025-03-13',
 
   Brazil: {
+    'August 2025': [
+      { edition: 'Deadman', price: 249.50, discount: 50, currency: 'R$', exchangeTax2: 5 },
+    ],
     'July 2025': [
       { edition: 'Bloodline', price: 389.94, discount: 40, currency: 'R$', exchangeTax2: 4.99 },
       { edition: 'Deadman', price: 299.94, discount: 40, currency: 'R$', exchangeTax2: 5 },
@@ -14,35 +17,47 @@ export const wwe2k25 = {
     ],
   },
   Europe: {
+    'August 2025': [
+      { edition: 'Bloodline', price: 77.99, discount: 40, currency: '€' },
+      { edition: 'Standard', price: 49.99, discount: 50, currency: '€' },
+    ],
     'July 2025': [
-      { edition: 'Deadline', price: 85.79, discount: 34, currency: '€' },
+      { edition: 'Bloodline', price: 85.79, discount: 34, currency: '€' },
       { edition: 'Standard', price: 44.99, discount: 40, currency: '€' },
     ],
     'March 2025': [
-      { edition: 'Deadline', price: 129.99, currency: '€' },
+      { edition: 'Bloodline', price: 129.99, currency: '€' },
       { edition: 'Deadman', price: 99.99, currency: '€' },
       { edition: 'Standard', price: 74.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
+    'August 2025': [
+      { edition: 'Bloodline', price: 71.99, discount: 40, currency: '£' },
+      { edition: 'Standard', price: 44.99, discount: 50, currency: '£' },
+    ],
     'July 2025': [
-      { edition: 'Deadline', price: 79.19, discount: 34, currency: '£' },
+      { edition: 'Bloodline', price: 79.19, discount: 34, currency: '£' },
       { edition: 'Standard', price: 38.99, discount: 40, currency: '£' },
     ],
     'March 2025': [
-      { edition: 'Deadline', price: 119.99, currency: '£' },
+      { edition: 'Bloodline', price: 119.99, currency: '£' },
       { edition: 'Deadman', price: 89.99, currency: '£' },
       { edition: 'Standard', price: 64.99, currency: '£' },
     ],
   },
   UnitedStates: {
+    'August 2025': [
+      { edition: 'Bloodline', price: 77.99, discount: 40, currency: '$' },
+      { edition: 'Deadman', price: 49.99, discount: 50, currency: '$' },
+    ],
     'July 2025': [
-      { edition: 'Deadline', price: 85.79, discount: 34, currency: '$' },
+      { edition: 'Bloodline', price: 85.79, discount: 34, currency: '$' },
       { edition: 'Deadman', price: 59.99, discount: 40, currency: '$' },
       { edition: 'Standard', price: 41.99, discount: 40, currency: '$' },
     ],
     'March 2025': [
-      { edition: 'Deadline', price: 129.99, currency: '$' },
+      { edition: 'Bloodline', price: 129.99, currency: '$' },
       { edition: 'Deadman', price: 99.99, currency: '$' },
       { edition: 'Standard', price: 69.99, currency: '$' },
     ],
