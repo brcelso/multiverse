2b75148e561c63b3c80dac8ee0d0ddaccc5bcb24
celso <celso.sjunior@outlook.com>
2025-08-30 17:44:13 -0300

    Atualizar o HollowKnightVoidHeartEdition.ts

--- 2k38/cycle/src/data/games/HollowKnightVoidHeartEdition.ts
+++ 2k38/cycle/src/data/games/HollowKnightVoidHeartEdition.ts
@@ -2,19 +2,19 @@ export const hollowknightvoidheartedition = {
   launchDate: '2018-09-25',
   Brazil: {
     'August 2025': [
-      { edition: "Voidheart Edition", price: 23.49, discount: 50, currency: 'R$', exchangeTax2: 4.27 },
+      { edition: "Voidheart Edition", price: 23.49, discount: 50, currency: 'R$' },
     ],
     'June 2025': [
-      { edition: "Voidheart Edition", price: 85.50, increase: 36, totalIncrease: 87, currency: 'R$', exchangeTax2: 4.27 },
+      { edition: "Voidheart Edition", price: 85.50, increase: 36, totalIncrease: 87, currency: 'R$', exchangeTax2: 5.70 },
     ],
     'February 2025': [
       { edition: "Voidheart Edition", price: 31.25, discount: 50, currency: 'R$'},
     ],
     'August 2023': [
-      { edition: "Voidheart Edition", price: 62.90, increase: 37, currency: 'R$', exchangeTax2: 3.14 },
+      { edition: "Voidheart Edition", price: 62.90, increase: 37, currency: 'R$', exchangeTax2: 4.19 },
     ],
     'September 2018': [
-      { edition: "Voidheart Edition", price: 45.90, currency: 'R$' , exchangeTax2: 2.29 },
+      { edition: "Voidheart Edition", price: 45.90, currency: 'R$' , exchangeTax2: 3.06 },
     ],
   },
   Europe: {
