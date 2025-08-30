035305b4c630a2367177d072cac1cd015077d032
celso <celso.sjunior@outlook.com>
2025-08-30 11:07:54 -0300

    Criar HollowKnightSilkSong.ts

---
+++ 2k38/cycle/src/data/games/HollowKnightSilkSong.ts
@@ -0,0 +1,23 @@
+export const hollowknightsilksong = {
+  launchDate: '2025-09-04',
+  Brazil: {
+    'September 2025': [
+      { edition: "Voidheart Edition", price: 85.50, currency: 'R$', exchangeTax2: 4.27 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: "Voidheart Edition", price: 14.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: "Voidheart Edition", price: 11.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: "Voidheart Edition", price: 14.99, currency: '$' },
+    ],
+  },
+}
