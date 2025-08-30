7812eebf0ef72de05e10c438e8671f0e724a7d45
celso <celso.sjunior@outlook.com>
2025-08-30 10:02:10 -0300

    Criar HollowKnightVoidHeartEdition.ts

---
+++ 2k38/cycle/src/data/games/HollowKnightVoidHeartEdition.ts
@@ -0,0 +1,32 @@
+export const hollowknightvoidheartedition = {
+  launchDate: '2018-09-25',
+  Brazil: {
+    'June 2025': [
+      { edition: "Voidheart Edition", price: 85.50, increase: 36, totalIncrease: 87, currency: 'R$', exchangeTax2: 4.27 },
+    ],
+    'February 2025': [
+      { edition: "Voidheart Edition", price: 31.25, discount: 50, currency: 'R$'},
+    ],
+    'August 2023': [
+      { edition: "Voidheart Edition", price: 62.90, increase: 37, currency: 'R$', exchangeTax2: 3.14 },
+    ],
+    'September 2018': [
+      { edition: "Voidheart Edition", price: 45.90, currency: 'R$' , exchangeTax2: 2.29 },
+    ],
+  },
+  Europe: {
+    'September 2018': [
+      { edition: "Voidheart Edition", price: 14.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2018': [
+      { edition: "Voidheart Edition", price: 11.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2018': [
+      { edition: "Voidheart Edition", price: 14.99, currency: '$' },
+    ],
+  },
+};
