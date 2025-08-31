5f676fb6bcccbb89850b9bc2691fa9ddcc9fa96c
celso <celso.sjunior@outlook.com>
2025-08-31 19:00:54 -0300

    Criar Youtube.ts

---
+++ 2k38/cycle/src/data/subs/Youtube.ts
@@ -0,0 +1,29 @@
+export const youtube = {
+  launchDate: '2018-06-18',
+  Brazil: {
+    'May 2025': [
+      { price: 53.90, increase: 28.6, currency: 'R$', exchangeTax: 2.79,
+        edition: "Familiar Monthly" },
+      { price: 26.90, increase: 8, currency: 'R$', exchangeTax: 2.79,
+        edition: "Standard Monthly" },
+      { price: 16.90, increase: 8, currency: 'R$', exchangeTax: 2.50, exchangeTax2: 5.5, realPrice: 109.94,
+        edition: "Student Monthly" },
+      ],
+    'June 2018': [
+      { price: 41.90, currency: 'R$', exchangeTax: 2.79,
+        edition: "Familiar Monthly" },
+      { price: 24.90, currency: 'R$', exchangeTax: 2.79,
+        edition: "Standard Monthly" },
+      { price: 13.90, currency: 'R$', exchangeTax: 2.50, exchangeTax2: 5.5, realPrice: 109.94,
+        edition: "Student Monthly" },
+      ],
+      },
+  UnitedStates: {
+    'June 2018': [
+      { price: 11.99, currency: '$', 
+        edition: "Monthly" },
+      { price: 9.99, currency: '$', 
+        edition: "Monthly" },
+      ],
+      },
+    }
