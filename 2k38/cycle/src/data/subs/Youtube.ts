cd6ebff6f6bb76bb76844c5ddc9968f808fffe90
celso <celso.sjunior@outlook.com>
2025-08-31 19:22:20 -0300

    Atualizar o Youtube.ts

--- 2k38/cycle/src/data/subs/Youtube.ts
+++ 2k38/cycle/src/data/subs/Youtube.ts
@@ -2,28 +2,52 @@ export const youtube = {
   launchDate: '2018-06-18',
   Brazil: {
     'May 2025': [
-      { price: 53.90, increase: 28.6, currency: 'R$', exchangeTax: 2.79,
+      { price: 53.90, increase: 28.6, totalIncrease: 63, currency: 'R$', exchangeTax: 2.34,
         edition: "Familiar Monthly" },
-      { price: 26.90, increase: 8, currency: 'R$', exchangeTax: 2.79,
+      { price: 26.90, increase: 8, totalIncrease: 30, currency: 'R$', exchangeTax: 1.92,
         edition: "Standard Monthly" },
-      { price: 16.90, increase: 8, currency: 'R$', exchangeTax: 2.50, exchangeTax2: 5.5, realPrice: 109.94,
+      { price: 16.90, increase: 8, currency: 'R$', exchangeTax: 2.11,
         edition: "Student Monthly" },
       ],
-    'June 2018': [
-      { price: 41.90, currency: 'R$', exchangeTax: 2.79,
+    'August 2023': [
+      { price: 41.90, increase: 30, currency: 'R$', exchangeTax: 1.82,
         edition: "Familiar Monthly" },
-      { price: 24.90, currency: 'R$', exchangeTax: 2.79,
+      { price: 24.90, increase: 20, currency: 'R$', exchangeTax: 1.77,
         edition: "Standard Monthly" },
-      { price: 13.90, currency: 'R$', exchangeTax: 2.50, exchangeTax2: 5.5, realPrice: 109.94,
+      { price: 13.90, currency: 'R$', exchangeTax: 1.73, 
         edition: "Student Monthly" },
+      ],
+    'September 2018': [
+      { price: 31.90, currency: 'R$', exchangeTax: 1.77,
+        edition: "Familiar Monthly" },
+      { price: 20.90, currency: 'R$', exchangeTax: 1.74,
+        edition: "Standard Monthly" },
       ],
       },
   UnitedStates: {
+    'July 2023': [
+      { price: 13.99, currency: '$', 
+        edition: "Standard Monthly" },
+      { price: 7.99, currency: '$', 
+        edition: "Student Monthly" },
+      ],
+    'October 2022': [
+      { price: 22.99, currency: '$', 
+        edition: "Familiar Monthly" },
+      ],
+    'November 2018': [
+      { price: 6.99, currency: '$', 
+        edition: "Student Monthly" },
+      ],
     'June 2018': [
+      { price: 17.99, currency: '$', 
+        edition: "Familiar Monthly" },
+      { price: 14.99, currency: '$', 
+        edition: "Familiar Monthly" },
       { price: 11.99, currency: '$', 
-        edition: "Monthly" },
+        edition: "Standard Monthly" },
       { price: 9.99, currency: '$', 
-        edition: "Monthly" },
+        edition: "Standard Monthly" },
       ],
       },
     }
