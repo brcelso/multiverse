0d292e802ca9405546cc2f0f5d10f6b8ea7ec485
celso <celso.sjunior@outlook.com>
2025-08-31 16:52:57 -0300

    Criar Disney+.ts

---
+++ 2k38/cycle/src/data/subs/Disney+.ts
@@ -0,0 +1,39 @@
+Disney+
+
+# Premium
+
+[Brazil] 
+R$19,90 March 2025 [-68,5%] 4 months discount
+R$527,90 February 2025 - June 2024 [-30%] [R$43,99] [R$62,90] [R$754,80] (annual discount = -30%)
+R$559,90 December 2023 [R$55,90] [R$559,90] [R$670,80]
+R$333,90 May 2023 [R$27,82] [R$33,90] [R$406,80]
+R$279,90 2022 [R$27,90]
+R$279,90 November 2020 [R$23,32] [R$27,90]
+R$237,90 November 2020 [R$19,82] [R$23,79]
+
+[UnitedStates] 
+$159.99 June 2024 [$13.33] [$15.99] [$191.88]
+$139.99 October 2023 [$11.66] [$13.99] [$167.88]
+$109,99 September 2022 [$9.16] [$10.99] [$131.88]
+$79.99 March 2021 [$6.66] [$7.99] [$95.88]
+$69.99 November 2020 [$83.88]
+$69.99 November 2019 [$5.83] [$6.99] 
+
+# Standard Ads Free 
+
+[Brazil]
+R$14,90 March 2025 [-68,5%] 4 months discount
+R$368,90 June 2024 [-30%] [R$30,74] [R$43,90] [R$526,80] (annual discount = -30%)
+
+# Standard w/ Ads
+
+[Brazil]
+R$9,90 March 2025 [-68,5%] 4 months discount
+R$27,99 March 2025 [monthly]
+
+[UnitedStates]
+
+$9.99 monthly October 2024 
+$7.99 monthly 2022 New Plan
+
+
