b063b24b1f850dcd28e2279b4e009db2d88adca2
celso <celso.sjunior@outlook.com>
2025-09-01 15:06:07 -0300

    Atualizar o vwnivus.ts

--- 2k38/cycle/src/data/cars/vwnivus.ts
+++ 2k38/cycle/src/data/cars/vwnivus.ts
@@ -2,11 +2,11 @@ export const vwNivus = {
   launchDate: '2020-06-25',
   Argentina: {
     'July 2025': [
-  { edition: 'Outfit 200TSI', price: 40.488.540, increase: 8.2, currency: '$' },
-  { edition: 'HighLine 200TSI AT', price: 39.579.200, increase: 8.1, totalIncrease: 1816, currency: '$' },
-  { edition: 'ComfortLine 200TSI AT', price: 36.655.200, increase: 8.1, totalIncrease: 1931, currency: '$' },
-  { edition: 'Trendline 200TSI AT', price: 34.646.000, increase: 8.1, currency: '$' },
-  { edition: '170 TSI MT', price: 32.639.100, increase: 8.1, currency: '$' },
+  { edition: 'Outfit 200TSI', price: 40488.540, increase: 8.2, currency: '$' },
+  { edition: 'HighLine 200TSI AT', price: 35579.200, increase: 8.1, totalIncrease: 1816, currency: '$' },
+  { edition: 'ComfortLine 200TSI AT', price: 36655.200, increase: 8.1, totalIncrease: 1931, currency: '$' },
+  { edition: 'Trendline 200TSI AT', price: 34646.000, increase: 8.1, currency: '$' },
+  { edition: '170 TSI MT', price: 32639.100, increase: 8.1, currency: '$' },
     ],
   },
   Brazil: {
