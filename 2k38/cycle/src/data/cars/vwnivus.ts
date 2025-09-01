37c7cf5fd1bd2f920a9fd46f3096c7462b420a43
celso <celso.sjunior@outlook.com>
2025-09-01 14:46:00 -0300

    Atualizar o vwnivus.ts

--- 2k38/cycle/src/data/cars/vwnivus.ts
+++ 2k38/cycle/src/data/cars/vwnivus.ts
@@ -1,21 +1,33 @@
 export const vwNivus = {
   launchDate: '2020-06-25',
   Argentina: {
-    'July 2025': [
-      { edition: 'Outfit 200TSI', price: 40.488.540, increase: 8.2, currency: '$' },
-      { edition: 'HighLine 200TSI AT', price: 39.579.200, increase: 8.1, currency: '$' },
-      { edition: 'ComfortLine TSI200 AT', price: 36.655.200, increase: 8.1, currency: '$' },
-      { edition: 'Trendline 200TSI AT', price: 34.646.000, increase: 8.1, currency: '$' },
-      { edition: '170 TSI MT', price: 32.639.100, increase: 8.1, currency: '$' },
-    ],
-    'February 2025': [
-      { edition: 'Outfit 200TSI', price: 37.431.300, currency: '$' },
-      { edition: 'HighLine 200TSI AT', price: 36.596.900, currency: '$' },
-      { edition: 'ComfortLine TSI200 AT', price: 33.893.300, currency: '$' },
-      { edition: 'Trendline 200TSI AT', price: 32.035.500, currency: '$' },
-      { edition: '170 TSI MT', price: 30.179.700, currency: '$' },
-    ],
-  },
+Argentina: {
+  'July 2025': [
+    { edition: 'Outfit 200TSI', price: 40.488.540, increase: 8.2, currency: '$' },
+    { edition: 'HighLine 200TSI AT', price: 39.579.200, increase: 8.1, currency: '$' },
+    { edition: 'ComfortLine 200TSI AT', price: 36.655.200, increase: 8.1, currency: '$' },
+    { edition: 'Trendline 200TSI AT', price: 34.646.000, increase: 8.1, currency: '$' },
+    { edition: '170 TSI MT', price: 32.639.100, increase: 8.1, currency: '$' },
+  ],
+  'February 2025': [
+    { edition: 'Outfit 200TSI', price: 37.431.300, currency: '$' },
+    { edition: 'HighLine 200TSI AT', price: 36.596.900, currency: '$' },
+    { edition: 'ComfortLine 200TSI AT', price: 33.893.300, currency: '$' },
+    { edition: 'Trendline 200TSI AT', price: 32.035.500, currency: '$' },
+    { edition: '170 TSI MT', price: 30.179.700, currency: '$' },
+  ],
+  'January 2022': [
+    { edition: 'HighLine Hero 200TSI AT', price: 4.708.250, currency: '$' },
+    { edition: 'HighLine 200TSI AT', price: 4.635.250, currency: '$' },
+    { edition: 'ComfortLine 200TSI AT', price: 3.360.700, currency: '$' },
+    { edition: '170 TSI MT', price: 3.149.744, currency: '$' },
+  ],
+  'November 2020': [
+    { edition: 'HighLine Pack Hero 200TSI AT', price: 2.065.500, currency: '$' },
+    { edition: 'HighLine 200TSI AT', price: 2.065.500, currency: '$' },
+    { edition: 'ComfortLine 200TSI AT', price: 1.804.650, currency: '$' },
+  ],
+},
   Brazil: {
     'August 2025': [
       { edition: 'ComfortLine 2025/2026', price: 131900, discount: 8, currency: 'R$', basePrice: 143490 },
