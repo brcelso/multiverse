63964cf507fc9cc27015c0242e693b24a2622be0
celso <celso.sjunior@outlook.com>
2025-08-31 22:35:15 -0300

    Atualizar o TheLastOfUsI.ts

--- 2k38/cycle/src/data/games/TheLastOfUsI.ts
+++ 2k38/cycle/src/data/games/TheLastOfUsI.ts
@@ -2,21 +2,31 @@ export const theLastOfUs = {
   launchDate: '2013-06-01',
 
   Brazil: {
+    'November 2024': [
+      { edition: 'Remastered PS4', price: 23.97, discount: 76, totalDiscount: 87, currency: '$', exchangeTax2: 2.39 },
+    ],
+    'November 2015': [
+      { edition: 'Remastered PS4', price: 99.50, discount: 45, currency: 'R$' },
+    ],
     'July 2014': [
-      { edition: 'Remastered', price: 149.90, currency: 'R$' },
+      { edition: 'Remastered PS4', price: 179.90, currency: 'R$', exchangeTax: 3.59 },
     ],
     'June 2013': [
-      { edition: 'Physical Media', price: 149.90, currency: 'R$' },
-      { edition: 'Digital Edition', price: 119.90, currency: 'R$' },
+      { edition: 'Physical Media', price: 149.90, currency: 'R$', exchangeTax: 2.49},
+      { edition: 'Digital Edition', price: 119.90, currency: 'R$', exchangeTax2: 2.00 },
     ],
   },
 
   UnitedStates: {
-    'February 2023': [
-      { edition: 'Remastered PS4', price: 9.99, currency: '$' , discount: 50},
+    'November 2024': [
+      { edition: 'Remastered PS4', price: 9.99, discount: 50, totalDiscount: 80, currency: '$' },
+    ],
+    'November 2015': [
+      { edition: 'Remastered PS4', price: 19.99, discount: 50, currency: '$' },
     ],
     'July 2014': [
-      { edition: 'Remastered PS4', price: 19.99, currency: '$' },
+      { edition: 'Remastered PS4', price: 39.99, discount: 20, currency: '$' },
+      { edition: 'Remastered PS4', price: 49.99, currency: '$' },
     ],
     'June 2013': [
       { edition: 'Survival Edition', price: 79.99, currency: '$' },
