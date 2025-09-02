e6f751b49bceb961fb5f1ae0ea87dae3741dd203
celso <celso.sjunior@outlook.com>
2025-09-01 23:25:35 -0300

    Criar Netflix.ts

---
+++ 2k38/cycle/src/data/subs/Netflix.ts
@@ -0,0 +1,92 @@
+export const netflix = {
+  launchDate: '2011-09-05',
+  Brazil: {
+    'January 2025': [
+      { price: 59.90, increase: 7.1, totalIncrease: 300, currency: 'R$', edition: "Premium Monthly" },
+      { price: 44.90, increase: 12.5, totalIncrease: 60, currency: 'R$', edition: "Standard Monthly" },
+      { price: 20.90, increase: 10.5, totalIncrease: 10, currency: 'R$', edition: "Standard w/Ads Monthly" },
+    ],
+    'July 2021': [
+      { price: 55.90, increase: 22, currency: 'R$', edition: "Premium Monthly" },
+      { price: 39.90, increase: 21, currency: 'R$', edition: "Standard Monthly" },
+      { price: 25.90, increase: 23, currency: 'R$', edition: "Basic Monthly" },
+    ],
+    'March 2019': [
+      { price: 45.90, increase: 21, currency: 'R$', edition: "Premium Monthly" },
+      { price: 32.90, increase: 18, currency: 'R$', edition: "Standard Monthly" },
+      { price: 21.90, increase: 10, currency: 'R$', edition: "Basic Monthly" },
+    ],
+    'July 2018': [
+      { price: 37.90, increase: 124, currency: 'R$', edition: "Premium Monthly [Ultra]" },
+      { price: 27.90, increase: 40, currency: 'R$', edition: "Standard Monthly" },
+      { price: 19.90, increase: 18, currency: 'R$', edition: "Basic Monthly" },
+    ],
+    'August 2013': [
+      { price: 16.90, increase: 13, currency: 'R$', edition: "Premium Monthly" },
+    ],
+    'September 2011': [
+      { price: 14.99, currency: 'R$', edition: "Premium Monthly" },
+    ],
+  },
+  UnitedStates: {
+    'January 2025': [
+      { price: 24.99, increase: 8.7, totalIncrease: 213, currency: '$', edition: "Premium Monthly" },
+      { price: 17.99, increase: 12.5, totalIncrease: 150, currency: '$', edition: "Standard Monthly" },
+      { price: 7.99, increase: 14.3, totalIncrease: 0, currency: '$', edition: "Standard w/Ads Monthly" },
+    ],
+    'October 2023': [
+      { price: 22.99, increase: 76.9, currency: '$', edition: "Premium Monthly" },
+      { price: 15.99, increase: 60, currency: '$', edition: "Standard Monthly" },
+      { price: 6.99, currency: '$', edition: "Standard w/Ads Monthly" },
+    ],
+    '2019': [
+      { price: 12.99, increase: 62.6, currency: '$', edition: "Standard Monthly" },
+    ],
+    'September 2011': [
+      { price: 7.99, currency: '$', edition: "Premium Monthly" },
+    ],
+  },
+  Europe: {
+    'October 2023': [
+      { price: 19.99, increase: 186, currency: '€', edition: "Premium Monthly" },
+      { price: 5.99, currency: '€', edition: "Standard w/Ads Monthly" },
+    ],
+    'January 2012': [
+      { price: 6.99, currency: '€', edition: "Premium Monthly" },
+    ],
+  },
+  UK: {
+    'February 2025': [
+      { price: 18.99, increase: 5.5, totalIncrease: 217, currency: '£', edition: "Premium Monthly" },
+      { price: 12.99, increase: 18.2, totalIncrease: 62, currency: '£', edition: "Standard Monthly" },
+      { price: 5.99, increase: 20, totalIncrease: 0, currency: '£', edition: "Standard w/Ads Monthly" },
+    ],
+    'October 2023': [
+      { price: 17.99, increase: 28.6, currency: '£', edition: "Premium Monthly" },
+      { price: 10.99, increase: 10, currency: '£', edition: "Standard Monthly" },
+      { price: 4.99, currency: '£', edition: "Standard w/Ads Monthly" },
+    ],
+    '2021': [
+      { price: 13.99, increase: 16.7, currency: '£', edition: "Premium Monthly" },
+      { price: 9.99, increase: 11.1, currency: '£', edition: "Standard Monthly" },
+      { price: 5.99, currency: '£', edition: "Basic Monthly" },
+    ],
+    '2019': [
+      { price: 11.99, increase: 20, currency: '£', edition: "Premium Monthly" },
+      { price: 8.99, increase: 12.5, currency: '£', edition: "Standard Monthly" },
+    ],
+    '2017': [
+      { price: 9.99, increase: 33.6, currency: '£', edition: "Premium Monthly" },
+      { price: 7.99, increase: 14.3, currency: '£', edition: "Standard Monthly" },
+    ],
+    '2016': [
+      { price: 7.49, increase: 7.1, currency: '£', edition: "Premium Monthly" },
+    ],
+    '2014': [
+      { price: 6.99, currency: '£', edition: "Premium Monthly" },
+    ],
+    'January 2012': [
+      { price: 5.99, currency: '£', edition: "Premium Monthly" },
+    ],
+  },
+};
