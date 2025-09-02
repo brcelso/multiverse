6bfda794ddb76bccee4897e92e1aeb8ef38fa2d3
celso <celso.sjunior@outlook.com>
2025-09-02 20:50:08 -0300

    Atualizar o App.tsx

--- 2k38/cycle/src/App.tsx
+++ 2k38/cycle/src/App.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import { GameSelector } from './components/GameSelector';
+import CardGame from './components/CardGame';
+import DynamicPriceChart from './components/DynamicPriceChart';
+import { Games } from './data';
 import './App.css';
 
+const gameNames = Object.keys(Games);
+
 function App() {
+  const [selectedGame, setSelectedGame] = useState(gameNames[0]);
+
   return (
     <div className="app-container">
-      <GameSelector />
+      {/* Seletor de jogo */}
+      <GameSelector 
+        selectedGame={selectedGame} 
+        setSelectedGame={setSelectedGame} 
+        gameOptions={gameNames}
+      />
+
+      {/* CardGame: resumo do jogo */}
+      <CardGame game={Games[selectedGame]} />
+
+      {/* Gráfico dinâmico */}
+      <div style={{ marginTop: '2rem' }}>
+        <DynamicPriceChart gameData={Games[selectedGame]} />
+      </div>
     </div>
   );
 }
