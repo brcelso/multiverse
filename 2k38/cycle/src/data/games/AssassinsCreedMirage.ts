1a592e2ec48e15f5fea7a4d82ced8a6e2ee86388
celso <celso.sjunior@outlook.com>
2025-09-03 18:13:10 -0300

    Atualizar o AssassinsCreedMirage.ts

--- 2k38/cycle/src/data/games/AssassinsCreedMirage.ts
+++ 2k38/cycle/src/data/games/AssassinsCreedMirage.ts
@@ -4,13 +4,13 @@ export const assassinsCreedMirage = {
   Brazil: {
     'June 2025': [
       { edition: 'Master Assassin Edition', price: 171.96, discount: 60, currency: 'R$', exchangeTax: 4.77 },
-      { edition: 'Package Edition', price: 117.47, discount: 75, currency: '$' },
+      { edition: 'Bundle Edition', price: 117.47, discount: 75, currency: '$' },
       { edition: 'Deluxe Edition', price: 89.97, discount: 70, currency: 'R$', exchangeTax: 4.79 },
       { edition: 'Standard Edition', price: 95.96, discount: 60, currency: 'R$', exchangeTax: 4.79 },
     ],
     'May 2023': [
       { edition: 'Master Assassin Edition', price: 429.90, currency: 'R$', exchangeTax: 4.77 },
-      { edition: 'Package Edition', price: 0, currency: '$' },
+      { edition: 'Bundle Edition', price: 469.90, currency: 'R$' },
       { edition: 'Deluxe Edition', price: 299.90, currency: 'R$', exchangeTax: 4.79 },
       { edition: 'Standard Edition', price: 239.90, currency: 'R$', exchangeTax: 4.79 },
     ],
@@ -18,10 +18,11 @@ export const assassinsCreedMirage = {
   UnitedStates: {
     'June 2025': [
       { edition: 'Master Assassin Edition', price: 35.99, discount: 60, currency: '$' },
+      { edition: 'Bundle Edition', price: 24.99, discount: 75, currency: '$' },
       { edition: 'Standard Edition', price: 19.99, discount: 60, currency: '$' },
     ],
     'May 2023': [
-      { edition: 'Package Edition', price: 0, currency: '$' },
+      { edition: 'Bundle Edition', price: 99.99, currency: '$' },
       { edition: 'Master Assassin Edition', price: 89.99, currency: '$' },
       { edition: 'Deluxe Edition', price: 0, currency: '$' },
       { edition: 'Standard Edition', price: 49.99, currency: '$' },
