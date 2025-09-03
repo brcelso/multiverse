7db216a70d5163609d782469b51d2f65bb109049
celso <celso.sjunior@outlook.com>
2025-09-03 18:03:54 -0300

    Atualizar o NFL2k26.ts

--- 2k38/cycle/src/data/games/NFL2k26.ts
+++ 2k38/cycle/src/data/games/NFL2k26.ts
@@ -2,6 +2,10 @@ export const nflCollegeFootball2026 = {
   launchDate: '2025-07-10',
 
   Brazil: {
+    'September 2025': [
+      { edition: 'MVP Bundle', price: 599.60, currency: 'R$', discount: 20, exchangeTax2: 4.99 },
+      { edition: 'Deluxe', price: 374.62, currency: 'R$', discount: 25, exchangeTax2: 5 },
+    ],
     'July 2025': [
       { edition: 'Deluxe', price: 569.90, increase: 14, currency: 'R$', exchangeTax2: 5.69 },
       { edition: 'Standard', price: 399.90, increase: 14.3, currency: 'R$', exchangeTax2: 5.71 },
@@ -11,6 +15,10 @@ export const nflCollegeFootball2026 = {
     ],
   },
 Europe: {
+    'September 2025': [
+      { edition: 'MVP Bundle', price: 127.99, discount: 20, currency: '€' },
+      { edition: 'Deluxe', price: 82.49, discount: 25, currency: '€' },
+    ],
     'July 2025': [
       { edition: 'MVP Bundle', price: 159.99, currency: '€' },
       { edition: 'Deluxe', price: 109.99, currency: '€' },
@@ -18,6 +26,10 @@ Europe: {
     ],
   },
   UnitedKingdom: {
+    'September 2025': [
+      { edition: 'MVP Bundle', price: 111.99, discount: 20, currency: '£' },
+      { edition: 'Deluxe', price: 74.99, discount: 25, currency: '£' },
+    ],
     'July 2025': [
       { edition: 'MVP Bundle', price: 139.99, currency: '£' },
       { edition: 'Deluxe', price: 99.99, currency: '£' },
@@ -25,6 +37,10 @@ Europe: {
     ],
   },
   UnitedStates: {
+    'September 2025': [
+      { edition: 'MVP Bundle', price: 119.99, discount: 20, currency: '$' },
+      { edition: 'Deluxe', price: 74.99, discount: 25, currency: '$' },
+    ],
     'July 2025': [
       { edition: 'MVP Bundle', price: 149.99, currency: '$' },
       { edition: 'Deluxe', price: 99.99, currency: '$' },
