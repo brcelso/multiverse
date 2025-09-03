599391d2c6a134f5de014381a16e0bbc5c545377
celso <celso.sjunior@outlook.com>
2025-09-03 17:08:44 -0300

    Criar 007FirstLight.ts

---
+++ 2k38/cycle/src/data/games/007FirstLight.ts
@@ -0,0 +1,30 @@
+export const 007firstlight = {
+  launchDate: '2026-03-26',
+  Brazil: {
+    'March 2026': [
+      { edition: "Deluxe Edition", price: 0, currency: 'R$', exchangeTax2: 0 },
+      { edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.28 },
+    ],
+  },
+  Europe: {
+    'March 2026': [
+      { edition: "Collector's Edition", price: 299.99, currency: '€' },
+      { edition: "Deluxe Edition", price: 79.99, currency: '€' },
+      { edition: "Standard Edition", price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'March 2026': [
+      { edition: "Collector's Edition", price: 259.99, currency: '£' },
+      { edition: "Deluxe Edition", price: 69.99, currency: '£' },
+      { edition: "Standard Edition", price: 59.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'March 2026': [
+      { edition: "Collector's Edition", price: 299.99, currency: '$' },
+      { edition: "Deluxe Edition", price: 79.99, currency: '$' },
+      { edition: "Standard Edition", price: 69.99, currency: '$' },
+    ],
+  },
+};
