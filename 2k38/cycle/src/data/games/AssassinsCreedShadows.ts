9e66b11c5b9f8ed24b9213f2921e2f0a052843c8
celso <celso.sjunior@outlook.com>
2025-09-05 18:44:44 -0300

    Atualizar o AssassinsCreedShadows.ts

--- 2k38/cycle/src/data/games/AssassinsCreedShadows.ts
+++ 2k38/cycle/src/data/games/AssassinsCreedShadows.ts
@@ -2,6 +2,10 @@ export const assassinsCreedShadows = {
   launchDate: '2025-03-20',
 
   Brazil: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 301.43, currency: 'R$', discount: 33, exchangeTax: 4.99 },
+      { edition: 'Standard Edition', price: 234.43, currency: 'R$', discount: 33, exchangeTax: 4.98 },
+    ],
     'July 2025': [
       { edition: 'Deluxe Edition', price: 337.42, currency: 'R$', discount: 25, exchangeTax: 4.99 },
       { edition: 'Standard Edition', price: 262.42, currency: 'R$', discount: 25, exchangeTax: 4.98 },
@@ -15,25 +19,35 @@ export const assassinsCreedShadows = {
       { edition: 'Standard Edition', price: 349.9, currency: 'R$', exchangeTax: 4.98 },
     ],
   },
-
+  Europe: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 66.99, discount: 33, currency: '€' },
+      { edition: 'Standard Edition', price: 53.59, discount: 33, currency: '€' },
+    ],
+    'March 2025': [
+      { edition: 'Deluxe Edition', price: 99.99, currency: '€' },
+      { edition: 'Standard Edition', price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 56.94, discount: 33, currency: '£' },
+      { edition: 'Standard Edition', price: 46.89, discount: 33, currency: '£' },
+    ],
+    'March 2025': [
+      { edition: 'Deluxe Edition', price: 84.99, currency: '£' },
+      { edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
   UnitedStates: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 60.29, discount: 33, currency: '$' },
+      { edition: 'Standard Edition', price: 46.89, discount: 33, currency: '$' },
+    ],
     'March 2025': [
       { edition: 'Deluxe Edition', price: 89.99, currency: '$' },
       { edition: 'Standard Edition', price: 69.99, currency: '$' },
     ],
   },
 
-  Europe: {
-    'March 2025': [
-      { edition: 'Deluxe Edition', price: 99.99, currency: '€' },
-      { edition: 'Standard Edition', price: 79.99, currency: '€' },
-    ],
-  },
-
-  UnitedKingdom: {
-    'March 2025': [
-      { edition: 'Deluxe Edition', price: 84.99, currency: '£' },
-      { edition: 'Standard Edition', price: 69.99, currency: '£' },
-    ],
-  },
 };
