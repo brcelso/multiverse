a58595e9f043600dbccf707ca97c9a1158160461
celso <celso.sjunior@outlook.com>
2025-09-05 14:40:29 -0300

    Atualizar o LostSoulAside.ts

--- 2k38/cycle/src/data/games/LostSoulAside.ts
+++ 2k38/cycle/src/data/games/LostSoulAside.ts
@@ -7,8 +7,8 @@ export const lostSoulAside = {
       { edition: 'Standard Edition', price: 339.90, increase: 13.5, currency: 'R$', exchangeTax: 4.99 },
     ],
     'August 2025': [
-      { edition: 'Deluxe Edition', price: 349.9, discount: 12, currency: 'R$', exchangeTax: 4.99 },
-      { edition: 'Standard Edition', price: 299.9, discount: 12, currency: 'R$', exchangeTax: 4.99 },
+      { edition: 'Deluxe Edition', price: 349.90, discount: 12, currency: 'R$', exchangeTax: 4.99 },
+      { edition: 'Standard Edition', price: 299.9p, discount: 12, currency: 'R$', exchangeTax: 4.99 },
     ],
     'June 2025': [
       { edition: "Deluxe Edition", price: 399.90, increase: 14.5, currency: 'R$', exchangeTax: 5.71 },
@@ -16,8 +16,8 @@ export const lostSoulAside = {
       { edition: 'Standard Edition', price: 215.93, discount: 28, currency: 'R$', exchangeTax: 4.85 },
     ],
     'May 2025': [
-      { edition: 'Deluxe Edition', price: 349.9, currency: 'R$', exchangeTax: 4.99 },
-      { edition: 'Standard Edition', price: 299.9, currency: 'R$', exchangeTax: 4.99 },
+      { edition: 'Deluxe Edition', price: 349.90, currency: 'R$', exchangeTax: 4.99 },
+      { edition: 'Standard Edition', price: 299.90, currency: 'R$', exchangeTax: 4.99 },
     ],
     'February 2025': [
       { edition: 'Standard Edition', price: 208.28, discount: 30.5, currency: 'R$', exchangeTax: 4.99 },
