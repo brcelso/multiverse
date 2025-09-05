753189f9f6f40c13e0d6d83574c9e90ecb2a7195
celso <celso.sjunior@outlook.com>
2025-09-05 17:07:36 -0300

    Criar IndianaJones.ts

---
+++ 2k38/cycle/src/data/games/IndianaJones.ts
@@ -0,0 +1,43 @@
+export const indianajones = {
+  launchDate: '2025-04-16',
+  Brazil: {
+    'September 2025': [
+      { edition: "The Order Of Giants", price: 99.90, currency: 'R$', exchangeTax: 5 },
+      { edition: "Standard Edition", price: 279.92, discount: 20, currency: 'R$', exchangeTax: 5.69 },
+    ],
+    'April 2025': [
+      { edition: "Premium Edition", price: 499.50, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Standard Edition", price: 499.50, currency: 'R$', exchangeTax: 4.99 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: "The Order Of Giants", price: 19.99, currency: '€' },
+      { edition: "Standard Edition", price: 63.99, discount: 20,  currency: '€' },
+    ],
+    'April 2025': [
+      { edition: "Premium Edition", price: 109.99, currency: '€' },
+      { edition: "Standard Edition", price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: "The Order Of Giants", price: 17.99, currency: '£' },
+      { edition: "Standard Edition", price: 55.99, discount: 20, currency: '£' },
+    ],
+    'April 2025': [
+      { edition: "Premium Edition", price: 99.99, currency: '£' },
+      { edition: "Standard Edition", price: 69.99, discount: 20, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: "The Order Of Giants", price: 19.99, currency: '$' },
+      { edition: "Standard Edition", price: 55.99, discount: 20, currency: '$' },
+    ],
+    'April 2025': [
+      { edition: "Premium Edition", price: 99.99, currency: '$' },
+      { edition: "Standard Edition", price: 69.99, currency: '$' },
+    ],
+  },
+};
