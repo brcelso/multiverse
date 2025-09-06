1d8008c9cf0cfc585685273a0c1f31c37bc37040
celso <celso.sjunior@outlook.com>
2025-09-05 23:06:16 -0300

    Criar Mafia.ts

---
+++ 2k38/cycle/src/data/games/Mafia.ts
@@ -0,0 +1,27 @@
+export const mafia = {
+  launchDate: '2025-08-08',
+  Brazil: {
+    'August 2025': [
+      { edition: "Deluxe Edition", price: 349.90, currency: 'R$', exchangeTax2: 4.99 },
+      { edition: "Standard Edition", price: 289.90, currency: 'R$', exchangeTax2: 5.79 },
+    ],
+  },
+  Europe: {
+    'August 2025': [
+      { edition: "Deluxe Edition", price: 59.90, currency: '€' },
+      { edition: "Standard Edition", price: 49.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'August 2025': [
+      { edition: "Deluxe Edition", price: 54.99, currency: '£' },
+      { edition: "Standard Edition", price: 44.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'August 2025': [
+      { edition: "Deluxe Edition", price: 59.99, currency: '$' },
+      { edition: "Standard Edition", price: 49.99, currency: '$' },
+    ],
+  },
+};
