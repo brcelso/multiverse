ec2108bcde83911bba7ca05caa67ca3fb68ea8c5
celso <celso.sjunior@outlook.com>
2025-09-06 22:07:11 -0300

    Atualizar o AstroBot.ts

--- 2k38/cycle/src/data/games/AstroBot.ts
+++ 2k38/cycle/src/data/games/AstroBot.ts
@@ -1,18 +1,39 @@
 export const astroBot = {
-  launchDate: '2025-02-28',
-
+  launchDate: '2024-09-06',
   Brazil: {
+    'September 2025': [
+      { edition: 'Standard Edition', price: 235.42, currency: 'R$', discount: 30 },
+      { edition: 'Standard Edition', price: 235.42, currency: 'R$', discount: 21.5 },
+    ],
+    'June 2025': [
+      { edition: 'Deluxe Edition', price: 399.90, currency: 'R$' },
+      { edition: 'Standard Edition', price: 339.90, currency: 'R$' 
+      { edition: 'Standard Edition', price: 166.50, currency: 'R$', discount: 44.5 },
+    ],
     'February 2025': [
-      { edition: 'Deluxe Edition', price: 248.91, currency: 'R$', discount: 17 },
       { edition: 'Deluxe Edition', price: 297.41, currency: 'R$', discount: 15 },
-      { edition: 'Standard Edition', price: 349.9, currency: 'R$' },
-      { edition: 'Standard Edition', price: 299.9, currency: 'R$' },
+      { edition: 'Standard Edition', price: 248.91, currency: 'R$', discount: 17 },
+      { edition: 'Deluxe Edition', price: 349.90, currency: 'R$' },
+      { edition: 'Standard Edition', price: 299.90, currency: 'R$' },
     ],
   },
-
+  Europe: {
+    'September 2024': [
+      { edition: 'Deluxe Edition', price: 79.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  UnitedKingdom: {
+    'September 2024': [
+      { edition: 'Deluxe Edition', price: 69.99, currency: '$' },
+      { edition: 'Standard Edition', price: 59.99, currency: '$' },
+    ],
+  },
   UnitedStates: {
     'February 2025': [
       { edition: 'Standard Edition', price: 49.79, currency: '$', discount: 17 },
+    ],
+    'September 2024': [
+      { edition: 'Deluxe Edition', price: 69.99, currency: '$' },
       { edition: 'Standard Edition', price: 59.99, currency: '$' },
     ],
   },
