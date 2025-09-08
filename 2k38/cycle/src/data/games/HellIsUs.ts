2ed4e66442e6279939b0befaae8bf272d463f1c6
celso <celso.sjunior@outlook.com>
2025-09-08 18:46:39 -0300

    Atualizar o HellIsUs.ts

--- 2k38/cycle/src/data/games/HellIsUs.ts
+++ 2k38/cycle/src/data/games/HellIsUs.ts
@@ -1,33 +1,34 @@
 export const hellisus = {
   launchDate: '2025-09-04',
   Brazil: {
-    'September 2025': [
-      { edition: "Deluxe Edition", price: 297.44, currency: 'R$', exchangeTax2: 0 },
-      { edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax2: 0 },
-      { edition: "Standard Edition", price: 222.44, currency: 'R$', exchangeTax2: 4.99 },
-      { edition: "Standard Edition", price: 339.90, currency: 'R$', exchangeTax2: 4.28 },
+    'August 2025': [
+      { edition: "Deluxe Edition", price: 297.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.71 },
+      { edition: "Standard Edition", price: 222.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.70 },
+    ],
+    'June 2025': [
+      { edition: "Standard Edition", price: 339.90, increase: 13.34, currency: 'R$', exchangeTax2: 5.66 },
     ],
     'February 2025': [
-      { edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax2: 0 },
-      { edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.28 },
+      { edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax2: 5.69 },
+      { edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.99 },
     ],
   },
   Europe: {
     'September 2025': [
       { edition: "Deluxe Edition", price: 79.99, currency: '€' },
-      { edition: "Standard Edition", price: 69.99, currency: '€' },
+      { edition: "Standard Edition", price: 59.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'September 2025': [
-      { edition: "Deluxe Edition", price: 69.99, currency: '£' },
-      { edition: "Standard Edition", price: 59.99, currency: '£' },
+      { edition: "Deluxe Edition", price: 64.99, currency: '£' },
+      { edition: "Standard Edition", price: 49.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'September 2025': [
       { edition: "Deluxe Edition", price: 79.99, currency: '$' },
-      { edition: "Standard Edition", price: 69.99, currency: '$' },
+      { edition: "Standard Edition", price: 59.99, currency: '$' },
     ],
   },
 };
