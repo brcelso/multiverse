c8579b8671f80f5142e7f7e0f3bc1d0a48845f4b
celso <celso.sjunior@outlook.com>
2025-09-08 02:32:25 -0300

    Atualizar o UntilDawn.ts

--- 2k38/cycle/src/data/games/UntilDawn.ts
+++ 2k38/cycle/src/data/games/UntilDawn.ts
@@ -1,10 +1,24 @@
 export const untilDawn = {
-  launchDate: '2024-04-24',
-
+  launchDate: '2024-10-04',
   Brazil: {
     'April 2024': [
       { edition: 'Standard Edition', price: 224.92, currency: 'R$', discount: 25 },
-      { edition: 'Standard Edition', price: 299.90, currency: 'R$' },
+      { edition: 'Standard Edition', price: 299.90, currency: 'R$', exchangeTax2: 4.99 },
+    ],
+  },
+  Europe: {
+    'April 2024': [
+      { edition: 'Standard Edition', price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'April 2024': [
+      { edition: 'Standard Edition', price: 59.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'April 2024': [
+      { edition: 'Standard Edition', price: 59.99, currency: '$' },
     ],
   },
 };
