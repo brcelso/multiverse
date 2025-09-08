f0cc18e9b176221e5c9b92479aafb329449ad64a
celso <celso.sjunior@outlook.com>
2025-09-08 17:51:39 -0300

    Criar MonsterHunterWilds.ts

---
+++ 2k38/cycle/src/data/games/MonsterHunterWilds.ts
@@ -0,0 +1,28 @@
+export const monsterhunter = {
+  launchDate: '2025-02-28',
+  Brazil: {
+    'February 2025': [
+      { edition: "Deluxe Edition", price: 0, currency: 'R$', exchangeTax2: 0 },
+      { edition: "Standard Edition", price: 110.40, currency: 'R$', exchangeTax2: 4.99 },
+      { edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.28 },
+    ],
+  },
+  Europe: {
+    'February 2025': [
+      { edition: "Deluxe Edition", price: 79.99, currency: '€' },
+      { edition: "Standard Edition", price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'February 2025': [
+      { edition: "Deluxe Edition", price: 69.99, currency: '£' },
+      { edition: "Standard Edition", price: 59.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'February 2025': [
+      { edition: "Deluxe Edition", price: 79.99, currency: '$' },
+      { edition: "Standard Edition", price: 69.99, currency: '$' },
+    ],
+  },
+};
