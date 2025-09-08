d0078fea410dd40bb37dee1d36933b3990a40f1c
celso <celso.sjunior@outlook.com>
2025-09-08 19:12:19 -0300

    Atualizar o HellIsUs.ts

--- 2k38/cycle/src/data/games/HellIsUs.ts
+++ 2k38/cycle/src/data/games/HellIsUs.ts
@@ -2,21 +2,21 @@ export const hellisus = {
   launchDate: '2025-09-04',
   Brazil: {
     'September 2025': [
-      { edition: "Deluxe Edition", price: 455.90, totalIncrease: 53, currency: 'R$', exchangeTax2: 3.71 },
-      { edition: "Standard Edition", price: 339.90, totalIncrease: 52.5, currency: 'R$', exchangeTax2: 3.70 },
+      { platform: "Xbox", edition: "Deluxe Edition", price: 455.90, totalIncrease: 53, currency: 'R$', exchangeTax2: 3.71 },
+      { platform: "Xbox", edition: "Standard Edition", price: 339.90, totalIncrease: 52.5, currency: 'R$', exchangeTax2: 3.70 },
     ],
     'August 2025': [
-      { edition: "Deluxe Edition", price: 297.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.71 },
-      { edition: "Standard Edition", price: 222.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.70 },
+      { platform: "PS5", edition: "Deluxe Edition", price: 297.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.71 },
+      { platform: "PS5", edition: "Standard Edition", price: 222.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.70 },
     ],
     'June 2025': [
-      { edition: "Standard Edition", price: 339.90, increase: 13.34, currency: 'R$', exchangeTax2: 5.66 },
+      { platform: "PS5", edition: "Standard Edition", price: 339.90, increase: 13.34, currency: 'R$', exchangeTax2: 5.66 },
     ],
     'February 2025': [
-      { edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax2: 5.69 },
-      { edition: "Deluxe Edition", price: 297.44, currency: 'R$', exchangeTax2: 3.71 },
-      { edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.99 },
-      { edition: "Standard Edition", price: 222.44, currency: 'R$', exchangeTax2: 3.70 },
+      { platform: "PS5", edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax2: 5.69 },
+      { platform: "Xbox", edition: "Deluxe Edition", price: 297.44, currency: 'R$', exchangeTax2: 3.71 },
+      { platform: "PS5", edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.99 },
+      { platform: "Xbox", edition: "Standard Edition", price: 222.44, currency: 'R$', exchangeTax2: 3.70 },
     ],
   },
   Europe: {
