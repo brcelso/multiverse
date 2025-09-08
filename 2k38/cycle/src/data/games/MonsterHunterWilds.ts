2dc12c6d0c63a1ae8eaf39c4adc67284686e28e9
celso <celso.sjunior@outlook.com>
2025-09-08 18:22:53 -0300

    Atualizar o MonsterHunterWilds.ts

--- 2k38/cycle/src/data/games/MonsterHunterWilds.ts
+++ 2k38/cycle/src/data/games/MonsterHunterWilds.ts
@@ -1,27 +1,36 @@
 export const monsterhunter = {
   launchDate: '2025-02-28',
   Brazil: {
+    'September 2025': [
+      { edition: "Standard Edition", price: 110.40, discount: 67, currency: 'R$', exchangeTax2: 4.99 },
+    ],
+    'June 2025': [
+      { edition: "Standard Edition", price: 265.52, discount: 20, currency: 'R$', exchangeTax2: 4.74 },
+    ],
     'February 2025': [
-      { edition: "Deluxe Edition", price: 0, currency: 'R$', exchangeTax2: 0 },
-      { edition: "Standard Edition", price: 110.40, currency: 'R$', exchangeTax2: 4.99 },
-      { edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.28 },
+      { edition: "Deluxe Premium Edition", price: 549.50, currency: 'R$', exchangeTax2: 4.99 },
+      { edition: "Deluxe Edition", price: 426.90, currency: 'R$', exchangeTax2: 4.74 },
+      { edition: "Standard Edition", price: 331.90, currency: 'R$', exchangeTax2: 4.74 },
     ],
   },
   Europe: {
     'February 2025': [
-      { edition: "Deluxe Edition", price: 79.99, currency: '€' },
-      { edition: "Standard Edition", price: 69.99, currency: '€' },
+      { edition: "Deluxe Premium Edition", price: 119.99, currency: '€' },
+      { edition: "Deluxe Edition", price: 99.99, currency: '€' },
+      { edition: "Standard Edition", price: 79.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'February 2025': [
-      { edition: "Deluxe Edition", price: 69.99, currency: '£' },
-      { edition: "Standard Edition", price: 59.99, currency: '£' },
+      { edition: "Deluxe Premium Edition", price: 99.99, currency: '£' },
+      { edition: "Deluxe Edition", price: 79.99, currency: '£' },
+      { edition: "Standard Edition", price: 64.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'February 2025': [
-      { edition: "Deluxe Edition", price: 79.99, currency: '$' },
+      { edition: "Deluxe Premium Edition", price: 109.99, currency: '$' },
+      { edition: "Deluxe Edition", price: 89.99, currency: '$' },
       { edition: "Standard Edition", price: 69.99, currency: '$' },
     ],
   },
