ca9d467b25016eaae3a1bcd82f379d909f0ecc6d
celso <celso.sjunior@outlook.com>
2025-09-08 17:57:46 -0300

    Criar HellIsUs.ts

---
+++ 2k38/cycle/src/data/games/HellIsUs.ts
@@ -0,0 +1,33 @@
+export const hellisus = {
+  launchDate: '2025-09-04',
+  Brazil: {
+    'September 2025': [
+      { edition: "Deluxe Edition", price: 297.44, currency: 'R$', exchangeTax2: 0 },
+      { edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax2: 0 },
+      { edition: "Standard Edition", price: 222.44, currency: 'R$', exchangeTax2: 4.99 },
+      { edition: "Standard Edition", price: 339.90, currency: 'R$', exchangeTax2: 4.28 },
+    ],
+    'February 2025': [
+      { edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax2: 0 },
+      { edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.28 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: "Deluxe Edition", price: 79.99, currency: '€' },
+      { edition: "Standard Edition", price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: "Deluxe Edition", price: 69.99, currency: '£' },
+      { edition: "Standard Edition", price: 59.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: "Deluxe Edition", price: 79.99, currency: '$' },
+      { edition: "Standard Edition", price: 69.99, currency: '$' },
+    ],
+  },
+};
