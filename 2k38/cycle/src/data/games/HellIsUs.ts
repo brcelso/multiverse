fa4d23d18a0b6cbcbc9c53507b72f39a6f15a605
celso <celso.sjunior@outlook.com>
2025-09-08 19:45:13 -0300

    Atualizar o HellIsUs.ts

--- 2k38/cycle/src/data/games/HellIsUs.ts
+++ 2k38/cycle/src/data/games/HellIsUs.ts
@@ -6,35 +6,35 @@ export const hellisus = {
       { platform: "Xbox", edition: "Standard Edition", price: 339.90, totalIncrease: 52.5, currency: 'R$', exchangeTax2: 3.70 },
     ],
     'August 2025': [
-      { platform: "PS5", edition: "Deluxe Edition", price: 297.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.71 },
-      { platform: "PS5", edition: "Standard Edition", price: 222.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.70 },
+      { platform: "Playstation 5", edition: "Deluxe Edition", price: 297.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.71 },
+      { platform: "Playstation 5", edition: "Standard Edition", price: 222.44, totalDiscount: 34.6, currency: 'R$', exchangeTax2: 3.70 },
     ],
     'June 2025': [
-      { platform: "PS5", edition: "Standard Edition", price: 339.90, increase: 13.34, currency: 'R$', exchangeTax2: 5.66 },
+      { platform: "Playstation 5", edition: "Standard Edition", price: 339.90, increase: 13.34, currency: 'R$', exchangeTax2: 5.66 },
     ],
     'February 2025': [
-      { platform: "PS5", edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax2: 5.69 },
+      { platform: "Playstation 5", edition: "Deluxe Edition", price: 455.90, currency: 'R$', exchangeTax2: 5.69 },
       { platform: "Xbox", edition: "Deluxe Edition", price: 297.44, currency: 'R$', exchangeTax2: 3.71 },
-      { platform: "PS5", edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.99 },
+      { platform: "Playstation 5", edition: "Standard Edition", price: 299.90, currency: 'R$', exchangeTax2: 4.99 },
       { platform: "Xbox", edition: "Standard Edition", price: 222.44, currency: 'R$', exchangeTax2: 3.70 },
     ],
   },
   Europe: {
     'February 2025': [
-      { platform: "PS5/Xbox", edition: "Deluxe Edition", price: 79.99, currency: '€' },
-      { platform: "PS5/Xbox", edition: "Standard Edition", price: 59.99, currency: '€' },
+      { platform: "Playstation 5/Xbox", edition: "Deluxe Edition", price: 79.99, currency: '€' },
+      { platform: "Playstation 5/Xbox", edition: "Standard Edition", price: 59.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'February 2025': [
-      { platform: "PS5/Xbox", edition: "Deluxe Edition", price: 64.99, currency: '£' },
-      { platform: "PS5/Xbox", edition: "Standard Edition", price: 49.99, currency: '£' },
+      { platform: "Playstation 5/Xbox", edition: "Deluxe Edition", price: 64.99, currency: '£' },
+      { platform: "Playstation 5/Xbox", edition: "Standard Edition", price: 49.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'February 2025': [
-      { platform: "PS5/Xbox", edition: "Deluxe Edition", price: 79.99, currency: '$' },
-      { platform: "PS5/Xbox", edition: "Standard Edition", price: 59.99, currency: '$' },
+      { platform: "Playstation 5/Xbox", edition: "Deluxe Edition", price: 79.99, currency: '$' },
+      { platform: "Playstation 5/Xbox", edition: "Standard Edition", price: 59.99, currency: '$' },
     ],
   },
 };
