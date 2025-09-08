063794a32ff637ef425a6d5dfc098fbbe084a347
Celso <celso.sjunior@outlook.com>
2025-09-08 01:51:35 -0300

    updated horizon

--- 2k38/cycle/src/data/games/HorizonForbiddenWest.ts
+++ 2k38/cycle/src/data/games/HorizonForbiddenWest.ts
@@ -1,6 +1,5 @@
 export const horizonForbiddenWest = {
   launchDate: '2022-02-18',
-
   Brazil: {
     'August 2025': [
       { edition: 'Complete Edition', price: 224.33, currency: 'R$', discount: 34 },
@@ -12,11 +11,44 @@ export const horizonForbiddenWest = {
       { edition: 'Complete Edition', price: 197.93, currency: 'R$', discount: 34 },
       { edition: 'Standard Edition', price: 149.7, currency: 'R$', discount: 40 },
     ],
+    'April 2023': [
+      { edition: 'Burning Shores', price: 104.90, currency: 'R$', exchangeTax2: 5.24 }
+    ],
     'February 2022': [
-      { edition: 'Complete Edition', price: 299.9, currency: 'R$' },
-      { edition: 'PS5 Pro', price: 249.5, currency: 'R$' },
-      { edition: 'PS5', price: 199.5, currency: 'R$' },
-      { edition: 'PS4', price: 149.5, currency: 'R$' },
+      { edition: 'Complete Edition', price: 299.9, currency: 'R$', exchangeTax2: 4.99 },
+      { edition: 'Standard Edition', price: 249.5, currency: 'R$', exchangeTax2: 5 },
+      { edition: 'Standard Edition', price: 199.5, currency: 'R$', exchangeTax2: 4 },
+      { edition: 'Standard Edition', price: 149.5, currency: 'R$', exchangeTax2: 3.73 },
+    ],
+  },
+  Europe: {
+    'April 2023': [
+      { edition: 'Burning Shores', price: 19.99, currency: '€' }
+    ],
+    'February 2022': [
+      { edition: 'Complete Edition', price: 69.99, currency: '€' },
+      { edition: 'Standard Edition', price: 59.99, currency: '€' },
+      { edition: 'Standard Edition', price: 49.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'April 2023': [
+      { edition: 'Burning Shores', price: 15.99, currency: '£' }
+    ],
+    'February 2022': [
+      { edition: 'Complete Edition', price: 59.99, currency: '£' },
+      { edition: 'Standard Edition', price: 49.99, currency: '£' },
+      { edition: 'Standard Edition', price: 44.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'April 2023': [
+      { edition: 'Burning Shores', price: 19.99, currency: '$' }
+    ],
+    'February 2022': [
+      { edition: 'Complete Edition', price: 59.99, currency: '$' },
+      { edition: 'Standard Edition', price: 49.99, currency: '$' },
+      { edition: 'Standard Edition', price: 39.99, currency: '$' },
     ],
   },
 };
