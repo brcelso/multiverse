1dfefc62da3af80cb4291391704be80a651f2cd1
celso <celso.sjunior@outlook.com>
2025-09-08 23:07:20 -0300

    Criar Spotify.ts

---
+++ 2k38/cycle/src/data/subs/Spotify.ts
@@ -0,0 +1,41 @@
+export const spotify = {
+  launchDate: '2011-07-14', 
+  Brazil: {
+    'September 2025': [
+      { price: 40.90, increase: 17, totalIncrease: 17, currency: 'R$', edition: "Premium Family Monthly" },
+      { price: 31.90, increase: 14, totalIncrease: 45, currency: 'R$', edition: "Premium Duo Monthly" },
+      { price: 23.90, increase: 9, totalIncrease: 41, currency: 'R$', edition: "Premium Individual Monthly" },
+      { price: 12.90, increase: 8, totalIncrease: 30, currency: 'R$', edition: "Premium Student Monthly" },
+    ],
+    'July 2023': [
+      { price: 34.90, currency: 'R$', edition: "Premium Family Monthly" },
+      { price: 27.90, increase: 12, currency: 'R$', edition: "Premium Duo Monthly" },
+      { price: 21.90, increase: 10, totalIncrease: 29, currency: 'R$', edition: "Premium Individual Monthly" },
+      { price: 11.90, increase: 20, totalIncrease: 20, currency: 'R$', edition: "Premium Student Monthly" },
+    ],
+    'January 2019': [
+      { price: 26.90, currency: 'R$', edition: "Premium Family Monthly" },
+      { price: 21.90, currency: 'R$', edition: "Premium Duo Monthly" },
+      { price: 16.90, currency: 'R$', edition: "Premium Individual Monthly" },
+      { price: 8.50, currency: 'R$', edition: "Premium Student Monthly" },
+    ],
+  },
+  UnitedStates: {
+    'June 2024': [
+      { price: 19.99, increase: 18, totalIncrease: 25, currency: '$', edition: "Premium Family Monthly" },
+      { price: 16.99, increase: 13, totalIncrease: 31, currency: '$', edition: "Premium Duo Monthly" },
+      { price: 11.99, increase: 9, totalIncrease: 20, currency: '$', edition: "Premium Individual Monthly" },
+      { price: 5.99, increase: 20, totalIncrease: 20, currency: '$', edition: "Premium Student Monthly" },
+    ],
+    'July 2023': [
+      { price: 16.99, increase: 6, totalIncrease: 6, currency: '$', edition: "Premium Family Monthly" },
+      { price: 14.99, increase: 15, totalIncrease: 15, currency: '$', edition: "Premium Duo Monthly" },
+      { price: 10.99, increase: 10, totalIncrease: 10, currency: '$', edition: "Premium Individual Monthly" },
+      { price: 5.99, increase: 20, totalIncrease: 20, currency: '$', edition: "Premium Student Monthly" },
+    ],
+    'July 2011': [
+      { price: 9.99, currency: '$', edition: "Premium Individual Monthly" },
+      { price: 4.99, currency: '$', edition: "Premium Student Monthly" },
+    ],
+  },
+};
