6466adcfb69d69bcc855669e0f2ac3474223ed1a
celso <celso.sjunior@outlook.com>
2025-09-08 21:29:33 -0300

    Atualizar o HollowKnightSilkSong.ts

--- 2k38/cycle/src/data/games/HollowKnightSilkSong.ts
+++ 2k38/cycle/src/data/games/HollowKnightSilkSong.ts
@@ -2,35 +2,35 @@ export const hollowknightsilksong = {
   launchDate: '2025-09-04',
   Argentina: {
     'September 2025': [
-      { edition: "Silksong", price: 6.99, currency: '$' },
-      { edition: "Silksong", price: 284, currency: 'ARS' },
+      { platform: "Playstation | Xbox", edition: "Silksong", price: 6.99, currency: '$' },
+      { platform: "Playstation | Xbox", edition: "Silksong", price: 284, currency: 'ARS' },
     ],
   },
   Japan: {
     'September 2025': [
-      { edition: "Silksong", price: 2300, currency: '¥' },
+      { platform: "Playstation | Xbox", edition: "Silksong", price: 2300, currency: '¥' },
     ],
   },
   Brazil: {
     'September 2025': [
-      { edition: "Silksong", price: 114.90, currency: 'R$', exchangeTax2: 5.74 },
-      { edition: "Silksong", price: 74.95, currency: 'R$', exchangeTax2: 3.74 },
-      { edition: "Silksong", price: 59.99, currency: 'R$', exchangeTax2: 3 },
+      { platform: "Playstation 5", edition: "Silksong", price: 114.90, currency: 'R$', exchangeTax2: 5.74 },
+      { platform: "Xbox", edition: "Silksong", price: 74.95, currency: 'R$', exchangeTax2: 3.74 },
+      { platform: "PC", edition: "Silksong", price: 59.99, currency: 'R$', exchangeTax2: 3 },
     ],
   },
   Europe: {
     'September 2025': [
-      { edition: "Silksong", price: 19.99, currency: '€' },
+      { platform: "Playstation | Xbox", edition: "Silksong", price: 19.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'September 2025': [
-      { edition: "Silksong", price: 16.75, currency: '£' },
+      { platform: "Playstation | Xbox", edition: "Silksong", price: 16.75, currency: '£' },
     ],
   },
   UnitedStates: {
     'September 2025': [
-      { edition: "Silksong", price: 19.99, currency: '$' },
+      { platform: "Playstation | Xbox", edition: "Silksong", price: 19.99, currency: '$' },
     ],
   },
 };
