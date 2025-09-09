d42876ce2a34daaf579c8460574cbc65082dca5d
celso <celso.sjunior@outlook.com>
2025-09-08 23:14:34 -0300

    Atualizar o Spotify.ts

--- 2k38/cycle/src/data/subs/Spotify.ts
+++ 2k38/cycle/src/data/subs/Spotify.ts
@@ -2,7 +2,7 @@ export const spotify = {
   launchDate: '2011-07-14',
   Brazil: {
     'September 2025': [
-      { price: 40.90, increase: 17, totalIncrease: 17, currency: 'R$', edition: "Premium Family Monthly", exchangeTax2: 2.05 },
+      { price: 40.90, increase: 17, currency: 'R$', edition: "Premium Family Monthly", exchangeTax2: 2.05 },
       { price: 31.90, increase: 14, totalIncrease: 45, currency: 'R$', edition: "Premium Duo Monthly", exchangeTax2: 1.88 },
       { price: 23.90, increase: 9, totalIncrease: 41, currency: 'R$', edition: "Premium Individual Monthly", exchangeTax2: 1.99 },
       { price: 12.90, increase: 8, totalIncrease: 30, currency: 'R$', edition: "Premium Student Monthly", exchangeTax2: 2.15 },
@@ -11,7 +11,7 @@ export const spotify = {
       { price: 34.90, currency: 'R$', edition: "Premium Family Monthly", exchangeTax2: 2.05 },
       { price: 27.90, increase: 12, currency: 'R$', edition: "Premium Duo Monthly", exchangeTax2: 1.86 },
       { price: 21.90, increase: 10, totalIncrease: 29, currency: 'R$', edition: "Premium Individual Monthly", exchangeTax2: 1.99 },
-      { price: 11.90, increase: 20, totalIncrease: 20, currency: 'R$', edition: "Premium Student Monthly", exchangeTax2: 1.99 },
+      { price: 11.90, increase: 20, currency: 'R$', edition: "Premium Student Monthly", exchangeTax2: 1.99 },
     ],
     'January 2019': [
       { price: 26.90, currency: 'R$', edition: "Premium Family Monthly", exchangeTax2: 1.35 },
