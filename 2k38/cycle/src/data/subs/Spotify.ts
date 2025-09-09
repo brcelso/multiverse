1d0251bdd06349551f708c9e06c51ff2938d0cb3
celso <celso.sjunior@outlook.com>
2025-09-08 23:12:27 -0300

    Atualizar o Spotify.ts

--- 2k38/cycle/src/data/subs/Spotify.ts
+++ 2k38/cycle/src/data/subs/Spotify.ts
@@ -1,23 +1,23 @@
 export const spotify = {
-  launchDate: '2011-07-14', 
+  launchDate: '2011-07-14',
   Brazil: {
     'September 2025': [
-      { price: 40.90, increase: 17, totalIncrease: 17, currency: 'R$', edition: "Premium Family Monthly" },
-      { price: 31.90, increase: 14, totalIncrease: 45, currency: 'R$', edition: "Premium Duo Monthly" },
-      { price: 23.90, increase: 9, totalIncrease: 41, currency: 'R$', edition: "Premium Individual Monthly" },
-      { price: 12.90, increase: 8, totalIncrease: 30, currency: 'R$', edition: "Premium Student Monthly" },
+      { price: 40.90, increase: 17, totalIncrease: 17, currency: 'R$', edition: "Premium Family Monthly", exchangeTax2: 2.05 },
+      { price: 31.90, increase: 14, totalIncrease: 45, currency: 'R$', edition: "Premium Duo Monthly", exchangeTax2: 1.88 },
+      { price: 23.90, increase: 9, totalIncrease: 41, currency: 'R$', edition: "Premium Individual Monthly", exchangeTax2: 1.99 },
+      { price: 12.90, increase: 8, totalIncrease: 30, currency: 'R$', edition: "Premium Student Monthly", exchangeTax2: 2.15 },
     ],
     'July 2023': [
-      { price: 34.90, currency: 'R$', edition: "Premium Family Monthly" },
-      { price: 27.90, increase: 12, currency: 'R$', edition: "Premium Duo Monthly" },
-      { price: 21.90, increase: 10, totalIncrease: 29, currency: 'R$', edition: "Premium Individual Monthly" },
-      { price: 11.90, increase: 20, totalIncrease: 20, currency: 'R$', edition: "Premium Student Monthly" },
+      { price: 34.90, currency: 'R$', edition: "Premium Family Monthly", exchangeTax2: 2.05 },
+      { price: 27.90, increase: 12, currency: 'R$', edition: "Premium Duo Monthly", exchangeTax2: 1.86 },
+      { price: 21.90, increase: 10, totalIncrease: 29, currency: 'R$', edition: "Premium Individual Monthly", exchangeTax2: 1.99 },
+      { price: 11.90, increase: 20, totalIncrease: 20, currency: 'R$', edition: "Premium Student Monthly", exchangeTax2: 1.99 },
     ],
     'January 2019': [
-      { price: 26.90, currency: 'R$', edition: "Premium Family Monthly" },
-      { price: 21.90, currency: 'R$', edition: "Premium Duo Monthly" },
-      { price: 16.90, currency: 'R$', edition: "Premium Individual Monthly" },
-      { price: 8.50, currency: 'R$', edition: "Premium Student Monthly" },
+      { price: 26.90, currency: 'R$', edition: "Premium Family Monthly", exchangeTax2: 1.35 },
+      { price: 21.90, currency: 'R$', edition: "Premium Duo Monthly", exchangeTax2: 1.29 },
+      { price: 16.90, currency: 'R$', edition: "Premium Individual Monthly", exchangeTax2: 1.41 },
+      { price: 8.50, currency: 'R$', edition: "Premium Student Monthly", exchangeTax2: 1.42 },
     ],
   },
   UnitedStates: {
