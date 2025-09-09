240b420ef3bc608c1f49e14554578d9492970cb3
celso <celso.sjunior@outlook.com>
2025-09-09 16:03:26 -0300

    Atualizar o Mafia.ts

--- 2k38/cycle/src/data/games/Mafia.ts
+++ 2k38/cycle/src/data/games/Mafia.ts
@@ -2,10 +2,11 @@ export const mafia = {
   launchDate: '2025-08-08',
   Brazil: {
     'September 2025': [
+      { edition: "Standard Edition", price: 204.02, currency: 'R$', exchangeTax2: 4.08 },
       { edition: "Standard Edition", price: 212, currency: 'R$', exchangeTax2: 4.24 },
     ],
     'August 2025': [
-      { edition: "Deluxe Edition", price: 349.90, currency: 'R$', exchangeTax2: 4.99 },
+      { edition: "Deluxe Edition", price: 349.90, currency: 'R$', exchangeTax2: 5.83 },
       { edition: "Standard Edition", price: 289.90, currency: 'R$', exchangeTax2: 5.79 },
     ],
   },
