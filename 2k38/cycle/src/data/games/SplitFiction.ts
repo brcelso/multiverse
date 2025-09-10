c2d7618ff5effc78cf43d843cf256a629a800b06
celso <celso.sjunior@outlook.com>
2025-09-10 20:11:38 -0300

    Criar SplitFiction.ts

---
+++ 2k38/cycle/src/data/games/SplitFiction.ts
@@ -0,0 +1,35 @@
+export const splitfiction = {
+  launchDate: '2025-03-06',
+  Brazil: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 224.55, discount: 10, currency: 'R$', exchangeTax: 5 },
+    ],
+    'February 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 249.50, currency: 'R$', exchangeTax: 5 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 44.99, discount: 10, currency: '€' },
+    ],
+    'February 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 49.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 40.49, discount: 10, currency: '£' },
+    ],
+    'February 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 44.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 44.99, discount: 10, currency: '$' },
+    ],
+    'February 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 49.99, currency: '$' },
+    ],
+  },
+};
