8d89640a90f54ced47f21fb4acf3525a2046f15d
celso <celso.sjunior@outlook.com>
2025-09-10 09:08:04 -0300

    Atualizar o iphone.ts

--- 2k38/cycle/src/data/consoles/iphone.ts
+++ 2k38/cycle/src/data/consoles/iphone.ts
@@ -1,144 +1,146 @@
 export const iphone = {
   launchDate: '2007-06-29',
   Brazil: {
-    'September 2024': [
-  { edition: 'iPhone 16 Pro Max 1TB', price: 15999, increase: 52.9, totalIncrease: 1679, currency: 'R$' },
-  { edition: 'iPhone 16 Pro Max 512GB', price: 14499, increase: 36.7, totalIncrease: 1512, currency: 'R$' },
-  { edition: 'iPhone 16 Pro Max 256GB', price: 12499, increase: 21.4, totalIncrease: 1312, currency: 'R$' },
-
-  { edition: 'iPhone 16 Pro 1TB', price: 14499, increase: 38.1, totalIncrease: 1512, currency: 'R$' },
-  { edition: 'iPhone 16 Pro 512GB', price: 12999, increase: 23.8, totalIncrease: 1367, currency: 'R$' },
-  { edition: 'iPhone 16 Pro 256GB', price: 11499, increase: 7.1, totalIncrease: 1212, currency: 'R$' },
-  { edition: 'iPhone 16 Pro 128GB', price: 10499, increase: 0, totalIncrease: 1112, currency: 'R$' },
-
-  { edition: 'iPhone 16 Plus 512GB', price: 12499, increase: 25.0, totalIncrease: 1312, currency: 'R$' },
-  { edition: 'iPhone 16 Plus 256GB', price: 10999, increase: 8.3, totalIncrease: 1123, currency: 'R$' },
-  { edition: 'iPhone 16 Plus 128GB', price: 9999, increase: 0, totalIncrease: 1012, currency: 'R$' },
-
-  { edition: 'iPhone 16 512GB', price: 11499, increase: 27.8, totalIncrease: 1212, currency: 'R$' },
-  { edition: 'iPhone 16 256GB', price: 9999, increase: 0, totalIncrease: 1012, currency: 'R$' },
-  { edition: 'iPhone 16 128GB', price: 8999, increase: 0, totalIncrease: 900, currency: 'R$' },
-],
-
-'September 2023': [
-  { edition: 'iPhone 15 Pro Max 1TB', price: 15499, increase: 45.8, totalIncrease: 1623, currency: 'R$' },
-  { edition: 'iPhone 15 Pro Max 512GB', price: 13999, increase: 30.6, totalIncrease: 1467, currency: 'R$' },
-  { edition: 'iPhone 15 Pro Max 256GB', price: 11999, increase: 13.3, totalIncrease: 1267, currency: 'R$' },
-
-  { edition: 'iPhone 15 Pro 1TB', price: 13999, increase: 40.0, totalIncrease: 1467, currency: 'R$' },
-  { edition: 'iPhone 15 Pro 512GB', price: 12499, increase: 25.0, totalIncrease: 1312, currency: 'R$' },
-  { edition: 'iPhone 15 Pro 256GB', price: 10999, increase: 0, totalIncrease: 1123, currency: 'R$' },
-  { edition: 'iPhone 15 Pro 128GB', price: 9999, increase: 0, totalIncrease: 1012, currency: 'R$' },
-
-  { edition: 'iPhone 15 Plus 512GB', price: 11999, increase: 26.3, totalIncrease: 1267, currency: 'R$' },
-  { edition: 'iPhone 15 Plus 256GB', price: 10499, increase: 7.1, totalIncrease: 1117, currency: 'R$' },
-  { edition: 'iPhone 15 Plus 128GB', price: 9499, increase: 0, totalIncrease: 1012, currency: 'R$' },
-
-  { edition: 'iPhone 15 512GB', price: 10999, increase: 15.0, totalIncrease: 1123, currency: 'R$' },
-  { edition: 'iPhone 15 256GB', price: 9499, increase: 0, totalIncrease: 1012, currency: 'R$' },
-  { edition: 'iPhone 15 128GB', price: 8499, increase: 0, totalIncrease: 660, currency: 'R$' },
-],
-
-'September 2022': [
-  { edition: 'iPhone 14 Pro Max 1TB', price: 14299, increase: 43.0, totalIncrease: 1340, currency: 'R$' },
-  { edition: 'iPhone 14 Pro Max 512GB', price: 12799, increase: 28.0, totalIncrease: 1190, currency: 'R$' },
-  { edition: 'iPhone 14 Pro Max 256GB', price: 10999, increase: 10.0, totalIncrease: 1010, currency: 'R$' },
-  { edition: 'iPhone 14 Pro Max 128GB', price: 9999, increase: 0, totalIncrease: 910, currency: 'R$' },
-
-  { edition: 'iPhone 14 Pro 1TB', price: 12999, increase: 30.0, totalIncrease: 1147, currency: 'R$' },
-  { edition: 'iPhone 14 Pro 512GB', price: 11499, increase: 15.0, totalIncrease: 980, currency: 'R$' },
-  { edition: 'iPhone 14 Pro 256GB', price: 9999, increase: 0, totalIncrease: 852, currency: 'R$' },
-  { edition: 'iPhone 14 Pro 128GB', price: 8999, increase: 0, totalIncrease: 700, currency: 'R$' },
-
-  { edition: 'iPhone 14 Plus 512GB', price: 10999, increase: 15.0, totalIncrease: 920, currency: 'R$' },
-  { edition: 'iPhone 14 Plus 256GB', price: 9499, increase: 0, totalIncrease: 800, currency: 'R$' },
-  { edition: 'iPhone 14 Plus 128GB', price: 8499, increase: 0, totalIncrease: 660, currency: 'R$' },
-
-  { edition: 'iPhone 14 512GB', price: 9999, increase: 15.0, totalIncrease: 852, currency: 'R$' },
-  { edition: 'iPhone 14 256GB', price: 8499, increase: 0, totalIncrease: 660, currency: 'R$' },
-  { edition: 'iPhone 14 128GB', price: 7499, increase: 0, totalIncrease: 555, currency: 'R$' },
-],
-
-'July 2008': [
-  { edition: 'iPhone 3G 16GB', price: 1199, increase: 33.3, totalIncrease: 33.3, currency: 'R$' },
-  { edition: 'iPhone 3G 8GB', price: 899, increase: 0, totalIncrease: 0, currency: 'R$' },
-],
-  },
-  UnitedStates: {
-    'September 2024': [
-  { edition: 'iPhone 16 Pro Max 1TB', price: 1599, increase: 300.3, totalIncrease: 300.3, currency: '$' },
-  { edition: 'iPhone 16 Pro Max 512GB', price: 1399, increase: 27.2, totalIncrease: 250.6, currency: '$' },
-  { edition: 'iPhone 16 Pro Max 256GB', price: 1199, increase: 14.3, totalIncrease: 200.3, currency: '$' },
-
-  { edition: 'iPhone 16 Pro 1TB', price: 1299, increase: 8.3, totalIncrease: 225.1, currency: '$' },
-  { edition: 'iPhone 16 Pro 512GB', price: 1099, increase: 10.0, totalIncrease: 175.2, currency: '$' },
-  { edition: 'iPhone 16 Pro 256GB', price: 999, increase: 9.1, totalIncrease: 150.1, currency: '$' },
-  { edition: 'iPhone 16 Pro 128GB', price: 999, increase: 0.0, totalIncrease: 150.1, currency: '$' },
-
-  { edition: 'iPhone 16 Plus 512GB', price: 1099, increase: 10.0, totalIncrease: 175.2, currency: '$' },
-  { edition: 'iPhone 16 Plus 256GB', price: 899, increase: 11.1, totalIncrease: 125.3, currency: '$' },
-  { edition: 'iPhone 16 Plus 128GB', price: 799, increase: 11.1, totalIncrease: 100.3, currency: '$' },
-
-  { edition: 'iPhone 16 512GB', price: 1099, increase: 22.2, totalIncrease: 175.2, currency: '$' },
-  { edition: 'iPhone 16 256GB', price: 899, increase: 12.5, totalIncrease: 125.3, currency: '$' },
-  { edition: 'iPhone 16 128GB', price: 799, increase: 11.1, totalIncrease: 100.3, currency: '$' },
+    'September 2025': [
+      { edition: 'iPhone 16 Pro Max 256GB', price: 8549, discount: : 32, currency: 'R$' },
+      { edition: 'iPhone 17 Pro Max 2TB', price: 18499, increase: 12.5, totalIncrease: 1950, currency: 'R$' },
+      { edition: 'iPhone 17 Pro Max 1TB', price: 17999, increase: 12.5, totalIncrease: 1875, currency: 'R$' },
+      { edition: 'iPhone 17 Pro Max 512GB', price: 15999, increase: 10.3, totalIncrease: 1675, currency: 'R$' },
+      { edition: 'iPhone 17 Pro 1TB', price: 16499, increase: 13.8, totalIncrease: 1712, currency: 'R$' },
+      { edition: 'iPhone 17 Pro Max 256GB', price: 13999, increase: 8.3, totalIncrease: 1475, currency: 'R$' },
+      { edition: 'iPhone 17 Pro 512GB', price: 14499, increase: 11.5, totalIncrease: 1512, currency: 'R$' },
+      { edition: 'iPhone 17 Plus 512GB', price: 13499, increase: 8.0, totalIncrease: 1412, currency: 'R$' },
+      { edition: 'iPhone 17 Pro 256GB', price: 12499, increase: 8.7, totalIncrease: 1312, currency: 'R$' },
+      { edition: 'iPhone 17 512GB', price: 12499, increase: 8.7, totalIncrease: 1312, currency: 'R$' },
+      { edition: 'iPhone 17 Pro 128GB', price: 11499, increase: 9.5, totalIncrease: 1212, currency: 'R$' },
+      { edition: 'iPhone 17 Plus 256GB', price: 11999, increase: 9.1, totalIncrease: 1267, currency: 'R$' },
+      { edition: 'iPhone 17 Plus 128GB', price: 10499, increase: 5.0, totalIncrease: 1112, currency: 'R$' },
+      { edition: 'iPhone 17 256GB', price: 10999, increase: 10.0, totalIncrease: 1167, currency: 'R$' },
+      { edition: 'iPhone 17 128GB', price: 9499, increase: 5.5, totalIncrease: 1000, currency: 'R$' },
     ],
+
+    'September 2024': [
+      { edition: 'iPhone 16 Pro Max 1TB', price: 15999, increase: 52.9, totalIncrease: 1679, currency: 'R$' },
+      { edition: 'iPhone 16 Pro Max 512GB', price: 14499, increase: 36.7, totalIncrease: 1512, currency: 'R$' },
+      { edition: 'iPhone 16 Pro 1TB', price: 14499, increase: 38.1, totalIncrease: 1512, currency: 'R$' },
+      { edition: 'iPhone 16 Pro Max 256GB', price: 12499, increase: 21.4, totalIncrease: 1312, currency: 'R$' },
+      { edition: 'iPhone 16 Plus 512GB', price: 12499, increase: 25.0, totalIncrease: 1312, currency: 'R$' },
+      { edition: 'iPhone 16 Pro 512GB', price: 12999, increase: 23.8, totalIncrease: 1367, currency: 'R$' },
+      { edition: 'iPhone 16 Pro 256GB', price: 11499, increase: 7.1, totalIncrease: 1212, currency: 'R$' },
+      { edition: 'iPhone 16 512GB', price: 11499, increase: 27.8, totalIncrease: 1212, currency: 'R$' },
+      { edition: 'iPhone 16 Pro 128GB', price: 10499, increase: 0, totalIncrease: 1112, currency: 'R$' },
+      { edition: 'iPhone 16 Plus 256GB', price: 10999, increase: 8.3, totalIncrease: 1123, currency: 'R$' },
+      { edition: 'iPhone 16 256GB', price: 9999, increase: 0, totalIncrease: 1012, currency: 'R$' },
+      { edition: 'iPhone 16 Plus 128GB', price: 9999, increase: 0, totalIncrease: 1012, currency: 'R$' },
+      { edition: 'iPhone 16 128GB', price: 8999, increase: 0, totalIncrease: 900, currency: 'R$' },
+    ],
+
     'September 2023': [
-  { edition: 'iPhone 15 Pro Max 1TB', price: 1599, increase: 300.3, totalIncrease: 300.3, currency: '$' },
-  { edition: 'iPhone 15 Pro Max 512GB', price: 1399, increase: 27.2, totalIncrease: 250.6, currency: '$' },
-  { edition: 'iPhone 15 Pro Max 256GB', price: 1199, increase: 14.3, totalIncrease: 200.3, currency: '$' },
-
-  { edition: 'iPhone 15 Pro 1TB', price: 1299, increase: 8.3, totalIncrease: 225.1, currency: '$' },
-  { edition: 'iPhone 15 Pro 512GB', price: 1099, increase: 10.0, totalIncrease: 175.2, currency: '$' },
-  { edition: 'iPhone 15 Pro 256GB', price: 999, increase: 9.1, totalIncrease: 150.1, currency: '$' },
-  { edition: 'iPhone 15 Pro 128GB', price: 999, increase: 0.0, totalIncrease: 150.1, currency: '$' },
-
-  { edition: 'iPhone 15 Plus 512GB', price: 1099, increase: 10.0, totalIncrease: 175.2, currency: '$' },
-  { edition: 'iPhone 15 Plus 256GB', price: 899, increase: 11.1, totalIncrease: 125.3, currency: '$' },
-  { edition: 'iPhone 15 Plus 128GB', price: 799, increase: 11.1, totalIncrease: 100.3, currency: '$' },
-
-  { edition: 'iPhone 15 512GB', price: 1099, increase: 22.2, totalIncrease: 175.2, currency: '$' },
-  { edition: 'iPhone 15 256GB', price: 899, increase: 12.5, totalIncrease: 125.3, currency: '$' },
-  { edition: 'iPhone 15 128GB', price: 799, increase: 11.1, totalIncrease: 100.3, currency: '$' },
+      { edition: 'iPhone 15 Pro Max 1TB', price: 14999, currency: 'R$' },
+      { edition: 'iPhone 15 Pro Max 512GB', price: 13499, currency: 'R$' },
+      { edition: 'iPhone 15 Pro 1TB', price: 13999, currency: 'R$' },
+      { edition: 'iPhone 15 Pro 512GB', price: 12499, currency: 'R$' },
+      { edition: 'iPhone 15 Plus 512GB', price: 11499, currency: 'R$' },
+      { edition: 'iPhone 15 512GB', price: 10999, currency: 'R$' },
+      { edition: 'iPhone 15 Pro 256GB', price: 10999, currency: 'R$' },
+      { edition: 'iPhone 15 256GB', price: 9999, currency: 'R$' },
+      { edition: 'iPhone 15 Plus 256GB', price: 9999, currency: 'R$' },
+      { edition: 'iPhone 15 Pro 128GB', price: 9499, currency: 'R$' },
+      { edition: 'iPhone 15 128GB', price: 8999, currency: 'R$' },
+      { edition: 'iPhone 15 Plus 128GB', price: 8999, currency: 'R$' },
     ],
+
     'September 2022': [
-  { edition: 'iPhone 14 Pro Max 1TB', price: 1599, increase: 14.3, totalIncrease: 300, currency: '$' },
-  { edition: 'iPhone 14 Pro Max 512GB', price: 1399, increase: 16.7, totalIncrease: 250, currency: '$' },
-  { edition: 'iPhone 14 Pro Max 256GB', price: 1199, increase: 9.1, totalIncrease: 200, currency: '$' },
-  { edition: 'iPhone 14 Pro Max 128GB', price: 1099, increase: 9.1, totalIncrease: 175, currency: '$' },
-
-  { edition: 'iPhone 14 Pro 1TB', price: 1499, increase: 15.4, totalIncrease: 275, currency: '$' },
-  { edition: 'iPhone 14 Pro 512GB', price: 1299, increase: 18.2, totalIncrease: 225, currency: '$' },
-  { edition: 'iPhone 14 Pro 256GB', price: 1099, increase: 10.0, totalIncrease: 175, currency: '$' },
-  { edition: 'iPhone 14 Pro 128GB', price: 999, increase: 10.0, totalIncrease: 150, currency: '$' },
-
-  { edition: 'iPhone 14 Plus 512GB', price: 1199, increase: 20.0, totalIncrease: 200, currency: '$' },
-  { edition: 'iPhone 14 Plus 256GB', price: 999, increase: 11.1, totalIncrease: 150, currency: '$' },
-  { edition: 'iPhone 14 Plus 128GB', price: 899, increase: 11.1, totalIncrease: 125, currency: '$' },
-
-  { edition: 'iPhone 14 512GB', price: 1099, increase: 22.2, totalIncrease: 175, currency: '$' },
-  { edition: 'iPhone 14 256GB', price: 899, increase: 12.5, totalIncrease: 125, currency: '$' },
-  { edition: 'iPhone 14 128GB', price: 799, increase: 11.1, totalIncrease: 100, currency: '$' },
+      { edition: 'iPhone 14 Pro Max 1TB', price: 13999, currency: 'R$' },
+      { edition: 'iPhone 14 Pro Max 512GB', price: 12499, currency: 'R$' },
+      { edition: 'iPhone 14 Pro 1TB', price: 12999, currency: 'R$' },
+      { edition: 'iPhone 14 Pro 512GB', price: 11499, currency: 'R$' },
+      { edition: 'iPhone 14 Plus 512GB', price: 10499, currency: 'R$' },
+      { edition: 'iPhone 14 512GB', price: 9999, currency: 'R$' },
+      { edition: 'iPhone 14 Pro 256GB', price: 9999, currency: 'R$' },
+      { edition: 'iPhone 14 256GB', price: 9499, currency: 'R$' },
+      { edition: 'iPhone 14 Plus 256GB', price: 9499, currency: 'R$' },
+      { edition: 'iPhone 14 Pro 128GB', price: 8999, currency: 'R$' },
+      { edition: 'iPhone 14 128GB', price: 8499, currency: 'R$' },
+      { edition: 'iPhone 14 Plus 128GB', price: 8499, currency: 'R$' },
     ],
+
+    'July 2008': [
+      { edition: 'iPhone 3G 16GB', price: 1199, increase: 33.3, totalIncrease: 33.3, currency: 'R$' },
+      { edition: 'iPhone 3G 8GB', price: 899, increase: 0, totalIncrease: 0, currency: 'R$' },
+    ],
+  },
+
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'iPhone 17 Pro Max 2TB', price: 1999, increase: 10.0, totalIncrease: 350.0, currency: '$' },
+      { edition: 'iPhone 17 Pro Max 1TB', price: 1699, increase: 6.3, totalIncrease: 325.0, currency: '$' },
+      { edition: 'iPhone 17 Pro Max 512GB', price: 1499, increase: 7.1, totalIncrease: 275.0, currency: '$' },
+      { edition: 'iPhone 17 Pro 1TB', price: 1499, increase: 7.1, totalIncrease: 275.0, currency: '$' },
+      { edition: 'iPhone 17 Pro Max 256GB', price: 1299, increase: 8.3, totalIncrease: 225.0, currency: '$' },
+      { edition: 'iPhone 17 Pro 512GB', price: 1299, increase: 8.3, totalIncrease: 225.0, currency: '$' },
+      { edition: 'iPhone 17 Plus 512GB', price: 1199, increase: 9.1, totalIncrease: 200.0, currency: '$' },
+      { edition: 'iPhone 17 Pro 256GB', price: 1099, increase: 10.0, totalIncrease: 175.0, currency: '$' },
+      { edition: 'iPhone 17 512GB', price: 1099, increase: 10.0, totalIncrease: 175.0, currency: '$' },
+      { edition: 'iPhone 17 Pro 128GB', price: 999, increase: 0, totalIncrease: 150.0, currency: '$' },
+      { edition: 'iPhone 17 Plus 256GB', price: 999, increase: 0, totalIncrease: 150.0, currency: '$' },
+      { edition: 'iPhone 17 Plus 128GB', price: 899, increase: 0, totalIncrease: 125.0, currency: '$' },
+      { edition: 'iPhone 17 256GB', price: 899, increase: 0, totalIncrease: 125.0, currency: '$' },
+      { edition: 'iPhone 17 128GB', price: 799, increase: 0, totalIncrease: 100.0, currency: '$' },
+    ],
+
+    'September 2024': [
+      { edition: 'iPhone 16 Pro Max 1TB', price: 1599, increase: 300.3, totalIncrease: 300.3, currency: '$' },
+      { edition: 'iPhone 16 Pro Max 512GB', price: 1399, increase: 27.2, totalIncrease: 250.6, currency: '$' },
+      { edition: 'iPhone 16 Pro Max 256GB', price: 1199, increase: 14.3, totalIncrease: 200.3, currency: '$' },
+      { edition: 'iPhone 16 Pro 1TB', price: 1299, increase: 8.3, totalIncrease: 225.1, currency: '$' },
+      { edition: 'iPhone 16 Pro 512GB', price: 1099, increase: 10.0, totalIncrease: 175.2, currency: '$' },
+      { edition: 'iPhone 16 Plus 512GB', price: 1099, increase: 10.0, totalIncrease: 175.2, currency: '$' },
+      { edition: 'iPhone 16 512GB', price: 1099, increase: 22.2, totalIncrease: 175.2, currency: '$' },
+      { edition: 'iPhone 16 Pro 256GB', price: 999, increase: 9.1, totalIncrease: 150.1, currency: '$' },
+      { edition: 'iPhone 16 Pro 128GB', price: 999, increase: 0.0, totalIncrease: 150.1, currency: '$' },
+      { edition: 'iPhone 16 Plus 256GB', price: 899, increase: 11.1, totalIncrease: 125.3, currency: '$' },
+      { edition: 'iPhone 16 256GB', price: 899, increase: 12.5, totalIncrease: 125.3, currency: '$' },
+      { edition: 'iPhone 16 Plus 128GB', price: 799, increase: 11.1, totalIncrease: 100.3, currency: '$' },
+      { edition: 'iPhone 16 128GB', price: 799, increase: 11.1, totalIncrease: 100.3, currency: '$' },
+    ],
+
+    'September 2023': [
+      { edition: 'iPhone 15 Pro Max 1TB', price: 1599, currency: '$' },
+      { edition: 'iPhone 15 Pro Max 512GB', price: 1399, currency: '$' },
+      { edition: 'iPhone 15 Pro 1TB', price: 1499, currency: '$' },
+      { edition: 'iPhone 15 Pro 512GB', price: 1299, currency: '$' },
+      { edition: 'iPhone 15 Plus 512GB', price: 1099, currency: '$' },
+      { edition: 'iPhone 15 512GB', price: 999, currency: '$' },
+      { edition: 'iPhone 15 Pro 256GB', price: 999, currency: '$' },
+      { edition: 'iPhone 15 256GB', price: 899, currency: '$' },
+      { edition: 'iPhone 15 Plus 256GB', price: 899, currency: '$' },
+      { edition: 'iPhone 15 Pro 128GB', price: 799, currency: '$' },
+      { edition: 'iPhone 15 128GB', price: 799, currency: '$' },
+      { edition: 'iPhone 15 Plus 128GB', price: 799, currency: '$' },
+    ],
+
+    'September 2022': [
+      { edition: 'iPhone 14 Pro Max 1TB', price: 1499, currency: '$' },
+      { edition: 'iPhone 14 Pro Max 512GB', price: 1299, currency: '$' },
+      { edition: 'iPhone 14 Pro 1TB', price: 1399, currency: '$' },
+      { edition: 'iPhone 14 Pro 512GB', price: 1199, currency: '$' },
+      { edition: 'iPhone 14 Plus 512GB', price: 999, currency: '$' },
+      { edition: 'iPhone 14 512GB', price: 899, currency: '$' },
+      { edition: 'iPhone 14 Pro 256GB', price: 899, currency: '$' },
+      { edition: 'iPhone 14 256GB', price: 849, currency: '$' },
+      { edition: 'iPhone 14 Plus 256GB', price: 849, currency: '$' },
+      { edition: 'iPhone 14 Pro 128GB', price: 799, currency: '$' },
+      { edition: 'iPhone 14 128GB', price: 749, currency: '$' },
+      { edition: 'iPhone 14 Plus 128GB', price: 749, currency: '$' },
+    ],
+
     'September 2007': [
-      {
-        edition: 'Iphone 1st Generation 8GB',
-        price: 399,
-        discount: 33.39,
-        currency: '$',
-      },
+      { edition: 'iPhone 1st Generation 8GB', price: 399, discount: 33.39, currency: '$' },
     ],
     'June 2007': [
-      {
-        edition: 'Iphone 1st Generation 8GB',
-        price: 599,
-        currency: '$',
-      },
-      {
-        edition: 'Iphone 1st Generation 4GB',
-        price: 499,
-        currency: '$',
-      },
+      { edition: 'iPhone 1st Generation 8GB', price: 599, currency: '$' },
+      { edition: 'iPhone 1st Generation 4GB', price: 499, currency: '$' },
     ],
   },
 };
