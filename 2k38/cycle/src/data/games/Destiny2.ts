4d335bdd356c986136dc6e8c45874eca4db931f5
celso <celso.sjunior@outlook.com>
2025-09-10 12:09:15 -0300

    Atualizar o Destiny2.ts

--- 2k38/cycle/src/data/games/Destiny2.ts
+++ 2k38/cycle/src/data/games/Destiny2.ts
@@ -2,26 +2,38 @@ export const destiny2 = {
   launchDate: '2025-07-15',
 
   Brazil: {
+    'September 2025': [
+      { edition: 'Year of Prophecy', price: 399.50, discount: 25, currency: 'R$', exchangeTax: 5 },
+    ],
     'July 2025': [
-      { edition: 'Definitive Edition', price: 399.9, currency: 'R$', exchangeTax: 5.71 },
+      { edition: 'Year of Prophecy', price: 499.50, currency: 'R$', exchangeTax: 5 },
     ],
   },
 
   UnitedStates: {
+    'September 2025': [
+      { edition: 'Year Of Prophecy', price: 74.99, discount: 25, currency: '$' },
+    ],
     'July 2025': [
-      { edition: 'Definitive Edition', price: 99.99, currency: '$' },
+      { edition: 'Year Of Prophecy', price: 99.99, currency: '$' },
     ],
   },
 
   Europe: {
+    'September 2025': [
+      { edition: 'Year Of Prophecy', price: 74.99, discount: 25, currency: '€' },
+    ],
     'July 2025': [
-      { edition: 'Definitive Edition', price: 89.99, currency: '€' },
+      { edition: 'Year Of Prophecy', price: 99.99, currency: '€' },
     ],
   },
 
   UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Year Of Prophecy', price: 74.99, discount: 25, currency: '£' },
+    ],
     'July 2025': [
-      { edition: 'Definitive Edition', price: 84.99, currency: '£' },
+      { edition: 'Year Of Prophecy', price: 99.99, currency: '£' },
     ],
   },
 };
