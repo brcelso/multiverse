29d2155fa4169ffdee8fa4437c65308d35832ff8
celso <celso.sjunior@outlook.com>
2025-09-10 11:59:50 -0300

    Atualizar o index.ts

--- 2k38/cycle/src/data/index.ts
+++ 2k38/cycle/src/data/index.ts
@@ -36,6 +36,7 @@ import { cyberpunk } from './games/CyberPunk';
 import { deathStranding } from './games/DeathStranding';
 import { deathStranding2OnTheBeach } from './games/DeathStranding2';
 import { demonsouls } from './games/DemonSouls';
+import { destiny2 } from './games/Destiny2';
 import { donkeyKongBananza } from './games/DonkeyKongBananza';
 import { doomTheDarkAges } from './games/DoomTheDarkAges';
 import { elderScrollsOblivionRemastered } from './games/ElderScrollsOblivionRem';
@@ -160,6 +161,7 @@ export const games = {
   'Death Stranding': deathStranding,
   'Death Stranding 2': deathStranding2OnTheBeach,
   'Demon Souls': demonsouls,
+  'Destiny 2': destiny2,
   'Donkey Kong Bananza': donkeyKongBananza,
   'Doom The Dark Ages': doomTheDarkAges,
   'Elder Scrolls Oblivion Remastered': elderScrollsOblivionRemastered,
