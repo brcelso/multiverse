8462634d0ac7b1a232ed5478dd2a6da5bfed0c06
celso <celso.sjunior@outlook.com>
2025-09-10 20:21:24 -0300

    Atualizar o TonyHawk3+4.ts

--- 2k38/cycle/src/data/games/TonyHawk3+4.ts
+++ 2k38/cycle/src/data/games/TonyHawk3+4.ts
@@ -2,32 +2,41 @@ export const tonyHawk3Plus4 = {
   launchDate: '2025-07-11',
 
   Brazil: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 272.30, discount: 30, currency: 'R$', exchangeTax: 5.55 },
+    ],
     'July 2025': [
       { edition: "Collector Edition", price: 599.9, currency: 'R$', exchangeTax: 4.61 },
-      { edition: 'Deluxe', price: 389.0, currency: 'R$', exchangeTax: 5.55 },
+      { edition: 'Deluxe Edition', price: 389.0, currency: 'R$', exchangeTax: 5.55 },
       { edition: 'Cross-Gen', price: 269.9, currency: 'R$', exchangeTax: 5.39 },
     ],
   },
-
-  UnitedStates: {
-    'July 2025': [
-      { edition: "Collector Edition", price: 129.99, currency: '$' },
-      { edition: 'Deluxe', price: 69.99, currency: '$' },
-      { edition: 'Cross-Gen', price: 49.99, currency: '$' },
-    ],
-  },
-
   Europe: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 48.99, discount: 30, currency: '€' },
+    ],
     'July 2025': [
-      { edition: 'Deluxe', price: 69.99, currency: '€' },
+      { edition: 'Deluxe Edition', price: 69.99, currency: '€' },
       { edition: 'Cross-Gen', price: 49.99, currency: '€' },
     ],
   },
-
   UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 41.99, discount: 30, currency: '£' },
+    ],
     'July 2025': [
-      { edition: 'Deluxe', price: 59.99, currency: '£' },
+      { edition: 'Deluxe Edition', price: 59.99, currency: '£' },
       { edition: 'Cross-Gen', price: 39.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 48.99, discount: 30, currency: '$' },
+    ],
+    'July 2025': [
+      { edition: "Collector Edition", price: 129.99, currency: '$' },
+      { edition: 'Deluxe Ediiton', price: 69.99, currency: '$' },
+      { edition: 'Cross-Gen', price: 49.99, currency: '$' },
     ],
   },
 };
