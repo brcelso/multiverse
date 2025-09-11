773a6133e7cb7117a18299f057c80d0892177a60
celso <celso.sjunior@outlook.com>
2025-09-11 10:36:37 -0300

    Atualizar o CallOfDutyModernWarfareIII.ts

--- 2k38/cycle/src/data/games/CallOfDutyModernWarfareIII.ts
+++ 2k38/cycle/src/data/games/CallOfDutyModernWarfareIII.ts
@@ -1,12 +1,35 @@
 export const callOfDutyModernWarfareIII = {
   launchDate: '2023-11-10',
-
   Brazil: {
     'February 2025': [
-      { edition: 'Standard Edition', price: 149.5, currency: 'R$', discount: 50 },
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 149.5, currency: 'R$', discount: 50 },
     ],
     'November 2023': [
-      { edition: 'Standard Edition', price: 299.0, currency: 'R$' },
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 299.0, currency: 'R$', exchangeTax2: 4.28, },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', discount: 50, price: 39.99, currency: '€' },
+    ],
+    'November 2023': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', discount: 50, price: 34.99, currency: '£' },
+    ],
+    'November 2023': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 69.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', discount: 50, price: 34.99, currency: '$' },
+    ],
+    'November 2023': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 69.99, currency: '$' },
     ],
   },
 };
