57713f8c274e8b4123886b8e9ba0ebe4d984e45f
celso <celso.sjunior@outlook.com>
2025-09-11 10:58:13 -0300

    Criar DarkSoulsIII.ts

---
+++ 2k38/cycle/src/data/games/DarkSoulsIII.ts
@@ -0,0 +1,43 @@
+export const darksoulsiii = {
+  launchDate: '2016-04-12',
+  Brazil: {
+    'Month 2025': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', discount: 0, price: 449.9, currency: 'R$', exchangeTax: 0 },
+      { platform: "Playstation 5", edition: 'Standard Edition', discount: 0, price: 399.9, currency: 'R$', exchangeTax: 0 },
+    ],
+    'April 2016': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 449.9, currency: 'R$', exchangeTax: 0 },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 399.9, currency: 'R$', exchangeTax: 0 },
+    ],
+  },
+  Europe: {
+    'Month 2025': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', discount: 0, price: 89.99, currency: '€' },
+      { platform: "Playstation 5", edition: 'Standard Edition', discount: 0, price: 79.99, currency: '€' },
+    ],
+    'April 2016': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 89.99, currency: '€' },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'Month 2025': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', discount: 0, price: 79.99, currency: '£' },
+      { platform: "Playstation 5", edition: 'Standard Edition', discount: 0, price: 69.99, currency: '£' },
+    ],
+    'April 2016': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 79.99, currency: '£' },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'Month 2025': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', discount: 0, price: 79.99, currency: '$' },
+      { platform: "Playstation 5", edition: 'Standard Edition', discount: 0, price: 69.99, currency: '$' },
+    ],
+    'April 2016': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 79.99, currency: '$' },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+};
