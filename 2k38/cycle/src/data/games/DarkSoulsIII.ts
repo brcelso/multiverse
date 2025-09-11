0d84559a68d196c0b797369fe78a1430b64235b1
celso <celso.sjunior@outlook.com>
2025-09-11 11:32:07 -0300

    Atualizar o DarkSoulsIII.ts

--- 2k38/cycle/src/data/games/DarkSoulsIII.ts
+++ 2k38/cycle/src/data/games/DarkSoulsIII.ts
@@ -2,17 +2,16 @@ export const darksoulsiii = {
   launchDate: '2016-04-12',
   Brazil: {
     'July 2025': [
-      { platform: "Playstation 5", edition: 'Standard Edition', increase: 13, totalIncrease: 36, price: 339.90, currency: 'R$', exchangeTax: 0 },
+      { platform: "Playstation 5", edition: 'Standard Edition', increase: 13, totalIncrease: 36, price: 339.90, currency: 'R$', exchangeTax: 5.66 },
     ],
     'December 2023': [
-      { platform: "Playstation 5", edition: 'Deluxe Edition', increase: 0, price: 399.50, currency: 'R$', exchangeTax: 0 },
-      { platform: "Playstation 5", edition: 'Standard Edition', increase: 20, price: 299.90, currency: 'R$', exchangeTax: 0 },
+      { platform: "Playstation 5", edition: 'Standard Edition', increase: 20, price: 299.90, currency: 'R$', exchangeTax: 5 },
     ],
     'June 2022': [
-      { platform: "Playstation 5", edition: 'Standard Edition', discount: 75, price: 62.49, currency: 'R$', exchangeTax: 0 },
+      { platform: "Playstation 5", edition: 'Standard Edition', discount: 75, price: 62.49, currency: 'R$' },
     ],
     'April 2016': [
-      { platform: "Playstation 5", edition: 'Standard Edition', price: 249.99, currency: 'R$', exchangeTax: 0 },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 249.99, currency: 'R$', exchangeTax: 4.16 },
     ],
   },
   Europe: {
