fcd7d53f0e57ac38acca6ebe9d837c3f7bc66ced
celso <celso.sjunior@outlook.com>
2025-09-11 20:21:49 -0300

    Criar BaldursGate3.ts

---
+++ 2k38/cycle/src/data/games/BaldursGate3.ts
@@ -0,0 +1,43 @@
+export const baldursgate3 = {
+  launchDate: '2023-09-06',
+  Brazil: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', discount: 25, price: 255.67, currency: 'R$', exchangeTax: 0 },
+      { platform: "Playstation 5", edition: 'Game Base', discount: 25, price: 224.92, currency: 'R$', exchangeTax: 0 },
+    ],
+    'September 2023': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 340.90, currency: 'R$', exchangeTax: 0 },
+      { platform: "Playstation 5", edition: 'Game Base', price: 299.90, currency: 'R$', exchangeTax: 0 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', discount: 25, price: 59.99, currency: '€' },
+      { platform: "Playstation 5", edition: 'Game Base', discount: 25, price: 52.49, currency: '€' },
+    ],
+    'September 2023': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 79.99, currency: '€' },
+      { platform: "Playstation 5", edition: 'Game Base', price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', discount: 25, price: 49.49, currency: '£' },
+      { platform: "Playstation 5", edition: 'Game Base', discount: 25, price: 43.49, currency: '£' },
+    ],
+    'September 2023': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 65.99, currency: '£' },
+      { platform: "Playstation 5", edition: 'Game Base', price: 57.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', discount: 25, price: 59.99, currency: '$' },
+      { platform: "Playstation 5", edition: 'Game Base', discount: 25, price: 52.49, currency: '$' },
+    ],
+    'September 2023': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 79.99, currency: '$' },
+      { platform: "Playstation 5", edition: 'Game Base', price: 69.99, currency: '$' },
+    ],
+  },
+};
