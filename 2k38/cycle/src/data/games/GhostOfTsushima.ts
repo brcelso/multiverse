c2634132ec576ff0901701c0ba1d8f66e799d4c8
celso <celso.sjunior@outlook.com>
2025-09-10 22:52:33 -0300

    Criar GhostOfTsushima.ts

---
+++ 2k38/cycle/src/data/games/GhostOfTsushima.ts
@@ -0,0 +1,43 @@
+export const ghostoftsushima = {
+  launchDate: '2021-08-20',
+  Brazil: {
+    'September 2025': [
+      { edition: 'Standard PS5', price: 146.95, discount: 58, currency: 'R$' },
+      { edition: 'Standard PS4', price: 98.96, discount: 67, currency: 'R$' },
+    ],
+    'August 2021': [
+      { edition: 'Standard PS5', price: 349.90, currency: 'R$', exchangeTax2: 4.99 },
+      { edition: 'Standard PS4', price: 299.90, currency: 'R$', exchangeTax2: 4.99 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: 'Standard PS5', price: 39.99, discount: 50, currency: '€' },
+      { edition: 'Standard PS4', price: 29.39, discount: 58, currency: '€' },
+    ],
+    'August 2021': [
+      { edition: 'Standard PS5', price: 79.99, currency: '€' },
+      { edition: 'Standard PS4', price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Standard PS5', price: 34.99, discount: 50, currency: '£' },
+      { edition: 'Standard PS4', price: 25.19, discount: 58, currency: '£' },
+    ],
+    'August 2021': [
+      { edition: 'Standard PS5', price: 69.99, currency: '£' },
+      { edition: 'Standard PS4', price: 59.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Standard PS5', price: 31.99, discount: 54, currency: '$' },
+      { edition: 'Standard PS4', price: 24.99, discount: 58, currency: '$' },
+    ],
+    'August 2021': [
+      { edition: 'Standard PS5', price: 69.99, currency: '$' },
+      { edition: 'Standard PS4', price: 59.99, currency: '$' },
+    ],
+  },
+};
