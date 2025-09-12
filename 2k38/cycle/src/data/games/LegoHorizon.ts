7959769e6f5147d9a5eace7024cf45126fc3aec5
celso <celso.sjunior@outlook.com>
2025-09-11 22:02:10 -0300

    Criar LegoHorizon.ts

---
+++ 2k38/cycle/src/data/games/LegoHorizon.ts
@@ -0,0 +1,37 @@
+export const legohorizon = {
+  launchDate: '2024-11-14',
+  Brazil: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', discount: 32.5, price: 229.90, currency: 'R$' },
+    ],
+    'July 2025': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 284.90, discount: 18, currency: 'R$', exchangeTax: 5.69 },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 99.99, discount: 70, currency: 'R$', exchangeTax: 2.50 },
+    ],
+    'June 2025': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 339.90, increase: 47.5, currency: 'R$', exchangeTax: 8.49 },
+    ],
+    'November 2024': [
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 349.90, currency: 'R$', exchangeTax: 6.99 },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 229.90, currency: 'R$', exchangeTax: 5.74 },
+    ],
+  },
+  Europe: {
+    'November 2024': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 49.99, currency: '€' },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 39.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'November 2024': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 44.99, currency: '£' },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 34.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'November 2024': [
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 49.99, currency: '$' },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 39.99, currency: '$' },
+    ],
+  },
+};
