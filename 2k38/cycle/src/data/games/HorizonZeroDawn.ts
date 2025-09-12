f6ee3c29537797b8e45a19c33d59e2b956760863
celso <celso.sjunior@outlook.com>
2025-09-11 21:18:13 -0300

    Atualizar o HorizonZeroDawn.ts

--- 2k38/cycle/src/data/games/HorizonZeroDawn.ts
+++ 2k38/cycle/src/data/games/HorizonZeroDawn.ts
@@ -2,8 +2,14 @@ export const horizonZeroDawn = {
   launchDate: '2020-02-28',
 
   Brazil: {
-    'February 2025': [
-      { edition: 'PS5 Remastered', price: 199.5, currency: 'R$' },
+    'September 2025': [
+      { edition: 'PS5 Remastered', price: 170.94, discount: 40, currency: 'R$' },
+    ],
+    'June 2025': [
+      { edition: 'PS5 Remastered', price: 284.90, increase: 43, currency: 'R$' },
+    ],
+    'September 2024': [
+      { edition: 'PS4', price: 199.5, increase: 100, currency: 'R$' },
     ],
     'February 2020': [
       { edition: 'PS4', price: 99.5, currency: 'R$' },
@@ -11,9 +17,13 @@ export const horizonZeroDawn = {
   },
 
   UnitedStates: {
+    'September 2024': [
+      { edition: 'PS5 Remastered', price: 49.99, currency: '$' },
+      { edition: 'PS4', price: 39.99, increase: 100, currency: '$' },
+    ],
     'February 2020': [
       { edition: 'PS5 Remastered', price: 49.99, currency: '$' },
-      { edition: 'PS4', price: 39.99, currency: '$' },
+      { edition: 'PS4', price: 19.99, currency: '$' },
     ],
   },
 };
