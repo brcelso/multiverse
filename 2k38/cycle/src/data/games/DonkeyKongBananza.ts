6d8b589cb0abf713dbc819c57ca4977a471a1181
celso <celso.sjunior@outlook.com>
2025-09-13 10:23:49 -0300

    Atualizar o DonkeyKongBananza.ts

--- 2k38/cycle/src/data/games/DonkeyKongBananza.ts
+++ 2k38/cycle/src/data/games/DonkeyKongBananza.ts
@@ -3,13 +3,19 @@ export const donkeyKongBananza = {
   launchDate: '2025-06-05',
 
   Brazil: {
+    'September 2025': [
+      { edition: "Bananza Bundle", price: 559.80, currency: 'R$', exchangeTax: 6.22 },
+    ],
     'June 2025': [
-      { edition: "Standard", price: 408.27, discount: 7, currency: 'R$', exchangeTax: 6.28 },
+      { edition: "Standard", price: 408.27, discount: 7, currency: 'R$', exchangeTax: 5.83 },
       { edition: "Standard", price: 439.90, currency: 'R$', exchangeTax: 6.28 },
     ],
   },
 
   UnitedStates: {
+    'September 2025': [
+      { edition: "Bananza Bundle", price: 89.98, currency: '$' },
+    ],
     'June 2025': [
       { edition: "Standard", price: 69.99, currency: '$' },
     ],
