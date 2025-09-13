5ebed010077d6a6ed25d874a76526aa403d0af5c
celso <celso.sjunior@outlook.com>
2025-09-13 10:31:58 -0300

    Criar PokemonLegendsZA.ts

---
+++ 2k38/cycle/src/data/games/PokemonLegendsZA.ts
@@ -0,0 +1,15 @@
+export const pokemonlegendsza = {
+  launchDate: '2025-10-16',
+  Brazil: {
+    'September 2025': [
+      { edition: "Mega Dimension", price: 199.00, currency: 'R$', exchangeTax: 6.63 },
+      { edition: "Standard", price: 439.90, currency: 'R$', exchangeTax: 6.28 },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: "Mega Dimension", price: 29.99, currency: '$' },
+      { edition: "Standard", price: 69.99, currency: '$' },
+    ],
+  },
+};
