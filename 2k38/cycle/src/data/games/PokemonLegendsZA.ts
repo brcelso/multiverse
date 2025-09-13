a54de01b551fbe35c6216738dacbeb26afd7be75
celso <celso.sjunior@outlook.com>
2025-09-13 13:01:02 -0300

    Atualizar o PokemonLegendsZA.ts

--- 2k38/cycle/src/data/games/PokemonLegendsZA.ts
+++ 2k38/cycle/src/data/games/PokemonLegendsZA.ts
@@ -3,13 +3,13 @@ export const pokemonlegendsza = {
   Brazil: {
     'September 2025': [
       { edition: "Mega Dimension", price: 199.00, currency: 'R$', exchangeTax: 6.63 },
-      { edition: "Standard", price: 439.90, currency: 'R$', exchangeTax: 7.33 },
+      { edition: "Standard", price: 439.90, currency: 'R$', exchangeTax: 6.28 },
     ],
   },
   UnitedStates: {
     'September 2025': [
       { edition: "Mega Dimension", price: 29.99, currency: '$' },
-      { edition: "Standard", price: 59.99, currency: '$' },
+      { edition: "Standard", price: 69.99, currency: '$' },
     ],
   },
 };
