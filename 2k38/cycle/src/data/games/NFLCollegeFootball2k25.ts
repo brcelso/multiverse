7a273ac2439214acf78004674197c2b2463091db
celso <celso.sjunior@outlook.com>
2025-09-14 22:15:01 -0300

    Atualizar o NFLCollegeFootball2k25.ts

--- 2k38/cycle/src/data/games/NFLCollegeFootball2k25.ts
+++ 2k38/cycle/src/data/games/NFLCollegeFootball2k25.ts
@@ -13,7 +13,26 @@ export const nflCollegeFootball2025 = {
       { edition: 'Standard Edition', price: 349.9, currency: 'R$' },
     ],
   },
-
+  Europe: {
+    'March 2025': [
+      { edition: 'Bundle Deluxe', price: 63.99, currency: '€', discount: 60 },
+      { edition: 'Standard Edition', price: 15.99, currency: '€', discount: 80 },
+    ],
+    'July 2024': [
+      { edition: 'Bundle Deluxe', price: 159.99, currency: '€' },
+      { edition: 'Standard Edition', price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'March 2025': [
+      { edition: 'Bundle Deluxe', price: 55.99, currency: '£', discount: 60 },
+      { edition: 'Standard Edition', price: 13.99, currency: '£', discount: 80 },
+    ],
+    'July 2024': [
+      { edition: 'Bundle Deluxe', price: 139.99, currency: '£' },
+      { edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
   UnitedStates: {
     'September 2025': [
       { edition: 'Standard Edition', price: 46.99, currency: '$', discount: 33 },
@@ -25,28 +44,6 @@ export const nflCollegeFootball2025 = {
     'July 2024': [
       { edition: 'Bundle Deluxe', price: 149.99, currency: '$' },
       { edition: 'Standard Edition', price: 69.99, currency: '$' },
-    ],
-  },
-
-  Europe: {
-    'March 2025': [
-      { edition: 'Bundle Deluxe', price: 63.99, currency: '€', discount: 60 },
-      { edition: 'Standard Edition', price: 15.99, currency: '€', discount: 80 },
-    ],
-    'July 2024': [
-      { edition: 'Bundle Deluxe', price: 159.99, currency: '€' },
-      { edition: 'Standard Edition', price: 79.99, currency: '€' },
-    ],
-  },
-
-  UnitedKingdom: {
-    'March 2025': [
-      { edition: 'Bundle Deluxe', price: 55.99, currency: '£', discount: 60 },
-      { edition: 'Standard Edition', price: 13.99, currency: '£', discount: 80 },
-    ],
-    'July 2024': [
-      { edition: 'Bundle Deluxe', price: 139.99, currency: '£' },
-      { edition: 'Standard Edition', price: 69.99, currency: '£' },
     ],
   },
 };
