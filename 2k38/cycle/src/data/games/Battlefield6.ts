e94b85f04d44b8b7dacd92e8b54ad0e3fcce2ea7
celso <celso.sjunior@outlook.com>
2025-09-14 22:23:52 -0300

    Atualizar o Battlefield6.ts

--- 2k38/cycle/src/data/games/Battlefield6.ts
+++ 2k38/cycle/src/data/games/Battlefield6.ts
@@ -1,31 +1,27 @@
 export const bf6 = {
   launchDate: '2025-10-10',
-
   Brazil: {
     'July 2025': [
       { edition: 'Phantom Edition', price: 499.50, currency: 'R$', exchangeTax: 5 },
       { edition: 'Standard Edition', price: 349.90, currency: 'R$', exchangeTax: 5 },
     ],
   },
-
-  UnitedStates: {
-    'July 2025': [
-      { edition: 'Phantom Edition', price: 99.99, currency: '$' },
-      { edition: 'Standard Edition', price: 69.99, currency: '$' },
-    ],
-  },
-
   Europe: {
     'July 2025': [
       { edition: 'Phantom Edition', price: 109.99, currency: '€' },
       { edition: 'Standard Edition', price: 79.99, currency: '€' },
     ],
   },
-
   UnitedKingdom: {
     'July 2025': [
       { edition: 'Phantom Edition', price: 99.99, currency: '£' },
       { edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'July 2025': [
+      { edition: 'Phantom Edition', price: 99.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
     ],
   },
 };
