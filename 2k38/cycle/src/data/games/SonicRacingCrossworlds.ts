7edf3a95cb333a276794bce84b2be5f71fb7c79a
celso <celso.sjunior@outlook.com>
2025-09-14 22:23:24 -0300

    Criar SonicRacingCrossworlds.ts

---
+++ 2k38/cycle/src/data/games/SonicRacingCrossworlds.ts
@@ -0,0 +1,28 @@
+export const sonicracing = {
+  launchDate: '2025-09-25',
+  Brazil: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 507.50, currency: 'R$', exchangeTax: 5.63 },
+      { edition: 'Standard Edition', price: 399.90, currency: 'R$', exchangeTax: 5.71 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 89.99, currency: '€' },
+      { edition: 'Standard Edition', price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 79.99, currency: '£' },
+      { edition: 'Standard Edition', price: 64.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 89.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+
+};
