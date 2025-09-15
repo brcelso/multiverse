157747311bc020a273fb9bd1b372f346a08fba75
celso <celso.sjunior@outlook.com>
2025-09-15 20:53:27 -0300

    Criar MKLegacy.ts

---
+++ 2k38/cycle/src/data/games/MKLegacy.ts
@@ -0,0 +1,31 @@
+export const mklegacy = {
+  launchDate: '2025-10-30',
+  Brazil: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: "Legacy Collection", price: 256.41, discount: 10, currency: 'R$', exchangeTax2: 5.69 },
+      { platform: "Xbox", edition: "Legacy Collection", price: 166.45, discount: 10, currency: 'R$', exchangeTax2: 3.69 },
+      { platform: "PC", edition: "Legacy Collection", price: 121.49, discount: 10, currency: 'R$', exchangeTax2: 2.70 },
+      { platform: "Playstation 5", edition: "Legacy Collection", price: 284.90, currency: 'R$', exchangeTax2: 5.69 },
+      { platform: "Xbox", edition: "Legacy Collection", price: 184.95, currency: 'R$', exchangeTax2: 3.69 },
+      { platform: "PC", edition: "Legacy Collection", price: 134.99, currency: 'R$', exchangeTax2: 2.70 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { platform: "Playstation | Xbox", edition: "Legacy Kollection", price: 44.99, discount: 10, currency: '€' },
+      { platform: "Playstation | Xbox", edition: "Legacy Kollection", price: 49.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { platform: "Playstation | Xbox", edition: "Legacy Kollection", price: 35.99, discount: 10, currency: '£' },
+      { platform: "Playstation | Xbox", edition: "Legacy Kollection", price: 39.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { platform: "Playstation | Xbox", edition: "Legacy Kollection", price: 44.99, discount: 10, currency: '$' },
+      { platform: "Playstation | Xbox", edition: "Legacy Kollection", price: 49.99, currency: '$' },
+    ],
+  },
+};
