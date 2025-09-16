fdced8769076ceaf707af22690e4db086bad9118
celso <celso.sjunior@outlook.com>
2025-09-15 21:06:26 -0300

    Atualizar o MKLegacy.ts

--- 2k38/cycle/src/data/games/MKLegacy.ts
+++ 2k38/cycle/src/data/games/MKLegacy.ts
@@ -2,12 +2,12 @@ export const mklegacy = {
   launchDate: '2025-10-30',
   Brazil: {
     'September 2025': [
-      { platform: "Playstation 5", edition: "Legacy Collection", price: 256.41, discount: 10, currency: 'R$', exchangeTax2: 5.69 },
-      { platform: "Xbox", edition: "Legacy Collection", price: 166.45, discount: 10, currency: 'R$', exchangeTax2: 3.69 },
-      { platform: "PC", edition: "Legacy Collection", price: 121.49, discount: 10, currency: 'R$', exchangeTax2: 2.70 },
-      { platform: "Playstation 5", edition: "Legacy Collection", price: 284.90, currency: 'R$', exchangeTax2: 5.69 },
-      { platform: "Xbox", edition: "Legacy Collection", price: 184.95, currency: 'R$', exchangeTax2: 3.69 },
-      { platform: "PC", edition: "Legacy Collection", price: 134.99, currency: 'R$', exchangeTax2: 2.70 },
+      { platform: "Playstation 5", edition: "Legacy Kollection", price: 256.41, discount: 10, currency: 'R$', exchangeTax2: 5.69 },
+      { platform: "Xbox", edition: "Legacy Kollection", price: 166.45, discount: 10, currency: 'R$', exchangeTax2: 3.69 },
+      { platform: "PC", edition: "Legacy Kollection", price: 121.49, discount: 10, currency: 'R$', exchangeTax2: 2.70 },
+      { platform: "Playstation 5", edition: "Legacy Kollection", price: 284.90, currency: 'R$', exchangeTax2: 5.69 },
+      { platform: "Xbox", edition: "Legacy Kollection", price: 184.95, currency: 'R$', exchangeTax2: 3.69 },
+      { platform: "PC", edition: "Legacy Kollection", price: 134.99, currency: 'R$', exchangeTax2: 2.70 },
     ],
   },
   Europe: {
