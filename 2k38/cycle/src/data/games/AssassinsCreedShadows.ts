4040a724e1e54729287a77acb3a8a956e2e4d420
celso <celso.sjunior@outlook.com>
2025-09-16 16:19:28 -0300

    Atualizar o AssassinsCreedShadows.ts

--- 2k38/cycle/src/data/games/AssassinsCreedShadows.ts
+++ 2k38/cycle/src/data/games/AssassinsCreedShadows.ts
@@ -1,8 +1,8 @@
 export const assassinsCreedShadows = {
   launchDate: '2025-03-20',
-
   Brazil: {
     'September 2025': [
+      { edition: 'Claws of Awaji', price: 124.90, currency: 'R$', exchangeTax2: 4.99 },
       { edition: 'Premium Edition', price: 549.90, currency: 'R$', exchangeTax2: 4.99 },
       { edition: 'Deluxe Edition', price: 301.43, currency: 'R$', discount: 33, exchangeTax: 4.99 },
       { edition: 'Standard Edition', price: 234.43, currency: 'R$', discount: 33, exchangeTax: 4.98 },
@@ -22,6 +22,7 @@ export const assassinsCreedShadows = {
   },
   Europe: {
     'September 2025': [
+      { edition: 'Claws of Awaji', price: 24.99, currency: '€' },
       { edition: 'Premium Edition', price: 109.99, currency: '€' },
       { edition: 'Deluxe Edition', price: 66.99, discount: 33, currency: '€' },
       { edition: 'Standard Edition', price: 53.59, discount: 33, currency: '€' },
@@ -33,6 +34,7 @@ export const assassinsCreedShadows = {
   },
   UnitedKingdom: {
     'September 2025': [
+      { edition: 'Claws of Awaji', price: 20.99, currency: '£' },
       { edition: 'Premium Edition', price: 99.99, currency: '£' },
       { edition: 'Deluxe Edition', price: 56.94, discount: 33, currency: '£' },
       { edition: 'Standard Edition', price: 46.89, discount: 33, currency: '£' },
@@ -44,6 +46,7 @@ export const assassinsCreedShadows = {
   },
   UnitedStates: {
     'September 2025': [
+      { edition: 'Claws of Awaji', price: 24.99, currency: '$' },
       { edition: 'Premium Edition', price: 109.99, currency: '$' },
       { edition: 'Deluxe Edition', price: 60.29, discount: 33, currency: '$' },
       { edition: 'Standard Edition', price: 46.89, discount: 33, currency: '$' },
