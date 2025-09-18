9adeafe8751f4dbd0c23d5a1bb149e2ebcd57f69
celso <celso.sjunior@outlook.com>
2025-09-17 21:36:25 -0300

    Criar FFTactics.ts

---
+++ 2k38/cycle/src/data/games/FFTactics.ts
@@ -0,0 +1,27 @@
+export const fftactics = {
+  launchDate: '2025-09-30',
+  Brazil: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 339.90, currency: 'R$', exchangeTax: 5.66 },
+      { edition: 'Standard Edition', price: 284.90, currency: 'R$', exchangeTax: 5.69 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 59.99, currency: '€' },
+      { edition: 'Standard Edition', price: 49.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 54.99, currency: '£' },
+      { edition: 'Standard Edition', price: 44.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 59.99, currency: '$' },
+      { edition: 'Standard Edition', price: 49.99, currency: '$' },
+    ],
+  },
+};
