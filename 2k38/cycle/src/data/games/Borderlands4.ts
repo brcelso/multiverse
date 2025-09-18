dd537c26491a6f2b96049e87f490935b28698ec3
celso <celso.sjunior@outlook.com>
2025-09-17 22:08:27 -0300

    Atualizar o Borderlands4.ts

--- 2k38/cycle/src/data/games/Borderlands4.ts
+++ 2k38/cycle/src/data/games/Borderlands4.ts
@@ -2,6 +2,7 @@ export const borderlands4 = {
   launchDate: '2025-09-12',
   Brazil: {
     'September 2025': [
+      { edition: 'Standard Edition', price: 252.47, discount: 32.5, currency: 'R$', exchangeTax: 3.60 },
       { edition: 'Standard Edition', price: 283.81, discount: 25, currency: 'R$', exchangeTax: 4.05 },
     ],
     'August 2025': [
