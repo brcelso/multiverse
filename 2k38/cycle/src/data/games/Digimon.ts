05754ccefd8f1a7caadedc5a6a90c1d9c48aad3e
celso <celso.sjunior@outlook.com>
2025-09-19 18:48:49 -0300

    Atualizar o Digimon.ts

--- 2k38/cycle/src/data/games/Digimon.ts
+++ 2k38/cycle/src/data/games/Digimon.ts
@@ -2,27 +2,30 @@ export const digimon = {
   launchDate: '2025-10-03',
   Brazil: {
     'September 2025': [
-      { edition: 'Deluxe Edition', price: 507.50, currency: 'R$', exchangeTax: 5.63 },
+      { edition: 'Ultimate Edition', price: 683.90, currency: 'R$', exchangeTax: 5.70 },
+      { edition: 'Deluxe Edition', price: 569.90, currency: 'R$', exchangeTax: 5.70 },
       { edition: 'Standard Edition', price: 399.90, currency: 'R$', exchangeTax: 5.71 },
     ],
   },
   Europe: {
     'September 2025': [
-      { edition: 'Deluxe Edition', price: 89.99, currency: '€' },
+      { edition: 'Ultimate Edition', price: 119.99, currency: '€' },
+      { edition: 'Deluxe Edition', price: 99.99, currency: '€' },
       { edition: 'Standard Edition', price: 69.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'September 2025': [
-      { edition: 'Deluxe Edition', price: 79.99, currency: '£' },
-      { edition: 'Standard Edition', price: 64.99, currency: '£' },
+      { edition: 'Ultimate Edition', price: 104.99, currency: '£' },
+      { edition: 'Deluxe Edition', price: 84.99, currency: '£' },
+      { edition: 'Standard Edition', price: 54.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'September 2025': [
+      { edition: 'Ultimate Edition', price: 119.99, currency: '$' },
       { edition: 'Deluxe Edition', price: 99.99, currency: '$' },
       { edition: 'Standard Edition', price: 69.99, currency: '$' },
     ],
   },
-
 };
