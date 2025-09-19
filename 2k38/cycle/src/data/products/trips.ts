37cc8076578f414b2b5c43ac290dbfe907b4938b
celso <celso.sjunior@outlook.com>
2025-09-19 17:23:55 -0300

    Atualizar o trips.ts

--- 2k38/cycle/src/data/products/trips.ts
+++ 2k38/cycle/src/data/products/trips.ts
@@ -3,6 +3,83 @@ export const trips = {
   Argentina: {
     'September 2025': [
       {
+        edition: 'Uber',
+        price: 13204,
+        exchangeTax: 257.38,
+        convertedPrice: 51.30,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Mafalda',
+        price: 25800,
+        exchangeTax: 270,
+        convertedPrice: 95.55,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Antiche Tentazioni',
+        price: 5600,
+        exchangeTax: 270,
+        convertedPrice: 20.74,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Choripaneria Propina',
+        price: 4000,
+        exchangeTax: 270,
+        convertedPrice: 14.81,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Choripaneria',
+        price: 37000,
+        exchangeTax: 270,
+        convertedPrice: 137.03,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Uber',
+        price: 5265,
+        exchangeTax: 257.20,
+        convertedPrice: 20.47,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Heineken',
+        price: 6400,
+        exchangeTax: 250,
+        convertedPrice: 25.60,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Subte',
+        price: 3405.78,
+        exchangeTax: 270,
+        convertedPrice: 12.61,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Whoopies',
+        price: 28500,
+        exchangeTax: 250,
+        convertedPrice: 114,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Dia',
+        price: 6298,
+        exchangeTax: 273,
+        convertedPrice: 23.06,
+        currency: 'ARS'
+      },
+      {
+        edition: 'Bella Gamba',
+        price: 33200,
+        exchangeTax: 273,
+        convertedPrice: 121.61,
+        currency: 'ARS'
+      },
+      {
         edition: 'Primero Cafe',
         price: 13600,
         exchangeTax: 273,
@@ -11,10 +88,10 @@ export const trips = {
       },
       {
         edition: 'Uber',
-        price: 43.89,
+        price: 11369,
         exchangeTax: 259,
-        convertedPrice: 11369,
-        currency: 'R$'
+        convertedPrice: 43.89,
+        currency: 'ARS'
       },
       {
         edition: 'Alfajor',
