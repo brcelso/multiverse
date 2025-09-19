accdda9cca32224074071c3db5080f317711e062
celso <celso.sjunior@outlook.com>
2025-09-19 18:43:54 -0300

    Criar Digimon.ts

---
+++ 2k38/cycle/src/data/games/Digimon.ts
@@ -0,0 +1,28 @@
+export const digimon = {
+  launchDate: '2025-10-03',
+  Brazil: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 507.50, currency: 'R$', exchangeTax: 5.63 },
+      { edition: 'Standard Edition', price: 399.90, currency: 'R$', exchangeTax: 5.71 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 89.99, currency: '€' },
+      { edition: 'Standard Edition', price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 79.99, currency: '£' },
+      { edition: 'Standard Edition', price: 64.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 99.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+
+};
