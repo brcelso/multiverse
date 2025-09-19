45f5eba9a36a0df2cd07caa4dd0903b7a5edd61e
celso <celso.sjunior@outlook.com>
2025-09-19 17:26:41 -0300

    Atualizar o trips.ts

--- 2k38/cycle/src/data/products/trips.ts
+++ 2k38/cycle/src/data/products/trips.ts
@@ -3,126 +3,126 @@ export const trips = {
   Argentina: {
     'September 2025': [
       {
-        edition: 'Uber',
+        edition: 'Uber 19-09',
         price: 13204,
         exchangeTax: 257.38,
         convertedPrice: 51.30,
         currency: 'ARS'
       },
       {
-        edition: 'Mafalda',
+        edition: 'Mafalda 19-09',
         price: 25800,
         exchangeTax: 270,
         convertedPrice: 95.55,
         currency: 'ARS'
       },
       {
-        edition: 'Antiche Tentazioni',
+        edition: 'Antiche Tentazioni 19-09',
         price: 5600,
         exchangeTax: 270,
         convertedPrice: 20.74,
         currency: 'ARS'
       },
       {
-        edition: 'Choripaneria Propina',
+        edition: 'Choripaneria Propina 19-09',
         price: 4000,
         exchangeTax: 270,
         convertedPrice: 14.81,
         currency: 'ARS'
       },
       {
-        edition: 'Choripaneria',
+        edition: 'Choripaneria 19-09',
         price: 37000,
         exchangeTax: 270,
         convertedPrice: 137.03,
         currency: 'ARS'
       },
       {
-        edition: 'Uber',
+        edition: 'Uber 19-09',
         price: 5265,
         exchangeTax: 257.20,
         convertedPrice: 20.47,
         currency: 'ARS'
       },
       {
-        edition: 'Heineken',
+        edition: 'Heineken 19-09',
         price: 6400,
         exchangeTax: 250,
         convertedPrice: 25.60,
         currency: 'ARS'
       },
       {
-        edition: 'Subte',
+        edition: 'Subte 19-09',
         price: 3405.78,
         exchangeTax: 270,
         convertedPrice: 12.61,
         currency: 'ARS'
       },
       {
-        edition: 'Whoopies',
+        edition: 'Whoopies 19-09',
         price: 28500,
         exchangeTax: 250,
         convertedPrice: 114,
         currency: 'ARS'
       },
       {
-        edition: 'Dia',
+        edition: 'Dia 18-09',
         price: 6298,
         exchangeTax: 273,
         convertedPrice: 23.06,
         currency: 'ARS'
       },
       {
-        edition: 'Bella Gamba',
+        edition: 'Bella Gamba 18-09',
         price: 33200,
         exchangeTax: 273,
         convertedPrice: 121.61,
         currency: 'ARS'
       },
       {
-        edition: 'Primero Cafe',
+        edition: 'Primero Cafe 18-09',
         price: 13600,
         exchangeTax: 273,
         convertedPrice: 49.81,
         currency: 'ARS'
       },
       {
-        edition: 'Uber',
+        edition: 'Uber 18-09',
         price: 11369,
         exchangeTax: 259,
         convertedPrice: 43.89,
         currency: 'ARS'
       },
       {
-        edition: 'Alfajor',
+        edition: 'Alfajor 18-09',
         price: 13.91,
         exchangeTax: 273,
         convertedPrice: 3800,
         currency: 'R$'
       },
       {
-        edition: 'Sube',
+        edition: 'Sube 18-09',
         price: 41.90,
         exchangeTax: 273,
         convertedPrice: 11140,
         currency: 'R$'
       },
       {
-        edition: 'Agua',
+        edition: 'Agua 18-09',
         price: 2400,
         exchangeTax: 273,
         convertedPrice: 8.79,
         currency: 'ARS'
       },
       {
-        edition: 'MC Donalds - Almuerzo - 18-09',
+        edition: 'MC Donalds 18-09',
         price: 21700,
         exchangeTax: 273,
         convertedPrice: 79.48,
         currency: 'ARS'
       },
       {
-        edition: 'Cambio Banco de La Nacion',
+        edition: 'Cambio Banco de La Nacion 18-09',
         price: 382200,
         exchangeTax: 273,
         convertedPrice: 1400,
