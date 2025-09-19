4a67d5105208347dfb57479b5e43938c7b6bbf0f
celso <celso.sjunior@outlook.com>
2025-09-19 18:11:36 -0300

    Criar NinjaGaiden4.ts

---
+++ 2k38/cycle/src/data/games/NinjaGaiden4.ts
@@ -0,0 +1,27 @@
+export const ninja4 = {
+  launchDate: '2025-10-20',
+  Brazil: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 424.00, currency: 'R$', exchangeTax: 4.71 },
+      { edition: 'Standard Edition', price: 399.90, currency: 'R$', exchangeTax: 5.71 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 89.99, currency: '€' },
+      { edition: 'Standard Edition', price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 79.99, currency: '£' },
+      { edition: 'Standard Edition', price: 59.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Deluxe Edition', price: 89.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+};
