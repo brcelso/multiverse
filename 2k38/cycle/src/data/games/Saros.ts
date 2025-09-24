3bdd7a8b054686f5e377aa2cc936750dfc717f3f
celso <celso.sjunior@outlook.com>
2025-09-24 18:10:48 -0300

    Criar Saros.ts

---
+++ 2k38/cycle/src/data/games/Saros.ts
@@ -0,0 +1,24 @@
+export const saros = {
+  launchDate: '2026-03-20',
+
+  Brazil: {
+    'March 2026': [
+      { platform: "Playstation 5", price: 0, edition: "Standard Edition", currency: 'R$', exchangeTax: 0 },
+    ],
+  },
+  Europe: {
+    'March 2026': [
+      { platform: "Playstation 5", price: 0, edition: "Standard Edition", currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'March 2026': [
+      { platform: "Playstation 5", price: 0, edition: "Standard Edition", currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'March 2026': [
+      { platform: "Playstation 5", price: 0, edition: "Standard Edition", currency: '$' },
+    ],
+  },
+};
