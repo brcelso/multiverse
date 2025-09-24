a7c39914915e47b4d4b042878a306797aa0a6c49
celso <celso.sjunior@outlook.com>
2025-09-23 23:09:28 -0300

    Criar DayZ.ts

---
+++ 2k38/cycle/src/data/games/DayZ.ts
@@ -0,0 +1,65 @@
+export const dayz = {
+  launchDate: '2019-05-30',
+  Brazil: {
+    'September 2025': [
+      { edition: 'Cool Edition', price: 239.94, discount: 40, currency: 'R$', exchangeTax: 5.71 },
+      { edition: 'Frostline', price: 96.41, discount: 33, currency: 'R$', exchangeTax: 5.33 },
+      { edition: 'Standard Edition', price: 156.75, discount: 50, currency: 'R$', exchangeTax: 5.70 },
+    ],
+    'June 2025': [
+      { edition: 'Standard Edition', price: 313.50, increase: 6.5, totalIncrease: 105, currency: 'R$', exchangeTax: 5.70},
+    ],
+    'January 2025': [
+      { edition: 'Standard Edition', price: 294.90, increase: 93, currency: 'R$', exchangeTax: 5.36 },
+    ],
+    'November 2024': [
+      { edition: 'Cool Edition', price: 399.90, currency: 'R$', exchangeTax: 5.71 },
+      { edition: 'Frostline', price: 143.90, currency: 'R$', exchangeTax: 5.33 },
+    ],
+    'May 2019': [
+      { edition: 'Standard Edition', price: 153.00, currency: 'R$', exchangeTax: 2.78 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: 'Cool Edition', price: 41.99, discount: 40, currency: '€' },
+      { edition: 'Frostline', price: 18.08, discount: 33, currency: '€' },
+      { edition: 'Standard Edition', price: 27.49, discount: 50, currency: '€' },
+    ],
+    'January 2025': [
+      { edition: 'Cool Edition', price: 69.99, currency: '€' },
+      { edition: 'Frostline', price: 26.99, currency: '€' },
+    ],
+    'May 2019': [
+      { edition: 'Standard Edition', price: 54.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Cool Edition', price: 34.79, discount: 40, currency: '£' },
+      { edition: 'Frostline', price: 14.73, discount: 33, currency: '£' },
+      { edition: 'Standard Edition', price: 23.99, discount: 50, currency: '£' },
+    ],
+    'January 2025': [
+      { edition: 'Cool Edition', price: 57.99, currency: '£' },
+      { edition: 'Frostline', price: 21.99, currency: '£' },
+    ],
+    'May 2019': [
+      { edition: 'Standard Edition', price: 47.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Cool Edition', price: 41.99, discount: 40, currency: '$' },
+      { edition: 'Frostline', price: 18.08, discount: 33, currency: '$' },
+      { edition: 'Standard Edition', price: 27.49, discount: 50, currency: '$' },
+    ],
+    'January 2025': [
+      { edition: 'Cool Edition', price: 69.99, currency: '$' },
+      { edition: 'Frostline', price: 26.99, currency: '$' },
+    ],
+    'May 2019': [
+      { edition: 'Standard Edition', price: 54.99, currency: '$' },
+    ],
+  },
+};
