46ede387b7af2f77a14d03331c48c06739f2426c
celso <celso.sjunior@outlook.com>
2025-09-24 18:40:23 -0300

    Criar Wolverine.ts

---
+++ 2k38/cycle/src/data/games/Wolverine.ts
@@ -0,0 +1,24 @@
+export const wolverine = {
+  launchDate: '2026-09-22',
+
+  Brazil: {
+    'September 2026': [
+      { platform: "Playstation 5", price: 0, edition: "Standard Edition", currency: 'R$', exchangeTax: 0 },
+    ],
+  },
+  Europe: {
+    'September 2026': [
+      { platform: "Playstation 5", price: 0, edition: "Standard Edition", currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2026': [
+      { platform: "Playstation 5", price: 0, edition: "Standard Edition", currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2026': [
+      { platform: "Playstation 5", price: 0, edition: "Standard Edition", currency: '$' },
+    ],
+  },
+};
