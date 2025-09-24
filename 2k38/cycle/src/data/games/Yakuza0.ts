d656797a6b625cd241a1d289d65c1fe570b42e7b
celso <celso.sjunior@outlook.com>
2025-09-24 17:44:58 -0300

    Atualizar o Yakuza0.ts

--- 2k38/cycle/src/data/games/Yakuza0.ts
+++ 2k38/cycle/src/data/games/Yakuza0.ts
@@ -3,25 +3,25 @@ export const yakuza0 = {
 
   Brazil: {
     'September 2025': [
-      { platform: "Playstation 5 | XBox", price: 285.44, currency: 'R$', exchangeTax: 5.71 },
+      { platform: "Playstation 5 | XBox", edition: "Director Cut", price: 285.44, currency: 'R$', exchangeTax: 5.71 },
     ],
   },
   Europe: {
     'September 2025': [
-      { platform: "Playstation 5 | XBox", price: 59.99, currency: '€' },
+      { platform: "Playstation 5 | XBox", edition: "Director Cut", price: 59.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'April 2025': [
-      { platform: "Switch 2", price: 44.95, currency: '£' },
+      { platform: "Switch 2", edition: "Director Cut", price: 44.95, currency: '£' },
     ],
   },
   UnitedStates: {
     'September 2025': [
-      { platform: "Switch 2", price: 37.00, discount: 25, currency: '$' },
+      { platform: "Switch 2", edition: "Director Cut", price: 37.00, discount: 25, currency: '$' },
     ],
     'April 2025': [
-      { platform: "Switch 2", price: 49.99, currency: '$' },
+      { platform: "Switch 2", edition: "Director Cut", price: 49.99, currency: '$' },
     ],
   },
 };
