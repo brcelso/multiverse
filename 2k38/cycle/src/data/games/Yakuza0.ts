76633001845b0ca4f074573c5e676d4368b3b1e2
celso <celso.sjunior@outlook.com>
2025-09-24 17:29:52 -0300

    Criar Yakuza0.ts

---
+++ 2k38/cycle/src/data/games/Yakuza0.ts
@@ -0,0 +1,30 @@
+export const marioKartWorldSW2 = {
+  launchDate: '2025-06-05',
+
+  Brazil: {
+    'September 2025': [
+      { price: 404.98, currency: 'R$', discount: 19, exchangeTax: 5.06 },
+    ],
+    'June 2025': [
+      { price: 499.9, currency: 'R$', exchangeTax: 6.24 },
+    ],
+  },
+  Europe: {
+    'June 2025': [
+      { price: 89.99, currency: '€' },
+      { price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'June 2025': [
+      { price: 74.99, currency: '£' },
+      { price: 66.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'June 2025': [
+      { price: 79.99, currency: '$' },
+      { price: 69.99, currency: '$' },
+    ],
+  },
+};
