a5ee00a98e59797cf078ebcee816e185ea565e1b
celso <celso.sjunior@outlook.com>
2025-09-24 22:52:20 -0300

    Atualizar o AssassinsCreedShadows.ts

--- 2k38/cycle/src/data/games/AssassinsCreedShadows.ts
+++ 2k38/cycle/src/data/games/AssassinsCreedShadows.ts
@@ -2,6 +2,8 @@ export const assassinsCreedShadows = {
   launchDate: '2025-03-20',
   Brazil: {
     'September 2025': [
+      { edition: 'Deluxe Edition', price: 269.94, currency: 'R$', discount: 40, exchangeTax: 4.99 },
+      { edition: 'Standard Edition', price: 209.94, currency: 'R$', discount: 40, exchangeTax: 4.98 },
       { edition: 'Claws of Awaji', price: 124.90, currency: 'R$', exchangeTax2: 4.99 },
       { edition: 'Premium Edition', price: 549.90, currency: 'R$', exchangeTax2: 4.99 },
       { edition: 'Deluxe Edition', price: 301.43, currency: 'R$', discount: 33, exchangeTax: 4.99 },
@@ -22,6 +24,8 @@ export const assassinsCreedShadows = {
   },
   Europe: {
     'September 2025': [
+      { edition: 'Deluxe Edition', price: 59.99, discount: 40, currency: '€' },
+      { edition: 'Standard Edition', price: 47.99, discount: 40, currency: '€' },
       { edition: 'Claws of Awaji', price: 24.99, currency: '€' },
       { edition: 'Premium Edition', price: 109.99, currency: '€' },
       { edition: 'Deluxe Edition', price: 66.99, discount: 33, currency: '€' },
@@ -34,6 +38,8 @@ export const assassinsCreedShadows = {
   },
   UnitedKingdom: {
     'September 2025': [
+      { edition: 'Deluxe Edition', price: 50.99, discount: 40, currency: '£' },
+      { edition: 'Standard Edition', price: 41.99, discount: 40, currency: '£' },
       { edition: 'Claws of Awaji', price: 20.99, currency: '£' },
       { edition: 'Premium Edition', price: 99.99, currency: '£' },
       { edition: 'Deluxe Edition', price: 56.94, discount: 33, currency: '£' },
@@ -46,6 +52,8 @@ export const assassinsCreedShadows = {
   },
   UnitedStates: {
     'September 2025': [
+      { edition: 'Deluxe Edition', price: 53.99, discount: 40, currency: '$' },
+      { edition: 'Standard Edition', price: 41.99, discount: 40, currency: '$' },
       { edition: 'Claws of Awaji', price: 24.99, currency: '$' },
       { edition: 'Premium Edition', price: 109.99, currency: '$' },
       { edition: 'Deluxe Edition', price: 60.29, discount: 33, currency: '$' },
