52984ba17ba644a8dcbc76474694841f3a9365b0
celso <celso.sjunior@outlook.com>
2025-09-26 14:33:08 -0300

    Criar Flight2024.ts

---
+++ 2k38/cycle/src/data/games/Flight2024.ts
@@ -0,0 +1,36 @@
+export const flight2024 = {
+  launchDate: '2025-12-08',
+  Brazil: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Aviator Edition', price: 859.00, currency: 'R$', exchangeTax: 4.99 },
+      { platform: "Playstation 5", edition: 'Premium Deluxe Edition', price: 559.00, currency: 'R$', exchangeTax: 4.30 },
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 429.00, currency: 'R$', exchangeTax: 4.29 },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 299.00, currency: 'R$', exchangeTax: 4.27 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Aviator Edition', price: 219.99, currency: '€' },
+      { platform: "Playstation 5", edition: 'Premium Deluxe Edition', price: 139.99, currency: '€' },
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 109.99, currency: '€' },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Aviator Edition', price: 199.99, currency: '£' },
+      { platform: "Playstation 5", edition: 'Premium Deluxe Edition', price: 129.99, currency: '£' },
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 99.99, currency: '£' },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { platform: "Playstation 5", edition: 'Aviator Edition', price: 199.99, currency: '$' },
+      { platform: "Playstation 5", edition: 'Premium Deluxe Edition', price: 129.99, currency: '$' },
+    ],
+      { platform: "Playstation 5", edition: 'Deluxe Edition', price: 99.99, currency: '$' },
+      { platform: "Playstation 5", edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+};
