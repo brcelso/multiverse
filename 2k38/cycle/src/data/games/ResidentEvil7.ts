244b8d08f0e93178a7bf91c01497c932deac191a
celso <celso.sjunior@outlook.com>
2025-09-27 20:39:48 -0300

    Atualizar o ResidentEvil7.ts

--- 2k38/cycle/src/data/games/ResidentEvil7.ts
+++ 2k38/cycle/src/data/games/ResidentEvil7.ts
@@ -1,6 +1,5 @@
 export const residentevil7 = {
   launchDate: '2022-06-13',
-
   Brazil: {
     'June 2025': [
       { edition: 'Gold Edition', price: 41.22, discount: 75, currency: 'R$', exchangeTax: 5 },
@@ -11,18 +10,6 @@ export const residentevil7 = {
       { edition: 'Standard Edition', price: 99.50, currency: 'R$' },
     ],
   },
-
-  UnitedStates: {
-    'June 2025': [
-      { edition: 'Gold Edition', price: 9.99, discount: 75, currency: '$' },
-      { edition: 'Standard Edition', price: 7.99, discount: 60, currency: '$' },
-    ],
-    'June 2022': [
-      { edition: 'Gold Edition', price: 39.99, currency: '$' },
-      { edition: 'Standard Edition', price: 19.99, currency: '$' },
-    ],
-  },
-
   Europe: {
     'June 2025': [
       { edition: 'Gold Edition', price: 9.99, discount: 75, currency: '€' },
@@ -33,7 +20,6 @@ export const residentevil7 = {
       { edition: 'Standard Edition', price: 19.99, currency: '€' },
     ],
   },
-
   UnitedKingdom: {
     'June 2025': [
       { edition: 'Gold Edition', price: 6.39, discount: 75, currency: '£' },
@@ -42,6 +28,16 @@ export const residentevil7 = {
     'June 2022': [
       { edition: 'Gold Edition', price: 34.99, currency: '£' },
       { edition: 'Standard Edition', price: 15.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'June 2025': [
+      { edition: 'Gold Edition', price: 9.99, discount: 75, currency: '$' },
+      { edition: 'Standard Edition', price: 7.99, discount: 60, currency: '$' },
+    ],
+    'June 2022': [
+      { edition: 'Gold Edition', price: 39.99, currency: '$' },
+      { edition: 'Standard Edition', price: 19.99, currency: '$' },
     ],
   },
 };
