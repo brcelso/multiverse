57b0e8637cb7a67be27e710752f60aff7d63f617
celso <celso.sjunior@outlook.com>
2025-09-27 04:28:27 -0300

    Atualizar o TombRaiderTrilogy.ts

--- 2k38/cycle/src/data/games/TombRaiderTrilogy.ts
+++ 2k38/cycle/src/data/games/TombRaiderTrilogy.ts
@@ -1,24 +1,36 @@
 export const tombraidertrilogy = {
   launchDate: '2021-03-18',
   Brazil: {
+    'September 2025': [
+      { edition: 'Definitive Survivor Trilogy', price: 62.47, discount: 75, currency: 'R$', exchangeTax2: 5 },
+    ],
     'March 2021': [
       { edition: 'Definitive Survivor Trilogy', price: 99.80, discount: 60, currency: 'R$', exchangeTax2: 5 },
       { edition: 'Definitive Survivor Trilogy', price: 249.90, currency: 'R$', exchangeTax2: 5 },
     ],
   },
   Europe: {
+    'September 2025': [
+      { edition: 'Definitive Survivor Trilogy', price: 12.49, discount: 75, currency: '€' },
+    ],
     'March 2021': [
       { edition: 'Definitive Survivor Trilogy', price: 19.99, discount: 60, currency: '€' },
       { edition: 'Definitive Survivor Trilogy', price: 49.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Definitive Survivor Trilogy', price: 11.24, discount: 75, currency: '£' },
+    ],
     'March 2021': [
       { edition: 'Definitive Survivor Trilogy', price: 17.99, discount: 60, currency: '£' },
       { edition: 'Definitive Survivor Trilogy', price: 44.99, currency: '£' },
     ],
   },
   UnitedStates: {
+    'September 2025': [
+      { edition: 'Definitive Survivor Trilogy', price: 12.49, discount: 75, currency: '$' },
+    ],
     'March 2021': [
       { edition: 'Definitive Survivor Trilogy', price: 19.99, discount: 60, currency: '$' },
       { edition: 'Definitive Survivor Trilogy', price: 49.99, currency: '$' },
