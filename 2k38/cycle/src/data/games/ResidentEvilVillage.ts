c637107167ef4ea51977402b6584d0772410a0d6
celso <celso.sjunior@outlook.com>
2025-09-27 20:37:34 -0300

    Atualizar o ResidentEvilVillage.ts

--- 2k38/cycle/src/data/games/ResidentEvilVillage.ts
+++ 2k38/cycle/src/data/games/ResidentEvilVillage.ts
@@ -1,6 +1,5 @@
 export const residentevilvillage = {
   launchDate: '2022-10-28',
-
   Brazil: {
     'August 2025': [
       { edition: 'Standard Edition', price: 46.12, discount: 75, currency: 'R$', exchangeTax: 5 },
@@ -16,7 +15,30 @@ export const residentevilvillage = {
       { edition: 'Standard Edition', price: 184.50, currency: 'R$' },
     ],
   },
-
+  Europe: {
+    'June 2025': [
+      { edition: 'Gold Edition Bundle', price: 19.99, discount: 75, currency: '€' },
+      { edition: 'Gold Edition', price: 14.99, discount: 70, currency: '€' },
+      { edition: 'Standard Edition', price: 9.99, discount: 75, currency: '€' },
+    ],
+    'October 2022': [
+      { edition: 'Gold Edition Bundle', price: 79.99, currency: '€' },
+      { edition: 'Gold Edition', price: 49.99, currency: '€' },
+      { edition: 'Standard Edition', price: 39.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'June 2025': [
+      { edition: 'Gold Edition Bundle', price: 16.24, discount: 75, currency: '£' },
+      { edition: 'Gold Edition', price: 13.49, discount: 70, currency: '£' },
+      { edition: 'Standard Edition', price: 8.74, discount: 75, currency: '£' },
+    ],
+    'October 2022': [
+      { edition: 'Gold Edition Bundle', price: 64.99, currency: '£' },
+      { edition: 'Gold Edition', price: 44.99, currency: '£' },
+      { edition: 'Standard Edition', price: 34.99, currency: '£' },
+    ],
+  },
   UnitedStates: {
     'June 2025': [
       { edition: 'Gold Edition Bundle', price: 19.99, discount: 75, currency: '$' },
@@ -30,29 +52,4 @@ export const residentevilvillage = {
     ],
   },
 
-  Europe: {
-    'June 2025': [
-      { edition: 'Gold Edition Bundle', price: 19.99, discount: 75, currency: '€' },
-      { edition: 'Gold Edition', price: 14.99, discount: 70, currency: '€' },
-      { edition: 'Standard Edition', price: 9.99, discount: 75, currency: '€' },
-    ],
-    'October 2022': [
-      { edition: 'Gold Edition Bundle', price: 79.99, currency: '€' },
-      { edition: 'Gold Edition', price: 49.99, currency: '€' },
-      { edition: 'Standard Edition', price: 39.99, currency: '€' },
-    ],
-  },
-
-  UnitedKingdom: {
-    'June 2025': [
-      { edition: 'Gold Edition Bundle', price: 16.24, discount: 75, currency: '£' },
-      { edition: 'Gold Edition', price: 13.49, discount: 70, currency: '£' },
-      { edition: 'Standard Edition', price: 8.74, discount: 75, currency: '£' },
-    ],
-    'October 2022': [
-      { edition: 'Gold Edition Bundle', price: 64.99, currency: '£' },
-      { edition: 'Gold Edition', price: 44.99, currency: '£' },
-      { edition: 'Standard Edition', price: 34.99, currency: '£' },
-    ],
-  },
 };
