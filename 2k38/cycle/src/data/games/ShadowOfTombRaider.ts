40ce0b3d6c1a9e92decab40249bbee94be52e568
celso <celso.sjunior@outlook.com>
2025-09-26 22:33:12 -0300

    Atualizar o ShadowOfTombRaider.ts

--- 2k38/cycle/src/data/games/ShadowOfTombRaider.ts
+++ 2k38/cycle/src/data/games/ShadowOfTombRaider.ts
@@ -1,29 +1,26 @@
 export const shadowOfTheTombRaider = {
   launchDate: '2018-09-01',
-
   Brazil: {
+    'September 2025': [
+      { edition: 'Definitive Edition', discount: 80, price: 45.98, currency: 'R$', exchangeTax: 5.74 },
+    ],
+    'June 2025': [
+      { edition: 'Definitive Edition', price: 229.90, increase: 15, currency: 'R$', exchangeTax: 5.74 },
+    ],
+    'April 2025': [
+      { edition: 'Definitive Edition', price: 39.90, currency: 'R$', discount: 80 },
+    ],
     'September 2018': [
       { edition: 'Croft Edition', price: 369.90, currency: 'R$' },
       { edition: 'Deluxe Edition', price: 289.90, currency: 'R$' },
       { edition: 'Standard Edition', price: 249.90, currency: 'R$', exchangeTax2: 4.16 },
       { edition: 'Definitive Edition', price: 199.50, currency: 'R$' },
     ],
-    'April 2025': [
-      { edition: 'Definitive Edition', price: 39.90, currency: 'R$', discount: 80 },
-    ],
   },
-
-  UnitedStates: {
-    'September 2018': [
-      { edition: 'Ultimate Collector Edition', price: 199.99, currency: '$' },
-      { edition: 'Croft Edition', price: 89.99, currency: '$' },
-      { edition: 'Standard Edition', price: 59.99, currency: '$' },
-      { edition: 'Definitive Edition', price: 39.99, currency: '$' },
-      { edition: 'Definitive Edition', price: 7.99, currency: '$', discount: 80 },
-    ],
-  },
-
   Europe: {
+    'September 2025': [
+      { edition: 'Definitive Edition', price: 7.99, currency: '€', discount: 80 },
+    ],
     'November 2019': [
       { edition: 'Definitive Edition', price: 39.99, currency: '€' },
       { edition: 'Definitive Edition', price: 7.99, currency: '€', discount: 80 },
@@ -32,15 +29,27 @@ export const shadowOfTheTombRaider = {
       { edition: 'Standard Edition', price: 59.99, currency: '€' },
     ],
   },
-
   UnitedKingdom: {
-    'November 2019': [
-      { edition: 'Definitive Edition', price: 49.99, currency: '£' },
+    'September 2025': [
+      { edition: 'Definitive Edition', price: 6.99, discount: 80, currency: '£' },
     ],
     'September 2018': [
-      { edition: 'Definitive Edition', price: 34.99, currency: '£' },
-      { edition: 'Definitive Edition', price: 54.99, currency: '£' },
       { edition: 'Definitive Edition', price: 6.99, currency: '£', discount: 80 },
+      { edition: 'Definitive Edition', price: 34.99, currency: '£' },
+      { edition: 'Standard Edition', price: 54.99, currency: '£' },
+
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Definitive Edition', price: 7.99, discount: 80, currency: '$' },
+    ],
+    'September 2018': [
+      { edition: 'Ultimate Collector Edition', price: 199.99, currency: '$' },
+      { edition: 'Croft Edition', price: 89.99, currency: '$' },
+      { edition: 'Standard Edition', price: 59.99, currency: '$' },
+      { edition: 'Definitive Edition', price: 39.99, currency: '$' },
+      { edition: 'Definitive Edition', price: 7.99, currency: '$', discount: 80 },
     ],
   },
 };
