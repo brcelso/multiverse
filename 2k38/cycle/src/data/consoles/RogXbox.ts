4562b98d9d176327446089016f8315c1c9b13dd2
celso <celso.sjunior@outlook.com>
2025-09-26 22:08:24 -0300

    Criar RogXbox.ts

---
+++ 2k38/cycle/src/data/consoles/RogXbox.ts
@@ -0,0 +1,33 @@
+export const rogxbox = {
+  launchDate: '2025-10-16',
+  Brazil: {
+    'September 2025': [
+      {
+        edition: 'Ally X',
+        price: 999.99,
+        currency: 'R$',
+        exchangeTax2: 4.99
+      },
+      {
+        edition: 'Ally',
+        price: 599.99,
+        currency: 'R$',
+        exchangeTax2: 4.99
+      },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      {
+        edition: 'Ally X',
+        price: 999.99,
+        currency: 'R$',
+      },
+      {
+        edition: 'Ally',
+        price: 599.99,
+        currency: 'R$',
+      },
+    ],
+  },
+};
