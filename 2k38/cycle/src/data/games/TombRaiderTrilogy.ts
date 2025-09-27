b94b0dab41bde50659e34a531077c6520fdf1b0a
celso <celso.sjunior@outlook.com>
2025-09-27 04:13:39 -0300

    Criar TombRaiderTrilogy.ts

---
+++ 2k38/cycle/src/data/games/TombRaiderTrilogy.ts
@@ -0,0 +1,27 @@
+export const tombraidertrilogy = {
+  launchDate: '2021-03-18',
+  Brazil: {
+    'March 2021': [
+      { edition: 'Definitive Survivor Trilogy', price: 99.80, discount: 60, currency: 'R$', exchangeTax2: 5 },
+      { edition: 'Definitive Survivor Trilogy', price: 249.90, currency: 'R$', exchangeTax2: 5 },
+    ],
+  },
+  Europe: {
+    'March 2021': [
+      { edition: 'Definitive Survivor Trilogy', price: 19.99, discount: 60, currency: '€' },
+      { edition: 'Definitive Survivor Trilogy', price: 49.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'March 2021': [
+      { edition: 'Definitive Survivor Trilogy', price: 17.99, discount: 60, currency: '£' },
+      { edition: 'Definitive Survivor Trilogy', price: 44.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'March 2021': [
+      { edition: 'Definitive Survivor Trilogy', price: 19.99, discount: 60, currency: '$' },
+      { edition: 'Definitive Survivor Trilogy', price: 49.99, currency: '$' },
+    ],
+  },
+};
