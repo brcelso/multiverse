b610390b1dd04a5754b14d691b42f621ea639102
celso <celso.sjunior@outlook.com>
2025-09-27 20:49:23 -0300

    Criar ResidentEvilRemakeTrilogy.ts

---
+++ 2k38/cycle/src/data/games/ResidentEvilRemakeTrilogy.ts
@@ -0,0 +1,35 @@
+export const residentevilremaketrilogy = {
+  launchDate: '2024-11-10',
+  Brazil: {
+    'September 2025': [
+      { edition: 'Remake Trilogy', price: 192.10, discount: 55, currency: 'R$', exchangeTax2: 4.74 },
+    ],
+    'November 2024': [
+      { edition: 'Remake Trilogy', price: 426.90, currency: 'R$', exchangeTax2: 4.74 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: 'Remake Trilogy', price: 40.49, discount: 55, currency: '€' },
+    ],
+    'November 2024': [
+      { edition: 'Remake Trilogy', price: 89.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Remake Trilogy', price: 33.74, discount: 55, currency: '£' },
+    ],
+    'November 2024': [
+      { edition: 'Remake Trilogy', price: 74.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Remake Trilogy', price: 40.49, discount: 55, currency: '$' },
+    ],
+    'November 2024': [
+      { edition: 'Remake Trilogy', price: 89.99, currency: '$' },
+    ],
+  },
+};
