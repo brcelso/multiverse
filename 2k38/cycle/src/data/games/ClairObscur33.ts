f7fbdfaabfd3716f37605fabd0eda07a587fc321
celso <celso.sjunior@outlook.com>
2025-09-26 22:58:38 -0300

    Atualizar o ClairObscur33.ts

--- 2k38/cycle/src/data/games/ClairObscur33.ts
+++ 2k38/cycle/src/data/games/ClairObscur33.ts
@@ -1,7 +1,9 @@
 export const clairObscurExpedition33 = {
   launchDate: '2025-04-24',
-
   Brazil: {
+    'September 2025': [
+      { edition: 'Deluxe', price: 269.9, discount: 10, currency: 'R$', exchangeTax: 5 },
+    ],
     'July 2025': [
       { edition: 'Deluxe', price: 269.9, discount: 10, currency: 'R$', exchangeTax: 5 },
     ],
@@ -10,28 +12,34 @@ export const clairObscurExpedition33 = {
       { edition: 'Standard', price: 249.5, currency: 'R$', exchangeTax: 5 },
     ],
   },
-
+  Europe: {
+    'September 2025': [
+      { edition: 'Deluxe', price: 53.99, discount: 10, currency: '$' },
+    ],
+    'April 2025': [
+      { edition: 'Deluxe', price: 59.99, currency: '€' },
+      { edition: 'Cross-Gen', price: 49.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Deluxe', price: 44.99, discount: 10, currency: '£' },
+    ],
+    'April 2025': [
+      { edition: 'Deluxe', price: 49.99, currency: '£' },
+      { edition: 'Cross-Gen', price: 44.99, currency: '£' },
+    ],
+  },
   UnitedStates: {
+    'September 2025': [
+      { edition: 'Deluxe', price: 53.99, discount: 10, currency: '$' },
+    ],
     'July 2025': [
       { edition: 'Deluxe', price: 53.99, discount: 10, currency: '$' },
     ],
     'April 2025': [
       { edition: 'Deluxe', price: 59.99, currency: '$' },
       { edition: 'Standard', price: 49.99, currency: '$' },
-    ],
-  },
-
-  Europe: {
-    'April 2025': [
-      { edition: 'Deluxe', price: 69.99, currency: '€' },
-      { edition: 'Cross-Gen', price: 49.99, currency: '€' },
-    ],
-  },
-
-  UnitedKingdom: {
-    'April 2025': [
-      { edition: 'Deluxe', price: 49.99, currency: '£' },
-      { edition: 'Cross-Gen', price: 44.99, currency: '£' },
     ],
   },
 };
