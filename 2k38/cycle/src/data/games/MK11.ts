15b3effd63aa1219004818939c04cd48005534ec
celso <celso.sjunior@outlook.com>
2025-09-27 22:23:42 -0300

    Criar MK11.ts

---
+++ 2k38/cycle/src/data/games/MK11.ts
@@ -0,0 +1,48 @@
+export const mortalkombat11 = {
+  launchDate: '2020-11-17',
+  Brazil: {
+    'September 2025': [
+      { edition: 'Ultimate Edition + Injustice 2', price: 44.99, discount: 90, currency: 'R$', exchangeTax2: 4.50 },
+      { edition: 'Ultimate Edition', price: 27.99, discount: 90, currency: 'R$', exchangeTax2: 4.66 },
+    ],
+    'November 2020': [
+      { edition: 'Ultimate Edition + Injustice 2', price: 449.99, currency: 'R$', exchangeTax2: 4.50 },
+      { edition: 'Ultimate Edition', price: 279.99, currency: 'R$', exchangeTax2: 4.66 },
+      { edition: 'Standard Edition', price: 199.99, currency: 'R$', exchangeTax2: 4 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: 'Ultimate Edition + Injustice 2', price: 9.99, discount: 90, currency: '€' },
+      { edition: 'Ultimate Edition', price: 7.19, discount: 88, currency: '€' },
+    ],
+    'November 2020': [
+      { edition: 'Ultimate Edition + Injustice 2', price: 99.99, currency: '€' },
+      { edition: 'Ultimate Edition', price: 59.99, currency: '€' },
+      { edition: 'Standard Edition', price: 49.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Ultimate Edition + Injustice 2', price: 8.99, discount: 90, currency: '£' },
+      { edition: 'Ultimate Edition', price: 5.99, discount: 88, currency: '£' },
+    ],
+    'November 2020': [
+      { edition: 'Ultimate Edition + Injustice 2', price: 89.99, currency: '£' },
+      { edition: 'Ultimate Edition', price: 49.99, currency: '£' },
+      { edition: 'Standard Edition', price: 39.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Ultimate Edition + Injustice 2', price: 9.99, discount: 90, currency: '$' },
+      { edition: 'Ultimate Edition', price: 5.99, discount: 90, currency: '$' },
+    ],
+    'November 2020': [
+      { edition: 'Ultimate Edition + Injustice 2', price: 99.99, currency: '$' },
+      { edition: 'Ultimate Edition', price: 59.99, currency: '$' },
+      { edition: 'Standard Edition', price: 49.99, currency: '$' },
+    ],
+  },
+
+};
