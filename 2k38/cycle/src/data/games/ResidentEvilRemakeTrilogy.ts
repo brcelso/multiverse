b87f70134024ed7e9ba51e1e6eec077c390d5dec
celso <celso.sjunior@outlook.com>
2025-09-28 01:25:07 -0300

    Atualizar o ResidentEvilRemakeTrilogy.ts

--- 2k38/cycle/src/data/games/ResidentEvilRemakeTrilogy.ts
+++ 2k38/cycle/src/data/games/ResidentEvilRemakeTrilogy.ts
@@ -3,33 +3,41 @@ export const residentevilremaketrilogy = {
   Brazil: {
     'September 2025': [
       { edition: 'Remake Trilogy', price: 192.10, discount: 55, currency: 'R$', exchangeTax2: 4.74 },
+      { edition: 'Raccoon City Edition', price: 74.97, discount: 75, currency: 'R$', exchangeTax2: 4.99 },
     ],
     'November 2024': [
       { edition: 'Remake Trilogy', price: 426.90, currency: 'R$', exchangeTax2: 4.74 },
+      { edition: 'Raccoon City Edition', price: 299.90, currency: 'R$', exchangeTax2: 4.99 },
     ],
   },
   Europe: {
     'September 2025': [
       { edition: 'Remake Trilogy', price: 40.49, discount: 55, currency: '€' },
+      { edition: 'Raccoon City Edition', price: 14.99, discount: 75, currency: '€' },
     ],
     'November 2024': [
       { edition: 'Remake Trilogy', price: 89.99, currency: '€' },
+      { edition: 'Raccoon City Edition', price: 59.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'September 2025': [
       { edition: 'Remake Trilogy', price: 33.74, discount: 55, currency: '£' },
+      { edition: 'Raccoon City Edition', price: 12.49, discount: 75, currency: '£' },
     ],
     'November 2024': [
       { edition: 'Remake Trilogy', price: 74.99, currency: '£' },
+      { edition: 'Raccoon City Edition', price: 49.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'September 2025': [
       { edition: 'Remake Trilogy', price: 40.49, discount: 55, currency: '$' },
+      { edition: 'Raccoon City Edition', price: 14.99, discount: 75, currency: '$' },
     ],
     'November 2024': [
       { edition: 'Remake Trilogy', price: 89.99, currency: '$' },
+      { edition: 'Raccoon City Edition', price: 59.99, currency: '$' },
     ],
   },
 };
