b4b9fe29013c0854b9624a28c9030423775f9ae4
celso <celso.sjunior@outlook.com>
2025-10-01 10:37:28 -0300

    Criar GamePass.ts

---
+++ 2k38/cycle/src/data/subs/GamePass.ts
@@ -0,0 +1,97 @@
+export const gamepass = {
+  launchDate: '2017-06-01',
+  UnitedStates: {
+    'September 2024': [
+      { price: 14.99, increase: 36, totalIncrease: 50, currency: '$', edition: "Game Pass Standard" },
+      { price: 9.99, increase: 0, totalIncrease: 100, currency: '$', edition: "Game Pass PC" },
+      { price: 16.99, increase: 13, totalIncrease: 13, currency: '$', edition: "Game Pass Ultimate" },
+    ],
+    'July 2023': [
+      { price: 10.99, increase: 10, totalIncrease: 10, currency: '$', edition: "Game Pass Console" },
+      { price: 9.99, increase: 100, totalIncrease: 100, currency: '$', edition: "Game Pass PC" },
+      { price: 16.99, increase: 13, totalIncrease: 13, currency: '$', edition: "Game Pass Ultimate" },
+    ],
+    'September 2020': [
+      { price: 9.99, increase: 0, totalIncrease: 0, currency: '$', edition: "Game Pass Console" },
+      { price: 9.99, increase: 100, totalIncrease: 100, currency: '$', edition: "Game Pass PC" },
+      { price: 14.99, increase: 0, totalIncrease: 0, currency: '$', edition: "Game Pass Ultimate" },
+    ],
+    'June 2019': [
+      { price: 9.99, currency: '$', edition: "Game Pass Console" },
+      { price: 4.99, currency: '$', edition: "Game Pass PC (Beta)" },
+      { price: 14.99, currency: '$', edition: "Game Pass Ultimate" },
+    ],
+    'June 2017': [
+      { price: 9.99, currency: '$', edition: "Game Pass Console" },
+    ],
+  },
+  Europe: {
+    'September 2024': [
+      { price: 14.99, increase: 36, totalIncrease: 50, currency: '€', edition: "Game Pass Standard" },
+      { price: 9.99, increase: 0, totalIncrease: 150, currency: '€', edition: "Game Pass PC" },
+      { price: 16.99, increase: 13, totalIncrease: 13, currency: '€', edition: "Game Pass Ultimate" },
+    ],
+    'July 2023': [
+      { price: 10.99, increase: 10, totalIncrease: 10, currency: '€', edition: "Game Pass Console" },
+      { price: 9.99, increase: 150, totalIncrease: 150, currency: '€', edition: "Game Pass PC" },
+      { price: 16.99, increase: 13, totalIncrease: 13, currency: '€', edition: "Game Pass Ultimate" },
+    ],
+    'September 2020': [
+      { price: 9.99, increase: 0, totalIncrease: 0, currency: '€', edition: "Game Pass Console" },
+      { price: 9.99, increase: 150, totalIncrease: 150, currency: '€', edition: "Game Pass PC" },
+      { price: 14.99, increase: 0, totalIncrease: 0, currency: '€', edition: "Game Pass Ultimate" },
+    ],
+    'June 2019': [
+      { price: 9.99, currency: '€', edition: "Game Pass Console" },
+      { price: 3.99, currency: '€', edition: "Game Pass PC (Beta)" },
+      { price: 14.99, currency: '€', edition: "Game Pass Ultimate" },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2024': [
+      { price: 12.99, increase: 0, totalIncrease: 63, currency: '£', edition: "Game Pass Standard" },
+      { price: 7.99, increase: 0, totalIncrease: 100, currency: '£', edition: "Game Pass PC" },
+      { price: 14.99, increase: 15, totalIncrease: 36, currency: '£', edition: "Game Pass Ultimate" },
+    ],
+    'July 2023': [
+      { price: 8.99, increase: 12, totalIncrease: 12, currency: '£', edition: "Game Pass Console" },
+      { price: 7.99, increase: 100, totalIncrease: 100, currency: '£', edition: "Game Pass PC" },
+      { price: 12.99, increase: 18, totalIncrease: 18, currency: '£', edition: "Game Pass Ultimate" },
+    ],
+    'September 2020': [
+      { price: 7.99, increase: 0, totalIncrease: 0, currency: '£', edition: "Game Pass Console" },
+      { price: 7.99, increase: 100, totalIncrease: 100, currency: '£', edition: "Game Pass PC" },
+      { price: 10.99, increase: 0, totalIncrease: 0, currency: '£', edition: "Game Pass Ultimate" },
+    ],
+    'June 2019': [
+      { price: 7.99, currency: '£', edition: "Game Pass Console" },
+      { price: 3.99, currency: '£', edition: "Game Pass PC (Beta)" },
+      { price: 10.99, currency: '£', edition: "Game Pass Ultimate" },
+    ],
+  },
+  Brazil: {
+    'September 2024': [
+      { price: 49.99, increase: 51, totalIncrease: 72, currency: 'R$', edition: "Game Pass Standard" },
+      { price: 29.99, increase: 0, totalIncrease: 114, currency: 'R$', edition: "Game Pass PC" },
+      { price: 59.99, increase: 33, totalIncrease: 100, currency: 'R$', edition: "Game Pass Ultimate" },
+    ],
+    'July 2023': [
+      { price: 32.99, increase: 10, totalIncrease: 14, currency: 'R$', edition: "Game Pass Console" },
+      { price: 29.99, increase: 114, totalIncrease: 114, currency: 'R$', edition: "Game Pass PC" },
+      { price: 44.99, increase: 13, totalIncrease: 13, currency: 'R$', edition: "Game Pass Ultimate" },
+    ],
+    'September 2020': [
+      { price: 29.99, increase: 0, totalIncrease: 3, currency: 'R$', edition: "Game Pass Console" },
+      { price: 29.99, increase: 114, totalIncrease: 114, currency: 'R$', edition: "Game Pass PC" },
+      { price: 39.99, increase: 0, totalIncrease: 0, currency: 'R$', edition: "Game Pass Ultimate" },
+    ],
+    'June 2019': [
+      { price: 29.99, currency: 'R$', edition: "Game Pass Console" },
+      { price: 13.99, currency: 'R$', edition: "Game Pass PC (Beta)" },
+      { price: 39.99, currency: 'R$', edition: "Game Pass Ultimate" },
+    ],
+    'June 2017': [
+      { price: 29.00, currency: 'R$', edition: "Game Pass Console" },
+    ],
+  }
+}
