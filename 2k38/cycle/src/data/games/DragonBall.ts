53ac3d462756ec220db2768196024edcba5d2319
celso <celso.sjunior@outlook.com>
2025-10-02 13:18:16 -0300

    Atualizar o DragonBall.ts

--- 2k38/cycle/src/data/games/DragonBall.ts
+++ 2k38/cycle/src/data/games/DragonBall.ts
@@ -13,24 +13,24 @@ export const dragonball = {
   },
   Europe: {
     'September 2025': [
-      { edition: 'Ultimate Edition', price: 65.99, discount: 40, currency: '$' },
-      { edition: 'Standard Edition', price: 39.89, discount: 43, currency: '$' },
+      { edition: 'Ultimate Edition', price: 65.99, discount: 40, currency: '€' },
+      { edition: 'Standard Edition', price: 39.89, discount: 43, currency: '€' },
     ],
     'October 2024': [
-      { edition: 'Ultimate Edition', price: 109.99, currency: '$' },
-      { edition: 'Deluxe Edition', price: 99.99, currency: '$' },
-      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+      { edition: 'Ultimate Edition', price: 109.99, currency: '€' },
+      { edition: 'Deluxe Edition', price: 99.99, currency: '€' },
+      { edition: 'Standard Edition', price: 69.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'September 2025': [
-      { edition: 'Ultimate Edition', price: 74.39, discount: 38, currency: '$' },
-      { edition: 'Standard Edition', price: 49.59, discount: 38, currency: '$' },
+      { edition: 'Ultimate Edition', price: 74.39, discount: 38, currency: '£' },
+      { edition: 'Standard Edition', price: 49.59, discount: 38, currency: '£' },
     ],
     'October 2024': [
-      { edition: 'Ultimate Edition', price: 119.99, currency: '$' },
-      { edition: 'Deluxe Edition', price: 109.99, currency: '$' },
-      { edition: 'Standard Edition', price: 79.99, currency: '$' },
+      { edition: 'Ultimate Edition', price: 119.99, currency: '£' },
+      { edition: 'Deluxe Edition', price: 109.99, currency: '£' },
+      { edition: 'Standard Edition', price: 79.99, currency: '£' },
     ],
   },
   UnitedStates: {
