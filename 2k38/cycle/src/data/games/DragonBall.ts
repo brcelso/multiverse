f1d7e0d7fe9158c46509f6b5744d8ced3b508b4c
celso <celso.sjunior@outlook.com>
2025-10-02 13:17:19 -0300

    Criar DragonBall.ts

---
+++ 2k38/cycle/src/data/games/DragonBall.ts
@@ -0,0 +1,47 @@
+export const dragonball = {
+  launchDate: '2024-10-10',
+  Brazil: {
+    'September 2025': [
+      { edition: 'Ultimate Edition', price: 353.94, discount: 40, currency: 'R$', exchangeTax: 5.35 },
+      { edition: 'Standard Edition', price: 199.44, discount: 43, currency: 'R$', exchangeTax: 5 },
+    ],
+    'October 2024': [
+      { edition: 'Ultimate Edition', price: 589.90, currency: 'R$', exchangeTax: 5.36 },
+      { edition: 'Deluxe Edition', price: 534.90, currency: 'R$', exchangeTax: 5.35 },
+      { edition: 'Standard Edition', price: 349.90, currency: 'R$', exchangeTax: 5 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { edition: 'Ultimate Edition', price: 65.99, discount: 40, currency: '$' },
+      { edition: 'Standard Edition', price: 39.89, discount: 43, currency: '$' },
+    ],
+    'October 2024': [
+      { edition: 'Ultimate Edition', price: 109.99, currency: '$' },
+      { edition: 'Deluxe Edition', price: 99.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Ultimate Edition', price: 74.39, discount: 38, currency: '$' },
+      { edition: 'Standard Edition', price: 49.59, discount: 38, currency: '$' },
+    ],
+    'October 2024': [
+      { edition: 'Ultimate Edition', price: 119.99, currency: '$' },
+      { edition: 'Deluxe Edition', price: 109.99, currency: '$' },
+      { edition: 'Standard Edition', price: 79.99, currency: '$' },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { edition: 'Ultimate Edition', price: 61.99, discount: 38, currency: '$' },
+      { edition: 'Standard Edition', price: 43.39, discount: 38, currency: '$' },
+    ],
+    'October 2024': [
+      { edition: 'Ultimate Edition', price: 99.99, currency: '$' },
+      { edition: 'Deluxe Edition', price: 87.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
+    ],
+  },
+};
