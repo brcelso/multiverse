491b1ede81cd54a92fc659f26f60b1a142c058a1
celso <celso.sjunior@outlook.com>
2025-10-02 23:51:10 -0300

    Atualizar o NinjaGaiden4.ts

--- 2k38/cycle/src/data/games/NinjaGaiden4.ts
+++ 2k38/cycle/src/data/games/NinjaGaiden4.ts
@@ -1,6 +1,10 @@
 export const ninja4 = {
   launchDate: '2025-10-20',
   Brazil: {
+    'October 2025': [
+      { edition: 'Deluxe Edition', price: 374.00, discount: 12, currency: 'R$', exchangeTax: 4.15 },
+      { edition: 'Standard Edition', price: 299.00, discount: 25, currency: 'R$', exchangeTax: 4.27 },
+    ],
     'September 2025': [
       { edition: 'Deluxe Edition', price: 424.00, currency: 'R$', exchangeTax: 4.71 },
       { edition: 'Standard Edition', price: 399.90, currency: 'R$', exchangeTax: 5.71 },
