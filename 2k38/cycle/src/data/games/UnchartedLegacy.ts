9088c4b38c55540f06d04d0981d391eae9a8494e
celso <celso.sjunior@outlook.com>
2025-10-05 20:20:34 -0300

    Criar UnchartedLegacy.ts

---
+++ 2k38/cycle/src/data/games/UnchartedLegacy.ts
@@ -0,0 +1,35 @@
+export const unchartedlegacy = {
+  launchDate: '2022-10-19',
+  Brazil: {
+    'October 2025': [
+      { edition: 'Legacy Of Thieves Collection', price: 79.96, currency: 'R$', discount: 60 }
+    ],
+    'October 2022': [
+      { edition: 'Legacy Of Thieves Collection', price: 199.90, currency: 'R$' }
+    ],
+  },
+  Europe: {
+    'October 2025': [
+      { edition: 'Legacy Of Thieves Collection', price: 79.96, currency: '€', discount: 60 }
+    ],
+    'October 2022': [
+      { edition: 'Legacy Of Thieves Collection', price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'October 2025': [
+      { edition: 'Legacy Of Thieves Collection', price: 59.99, currency: '£', discount: 60 },
+    ],
+    'October 2022': [
+      { edition: 'Legacy Of Thieves Collection', price: 59.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { edition: 'Legacy Of Thieves Collection', price: 59.99, currency: '$', discount: 60 },
+    ],
+    'October 2022': [
+      { edition: 'Legacy Of Thieves Collection', price: 59.99, currency: '$' },
+    ],
+  },
+};
