96aec39e667dffc6bc9cddf7bd7a961037b7c64f
celso <celso.sjunior@outlook.com>
2025-10-05 20:30:45 -0300

    Atualizar o UnchartedLegacy.ts

--- 2k38/cycle/src/data/games/UnchartedLegacy.ts
+++ 2k38/cycle/src/data/games/UnchartedLegacy.ts
@@ -2,34 +2,36 @@ export const unchartedlegacy = {
   launchDate: '2022-10-19',
   Brazil: {
     'October 2025': [
-      { edition: 'Legacy Of Thieves Collection', price: 79.96, currency: 'R$', discount: 60 }
+      { platform: "Steam", edition: 'Legacy Of Thieves Collection', price: 79.96, currency: 'R$', discount: 60, exchangeTax2: 3.99 },
+      { platform: "Playstation", edition: 'Legacy Of Thieves Collection', price: 113.96, currency: 'R$', discount: 60, exchangeTax2: 5.69 }
     ],
     'October 2022': [
-      { edition: 'Legacy Of Thieves Collection', price: 199.90, currency: 'R$' }
+      { platform: "Steam", edition: 'Legacy Of Thieves Collection', price: 199.90, currency: 'R$', exchangeTax2: 3.99 },
+      { platform: "Playstation", edition: 'Legacy Of Thieves Collection', price: 284.90, currency: 'R$', exchangeTax2: 5.69 }
     ],
   },
   Europe: {
     'October 2025': [
-      { edition: 'Legacy Of Thieves Collection', price: 79.96, currency: '€', discount: 60 }
+      { edition: 'Legacy Of Thieves Collection', price: 19.99, currency: '€', discount: 60 }
     ],
     'October 2022': [
-      { edition: 'Legacy Of Thieves Collection', price: 69.99, currency: '€' },
+      { edition: 'Legacy Of Thieves Collection', price: 49.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'October 2025': [
-      { edition: 'Legacy Of Thieves Collection', price: 59.99, currency: '£', discount: 60 },
+      { edition: 'Legacy Of Thieves Collection', price: 17.99, currency: '£', discount: 60 },
     ],
     'October 2022': [
-      { edition: 'Legacy Of Thieves Collection', price: 59.99, currency: '£' },
+      { edition: 'Legacy Of Thieves Collection', price: 44.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'October 2025': [
-      { edition: 'Legacy Of Thieves Collection', price: 59.99, currency: '$', discount: 60 },
+      { edition: 'Legacy Of Thieves Collection', price: 19.99, currency: '$', discount: 60 },
     ],
     'October 2022': [
-      { edition: 'Legacy Of Thieves Collection', price: 59.99, currency: '$' },
+      { edition: 'Legacy Of Thieves Collection', price: 49.99, currency: '$' },
     ],
   },
 };
