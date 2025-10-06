8c369f399e818ff75b60e732494af9ba5955995c
celso <celso.sjunior@outlook.com>
2025-10-06 17:07:08 -0300

    Atualizar o FFTactics.ts

--- 2k38/cycle/src/data/games/FFTactics.ts
+++ 2k38/cycle/src/data/games/FFTactics.ts
@@ -2,12 +2,12 @@ export const fftactics = {
   launchDate: '2025-09-30',
   Brazil: {
     'September 2025': [
-      { edition: 'Standard Edition', price: 239.20, discount: 29.5, currency: 'R$', exchangeTax: 4.78 },
-      { edition: 'Standard Edition', price: 278.91, discount: 2.3, currency: 'R$', exchangeTax: 5.69 },
-      { edition: 'Deluxe Edition', price: 339.90, currency: 'R$', exchangeTax: 5.66 },
-      { edition: 'Standard Edition', price: 284.90, currency: 'R$', exchangeTax: 5.69 },
-      { edition: 'Deluxe Edition', price: 249.90, currency: 'R$', exchangeTax: 3.57 },
-      { edition: 'Standard Edition', price: 219.90, currency: 'R$', exchangeTax: 4.39 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 239.20, discount: 29.5, currency: 'R$', exchangeTax: 4.78 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 278.91, discount: 2.3, currency: 'R$', exchangeTax: 5.69 },
+      { platform: "Playstation", edition: 'Deluxe Edition', price: 339.90, currency: 'R$', exchangeTax: 5.66 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 284.90, currency: 'R$', exchangeTax: 5.69 },
+      { platform: "Steam", edition: 'Deluxe Edition', price: 249.90, currency: 'R$', exchangeTax: 3.57 },
+      { platform: "Steam", edition: 'Standard Edition', price: 219.90, currency: 'R$', exchangeTax: 4.39 },
     ],
   },
   Europe: {
