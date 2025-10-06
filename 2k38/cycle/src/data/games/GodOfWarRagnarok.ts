ab90c36425d26ad7877549bd682f4f371bcdf1c2
celso <celso.sjunior@outlook.com>
2025-10-06 20:38:37 -0300

    Atualizar o GodOfWarRagnarok.ts

--- 2k38/cycle/src/data/games/GodOfWarRagnarok.ts
+++ 2k38/cycle/src/data/games/GodOfWarRagnarok.ts
@@ -1,67 +1,71 @@
 export const godOfWarRagnarok = {
   launchDate: '2022-11-09',
   Brazil: {
+    'October 2025': [
+      { platform: "Steam", edition: 'Deluxe Edition', price: 239.92, currency: 'R$', discount: 20, exchangeTax2: 3.41 },
+      { platform: "Steam", edition: 'Standard Edition', price: 164.90, currency: 'R$', discount: 34, exchangeTax2: 2.74 },
+    ],
     'September 2025': [
-      { edition: 'Deluxe Edition', price: 227.95, currency: 'R$', discount: 50 },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 227.95, currency: 'R$', discount: 50 },
     ],
     'July 2025': [
-      { edition: 'Deluxe Edition', price: 455.90, currency: 'R$', increase: 14 },
-      { edition: 'Standard PS5', price: 134.91, currency: 'R$', discount: 61.5 },
-      { edition: 'Standard PS4', price: 89.91, currency: 'R$', discount: 70 },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 455.90, currency: 'R$', increase: 14 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 134.91, currency: 'R$', discount: 61.5 },
+      { platform: "Playstation", edition: 'Standard PS4', price: 89.91, currency: 'R$', discount: 70 },
     ],
     'January 2025': [
-      { edition: 'Deluxe Edition', price: 199.75, currency: 'R$', discount: 50 },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 199.75, currency: 'R$', discount: 50 },
     ],
     'March 2025': [
-      { edition: 'Standard PS5', price: 174.95, currency: 'R$', discount: 50 },
-      { edition: 'Standard PS4', price: 125.95, currency: 'R$', discount: 58 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 174.95, currency: 'R$', discount: 50 },
+      { platform: "Playstation", edition: 'Standard PS4', price: 125.95, currency: 'R$', discount: 58 },
     ],
     'November 2022': [
-      { edition: 'Deluxe Edition', price: 399.9, currency: 'R$', exchangeTax2: 5 },
-      { edition: 'Standard PS5', price: 349.9, currency: 'R$', exchangeTax2: 5 },
-      { edition: 'Standard PS4', price: 299.9, currency: 'R$', exchangeTax2: 5 },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 399.9, currency: 'R$', exchangeTax2: 5 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 349.9, currency: 'R$', exchangeTax2: 5 },
+      { platform: "Playstation", edition: 'Standard PS4', price: 299.9, currency: 'R$', exchangeTax2: 5 },
     ],
   },
   Europe: {
     'September 2025': [
-      { edition: 'Deluxe Edition', price: 50.39, currency: '€', discount: 44 },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 50.39, currency: '€', discount: 44 },
     ],
     'November 2022': [
-      { edition: 'Deluxe', price: 89.99, currency: '€' },
-      { edition: 'Standard PS5', price: 79.99, currency: '€' },
-      { edition: 'Standard PS4', price: 69.99, currency: '€' },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 89.99, currency: '€' },
+      { platform: "Playstation", edition: 'Standard Edition', price: 79.99, currency: '€' },
+      { platform: "Playstation", edition: 'Standard PS4', price: 69.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'September 2025': [
-      { edition: 'Deluxe Edition', price: 44.79, currency: '£', discount: 44 },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 44.79, currency: '£', discount: 44 },
     ],
     'March 2025': [
-      { edition: 'Standard PS5', price: 34.99, currency: '£', discount: 50 },
-      { edition: 'Standard PS4', price: 25.19, currency: '£', discount: 58 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 34.99, currency: '£', discount: 50 },
+      { platform: "Playstation", edition: 'Standard PS4', price: 25.19, currency: '£', discount: 58 },
     ],
     'November 2022': [
-      { edition: 'Complete PS5', price: 69.99, currency: '£' },
-      { edition: 'Standard PS4', price: 59.99, currency: '£' },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 69.99, currency: '£' },
+      { platform: "Playstation", edition: 'Standard PS4', price: 59.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'September 2025': [
-      { edition: 'Deluxe Edition', price: 31.99, currency: '$', discount: 60 },
-      { edition: 'Standard PS5', price: 57.99, currency: '$', discount: 17 },
-      { edition: 'Standard PS4', price: 53.99, currency: '$', discount: 10 },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 31.99, currency: '$', discount: 60 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 57.99, currency: '$', discount: 17 },
+      { platform: "Playstation", edition: 'Standard PS4', price: 53.99, currency: '$', discount: 10 },
     ],
     'January 2025': [
-      { edition: 'Deluxe Edition', price: 39.99, currency: '$', discount: 50 },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 39.99, currency: '$', discount: 50 },
     ],
     'March 2025': [
-      { edition: 'Standard PS5', price: 29.39, currency: '$', discount: 58 },
-      { edition: 'Standard PS4', price: 25.19, currency: '$', discount: 58 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 29.39, currency: '$', discount: 58 },
+      { platform: "Playstation", edition: 'Standard PS4', price: 25.19, currency: '$', discount: 58 },
     ],
     'November 2022': [
-      { edition: 'Deluxe Edition', price: 79.99, currency: '$' },
-      { edition: 'Standard PS5', price: 69.99, currency: '$' },
-      { edition: 'Standard PS4', price: 59.99, currency: '$' },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition', price: 79.99, currency: '$' },
+      { platform: "Playstation", edition: 'Standard Edition', price: 69.99, currency: '$' },
+      { platform: "Playstation", edition: 'Standard PS4', price: 59.99, currency: '$' },
     ],
   },
 
