2dc84eb41fec686920a0964b7e9fdca02e64a644
Celso <celso.sjunior@outlook.com>
2025-10-06 13:55:51 -0300

    added gow 2018

--- 2k38/cycle/src/data/games/GodOfWar.ts
+++ 2k38/cycle/src/data/games/GodOfWar.ts
@@ -3,16 +3,44 @@ export const godofwar = {
   Brazil: {
     'October 2025': [
       { platform: "Steam", edition: "Standard Edition", price: 79.96, discount: 60, currency: 'R$', exchangeTax2: 4 },
+      { platform: "Playstation", edition: "Digital Deluxe Edition", price: 169.90, currency: 'R$', exchangeTax2: 0 },
+      { platform: "Playstation", edition: "Standard Edition", price: 114.90, currency: 'R$', exchangeTax2: 0 },
     ],
     'April 2018': [
+      { platform: "Playstation", edition: "Digital Deluxe Edition", price: 249.90, currency: 'R$', exchangeTax2: 4 },
+      { platform: "Playstation", edition: "Standard Edition", price: 199.90, currency: 'R$', exchangeTax2: 3.33 },
       { platform: "Steam", edition: "Standard Edition", price: 199.90, currency: 'R$', exchangeTax2: 4 },
+    ],
+  },
+  Europe: {
+    'October 2025': [
+      { platform: "Playstation", edition: "Digital Deluxe Edition", price: 29.99, currency: '$' },
+      { platform: "Playstation", edition: "Standard Edition", price: 19.99, currency: '$' },
+    ],
+    'April 2018': [
+      { platform: "Playstation", edition: "Digital Deluxe Edition", price: 69.99, currency: '$' },
+      { platform: "Playstation", edition: "Standard Edition", price: 59.99, currency: '$' },
+    ],
+  },
+  UnitedKingdom: {
+    'October 2025': [
+      { platform: "Playstation", edition: "Digital Deluxe Edition", price: 24.99, currency: '$' },
+      { platform: "Playstation", edition: "Standard Edition", price: 15.99, currency: '$' },
+    ],
+    'April 2018': [
+      { platform: "Playstation", edition: "Digital Deluxe Edition", price: 64.99, currency: '$' },
+      { platform: "Playstation", edition: "Standard Edition", price: 54.99, currency: '$' },
     ],
   },
   UnitedStates: {
     'October 2025': [
       { platform: "Steam", edition: "Standard Edition", price: 19.99, discount: 60, currency: '$' },
+      { platform: "Playstation", edition: "Digital Deluxe Edition", price: 29.99, totalDiscount: 58, currency: '$' },
+      { platform: "Playstation", edition: "Standard Edition", price: 19.99, totalDiscount: 66.7, currency: '$' },
     ],
     'April 2018': [
+      { platform: "Playstation", edition: "Digital Deluxe Edition", price: 69.99, currency: '$' },
+      { platform: "Playstation", edition: "Standard Edition", price: 59.99, currency: '$' },
       { platform: "Steam", edition: "Standard Edition", price: 49.99, currency: '$' },
     ],
   },
