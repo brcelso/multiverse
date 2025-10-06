298286293e13cce7e156872a7cae4aa3744e578e
celso <celso.sjunior@outlook.com>
2025-10-06 17:01:47 -0300

    Atualizar o FFTactics.ts

--- 2k38/cycle/src/data/games/FFTactics.ts
+++ 2k38/cycle/src/data/games/FFTactics.ts
@@ -6,6 +6,8 @@ export const fftactics = {
       { edition: 'Standard Edition', price: 278.91, discount: 2.3, currency: 'R$', exchangeTax: 5.69 },
       { edition: 'Deluxe Edition', price: 339.90, currency: 'R$', exchangeTax: 5.66 },
       { edition: 'Standard Edition', price: 284.90, currency: 'R$', exchangeTax: 5.69 },
+      { edition: 'Deluxe Edition', price: 249.90, currency: 'R$', exchangeTax: 3.57 },
+      { edition: 'Standard Edition', price: 219.90, currency: 'R$', exchangeTax: 4.39 },
     ],
   },
   Europe: {
