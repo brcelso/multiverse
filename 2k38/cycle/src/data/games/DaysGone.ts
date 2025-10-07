f88b59ab24aac29c514edade26fcdfb3b2c3d47b
celso <celso.sjunior@outlook.com>
2025-10-06 21:39:24 -0300

    Criar DaysGone.ts

---
+++ 2k38/cycle/src/data/games/DaysGone.ts
@@ -0,0 +1,38 @@
+export const daysgone = {
+  launchDate: '2019-04-26',
+  Brazil: {
+    'October 2025': [
+      { platform: "Steam", edition: 'Standard Edition', price: 99.75, discount: 50, currency: 'R$', exchangeTax: 5.74 },
+    ],
+    'April 2019': [
+      { platform: "Playstation", edition: 'Remastered', price: 249.90, currency: 'R$', exchangeTax: 5 },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition PS4', price: 284.90, currency: 'R$', exchangeTax: 5.69 },
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 229.90, currency: 'R$', exchangeTax: 5.74 },
+      { platform: "Steam", edition: 'Standard Edition', price: 199.50, currency: 'R$', exchangeTax: 5.74 },
+    ],
+  },
+  Europe: {
+    'April 2019': [
+      { platform: "Playstation", edition: 'Remastered', price: 49.99, currency: '€' },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition PS4', price: 49.99, currency: '€' },
+      { platform: "Playstation | Steam", edition: 'Standard Edition PS4', price: 39.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'April 2019': [
+      { platform: "Playstation", edition: 'Remastered', price: 44.99, currency: '£' },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition PS4', price: 44.99, currency: '£' },
+      { platform: "Playstation | Steam", edition: 'Standard Edition PS4', price: 34.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { platform: "Steam", edition: 'Standard Edition', discount: 50, price: 19.99, currency: '$' },
+    ],
+    'April 2019': [
+      { platform: "Playstation", edition: 'Remastered', price: 49.99, currency: '$' },
+      { platform: "Playstation", edition: 'Digital Deluxe Edition PS4', price: 49.99, currency: '$' },
+      { platform: "Playstation | Steam", edition: 'Standard Edition PS4', price: 39.99, currency: '$' },
+    ],
+  },
+};
