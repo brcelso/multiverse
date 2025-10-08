1861199d1f780a9166641a464191d74dd1bdd690
celso <celso.sjunior@outlook.com>
2025-10-08 10:30:22 -0300

    Criar TombRaiderRemastered.ts

---
+++ 2k38/cycle/src/data/games/TombRaiderRemastered.ts
@@ -0,0 +1,63 @@
+export const tombRaiderRemastered = {
+  launchDate: '2024-02-14',
+  Brazil: {
+    'October 2025': [
+      { edition: 'Remastered Bundle', price: 117.96, currency: 'R$', discount: 60, exchangeTax: 5.36 },
+      { edition: 'Remastered Bundle', price: 147.45, currency: 'R$', discount: 50, exchangeTax: 5.36 },
+      { edition: 'Remastered', price: 31.98, currency: 'R$', discount: 80, exchangeTax: 5.33 },
+      { edition: 'Remastered', price: 47.97, currency: 'R$', discount: 70, exchangeTax: 5.33 },
+    ],
+    'February 2025': [
+      { edition: 'Remastered Bundle', price: 294.90, currency: 'R$', exchangeTax: 5.33 },
+    ],
+    'February 2024': [
+      { edition: 'Remastered', price: 143.91, currency: 'R$', discount: 10, exchangeTax: 5.33 },
+      { edition: 'Remastered', price: 159.99, currency: 'R$', exchangeTax: 5.33 },
+    ],
+  },
+  Europe: {
+    'October 2025': [
+      { edition: 'Remastered Bundle', price: 21.99, currency: '€', discount: 60 },
+      { edition: 'Remastered Bundle', price: 27.49, currency: '€', discount: 50 },
+      { edition: 'Remastered', price: 5.99, currency: '€', discount: 80 },
+      { edition: 'Remastered', price: 8.99, currency: '€', discount: 70 },
+    ],
+    'February 2025': [
+      { edition: 'Remastered Bundle', '€': 54.99, currency: '$' },
+    ],
+    'February 2024': [
+      { edition: 'Remastered', price: 26.99, currency: '€', discount: 10 },
+      { edition: 'Remastered', price: 29.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'October 2025': [
+      { edition: 'Remastered Bundle', price: 17.99, currency: '£', discount: 60 },
+      { edition: 'Remastered Bundle', price: 22.49, currency: '£', discount: 50 },
+      { edition: 'Remastered', price: 4.99, currency: '£', discount: 80 },
+      { edition: 'Remastered', price: 7.49, currency: '£', discount: 70 },
+    ],
+    'February 2025': [
+      { edition: 'Remastered Bundle', price: 44.99, currency: '£' },
+    ],
+    'February 2024': [
+      { edition: 'Remastered', price: 22.49, currency: '£', discount: 10 },
+      { edition: 'Remastered', price: 24.99, currency: '£' },
+    ],
+  },  
+  UnitedStates: {
+    'October 2025': [
+      { edition: 'Remastered Bundle', price: 21.99, currency: '$', discount: 60 },
+      { edition: 'Remastered Bundle', price: 27.49, currency: '$', discount: 50 },
+      { edition: 'Remastered', price: 5.99, currency: '$', discount: 80 },
+      { edition: 'Remastered', price: 8.99, currency: '$', discount: 70 },
+    ],
+    'February 2025': [
+      { edition: 'Remastered Bundle', price: 54.99, currency: '$' },
+    ],
+    'February 2024': [
+      { edition: 'Remastered', price: 26.99, currency: '$', discount: 10 },
+      { edition: 'Remastered', price: 29.99, currency: '$' },
+    ],
+  },
+};
