bd51776bbe1b3e210bdc5a055d8d1ba5ff291cd5
celso <celso.sjunior@outlook.com>
2025-10-08 03:44:25 -0300

    Atualizar o IndianaJones.ts

--- 2k38/cycle/src/data/games/IndianaJones.ts
+++ 2k38/cycle/src/data/games/IndianaJones.ts
@@ -1,7 +1,13 @@
 export const indianajones = {
   launchDate: '2025-04-16',
   Brazil: {
+    'October 2025': [
+      { edition: "Premium + DLC", price: 127.92, discount: 20, currency: 'R$', exchangeTax: 4.56 },
+      { edition: "Premium Edition", price: 349.65, discount: 30, currency: 'R$', exchangeTax: 4.99 },
+      { edition: "Standard Edition", price: 279.92, discount: 20, currency: 'R$', exchangeTax: 5.69 },
+    ],
     'September 2025': [
+      { edition: "Premium + DLC", price: 159.90, currency: 'R$', exchangeTax: 4.56 },
       { edition: "The Order Of Giants", price: 99.90, currency: 'R$', exchangeTax: 5 },
       { edition: "Standard Edition", price: 279.92, discount: 20, currency: 'R$', exchangeTax: 5.69 },
     ],
@@ -11,7 +17,13 @@ export const indianajones = {
     ],
   },
   Europe: {
+    'October 2025': [
+      { edition: "Premium + DLC", price: 27.99, discount: 20, currency: '€' },
+      { edition: "Premium Edition", price: 76.99, discount: 30, currency: '€' },
+      { edition: "Standard Edition", price: 63.99, discount: 20, currency: '€' },
+    ],
     'September 2025': [
+      { edition: "Premium + DLC", price: 34.99, currency: '€' },
       { edition: "The Order Of Giants", price: 19.99, currency: '€' },
       { edition: "Standard Edition", price: 63.99, discount: 20,  currency: '€' },
     ],
@@ -21,7 +33,13 @@ export const indianajones = {
     ],
   },
   UnitedKingdom: {
+    'October 2025': [
+      { edition: "Premium + DLC", price: 26.39, discount: 20, currency: '£' },
+      { edition: "Premium Edition", price: 69.99, discount: 30, currency: '£' },
+      { edition: "Standard Edition", price: 55.99, discount: 20, currency: '£' },
+    ],
     'September 2025': [
+      { edition: "Premium + DLC", price: 32.99, currency: '£' },
       { edition: "The Order Of Giants", price: 17.99, currency: '£' },
       { edition: "Standard Edition", price: 55.99, discount: 20, currency: '£' },
     ],
@@ -31,7 +49,11 @@ export const indianajones = {
     ],
   },
   UnitedStates: {
+    'October 2025': [
+      { edition: "Premium + DLC", price: 27.99, discount: 20, currency: '$' },
+    ],
     'September 2025': [
+      { edition: "Premium + DLC", price: 34.99, currency: '$' },
       { edition: "The Order Of Giants", price: 19.99, currency: '$' },
       { edition: "Standard Edition", price: 55.99, discount: 20, currency: '$' },
     ],
