da2ccec0ffb441e5b9011956dd9c9e37065f8940
celso <celso.sjunior@outlook.com>
2025-10-08 17:14:37 -0300

    Criar ResidentEvil6.ts

---
+++ 2k38/cycle/src/data/games/ResidentEvil6.ts
@@ -0,0 +1,23 @@
+export const residentevil6 = {
+  launchDate: '2016-06-28',
+  Brazil: {
+    'June 2016': [
+      { edition: 'Standard Edition', price: 149.00, currency: 'R$', exchangeTax: 7.45 },
+    ],
+  },
+  Europe: {
+    'June 2016': [
+      { edition: 'Standard Edition', price: 19.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'June 2016': [
+      { edition: 'Standard Edition', price: 15.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'June 2016': [
+      { edition: 'Standard Edition', price: 19.99, currency: '$' },
+    ],
+  },
+};
