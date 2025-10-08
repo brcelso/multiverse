d8893c9fa548e8307859c2ad457bd7292cd54ac4
celso <celso.sjunior@outlook.com>
2025-10-08 16:54:45 -0300

    Criar ResidentEvil.ts

---
+++ 2k38/cycle/src/data/games/ResidentEvil.ts
@@ -0,0 +1,35 @@
+export const residentevil = {
+  launchDate: '2015-01-20',
+  Brazil: {
+    'July 2025': [
+      { edition: 'Standard Edition', price: 20.83, discount: 75, currency: 'R$', exchangeTax: 4.17 },
+    ],
+    'January 2015': [
+      { edition: 'Standard Edition', price: 83.50, currency: 'R$', exchangeTax: 4.17 },
+    ],
+  },
+  Europe: {
+    'July 2025': [
+      { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '€' },
+    ],
+    'January 2015': [
+      { edition: 'Standard Edition', price: 19.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'July 2025': [
+      { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '£' },
+    ],
+    'January 2015': [
+      { edition: 'Standard Edition', price: 19.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'July 2025': [
+      { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '$' },
+    ],
+    'January 2015': [
+      { edition: 'Standard Edition', price: 19.99, currency: '$' },
+    ],
+  },
+};
