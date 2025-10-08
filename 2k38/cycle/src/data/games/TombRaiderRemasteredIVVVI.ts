323639405de29d6105fe4d1f9ca2fe352675c028
celso <celso.sjunior@outlook.com>
2025-10-08 10:00:52 -0300

    Atualizar o TombRaiderRemasteredIVVVI.ts

--- 2k38/cycle/src/data/games/TombRaiderRemasteredIVVVI.ts
+++ 2k38/cycle/src/data/games/TombRaiderRemasteredIVVVI.ts
@@ -1,43 +1,42 @@
 export const tombRaiderRemasteredIVVVI = {
   launchDate: '2025-02-14',
-
   Brazil: {
     'February 2025': [
-      { edition: 'Regular', price: 143.91, currency: 'R$', discount: 10 },
-      { edition: 'Regular', price: 159.99, currency: 'R$', exchangeTax: 5.33 },
+      { edition: 'Remastered', price: 143.91, currency: 'R$', discount: 10 },
+      { edition: 'Remastered', price: 159.99, currency: 'R$', exchangeTax: 5.33 },
     ],
     'March 2025': [
-      { edition: 'Regular', price: 119.92, currency: 'R$', discount: 25 },
+      { edition: 'Remastered', price: 119.92, currency: 'R$', discount: 25 },
     ],
   },
   Europe: {
     'February 2025': [
-      { edition: 'Regular', price: 26.99, currency: '€', discount: 10 },
-      { edition: 'Regular', price: 29.99, currency: '€' },
+      { edition: 'Remastered', price: 26.99, currency: '€', discount: 10 },
+      { edition: 'Remastered', price: 29.99, currency: '€' },
     ],
     'March 2025': [
-      { edition: 'Regular', price: 22.49, currency: '€', discount: 25 },
+      { edition: 'Remastered', price: 22.49, currency: '€', discount: 25 },
     ],
   },
   UnitedKingdom: {
     'February 2025': [
-      { edition: 'Regular', price: 22.49, currency: '£', discount: 10 },
-      { edition: 'Regular', price: 24.99, currency: '£' },
+      { edition: 'Remastered', price: 22.49, currency: '£', discount: 10 },
+      { edition: 'Remastered', price: 24.99, currency: '£' },
     ],
     'March 2025': [
-      { edition: 'Regular', price: 18.74, currency: '£', discount: 25 },
+      { edition: 'Remastered', price: 18.74, currency: '£', discount: 25 },
     ],
   },  
   UnitedStates: {
     'October 2025': [
-      { edition: 'Regular', price: 14.99, currency: '$', discount: 50 },
+      { edition: 'Remastered', price: 14.99, currency: '$', discount: 50 },
     ],
     'February 2025': [
-      { edition: 'Regular', price: 26.99, currency: '$', discount: 10 },
-      { edition: 'Regular', price: 29.99, currency: '$' },
+      { edition: 'Remastered', price: 26.99, currency: '$', discount: 10 },
+      { edition: 'Remastered', price: 29.99, currency: '$' },
     ],
     'March 2025': [
-      { edition: 'Regular', price: 22.49, currency: '$', discount: 25 },
+      { edition: 'Remastered', price: 22.49, currency: '$', discount: 25 },
     ],
   },
 };
