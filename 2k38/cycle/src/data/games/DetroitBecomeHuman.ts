33fc0c98fd2237a2c5d3a418fc85fd2ae0e35b9e
celso <celso.sjunior@outlook.com>
2025-10-14 12:24:01 -0300

    Atualizar o DetroitBecomeHuman.ts

--- 2k38/cycle/src/data/games/DetroitBecomeHuman.ts
+++ 2k38/cycle/src/data/games/DetroitBecomeHuman.ts
@@ -3,9 +3,10 @@ export const detroitbecomehuman = {
   Brazil: {
     'October 2025': [
       { platform: "Playstation", edition: 'Standard Edition PS4', price: 71.50, currency: 'R$', exchangeTax2: 3.57, totalDiscount: 50 },
-    ],
+      { platform: "Steam", edition: 'Standard Edition', price: 23.99, totalDiscount: 80, currency: 'R$', exchangeTax2: 3 },
+],
     'January 2019': [
-      { platform: "Steam", edition: 'Standard Edition', price: 59.99, totalDiscount: 66.6, currency: 'R$', exchangeTax2: 3 },
+      { platform: "Steam", edition: 'Standard Edition', price: 119.99, totalDiscount: 33.3, currency: 'R$', exchangeTax2: 3 },
     ],
     'March 2016': [
       { platform: "Playstation", edition: 'Standard Edition PS4', price: 149.90, totalDiscount: 25, currency: 'R$', exchangeTax2: 5 },
@@ -31,9 +32,10 @@ export const detroitbecomehuman = {
   UnitedKingdom: {
     'October 2025': [
       { platform: "Playstation", edition: 'Standard Edition PS4', price: 16.99, currency: '£', totalDiscount: 50 },
+      { platform: "Steam", edition: 'Standard Edition', price: 6.79, discount: 80, currency: '£' },
     ],
     'January 2019': [
-      { platform: "Steam", edition: 'Standard Edition', price: 19.99, discount: 33.3, totalDiscount: 66.6, currency: '£' },
+      { platform: "Steam", edition: 'Standard Edition', price: 33.99, discount: 33.3, totalDiscount: 66.6, currency: '£' },
     ],
     'March 2016': [
       { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, totalDiscount: 50, currency: '£' },
