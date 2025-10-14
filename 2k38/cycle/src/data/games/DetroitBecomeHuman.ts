6fcf490f977b33bbc155582a8464fa7e39c70edd
celso <celso.sjunior@outlook.com>
2025-10-13 22:38:32 -0300

    Criar DetroitBecomeHuman.ts

---
+++ 2k38/cycle/src/data/games/DetroitBecomeHuman.ts
@@ -0,0 +1,59 @@
+export const detroitbecomehuman = {
+  launchDate: '2018-05-25',
+  Brazil: {
+    'October 2025': [
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 99.99, currency: 'R$', exchangeTax2: 3.33, totalDiscount: 50 },
+    ],
+    'January 2019': [
+      { platform: "Steam", edition: 'Standard Edition', price: 59.99, totalDiscount: 66.6, currency: 'R$', exchangeTax2: 3 },
+    ],
+    'March 2016': [
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 149.90, totalDiscount: 25, currency: 'R$', exchangeTax2: 5 },
+    ],
+    'February 2010': [
+      { platform: "Playstation", edition: 'Standard Edition PS3', price: 199.99, currency: 'R$', exchangeTax2: 3.33 },
+    ],
+  },
+  Europe: {
+    'October 2025': [
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, currency: '€', totalDiscount: 50 },
+    ],
+    'January 2019': [
+      { platform: "Steam", edition: 'Standard Edition', price: 19.99, discount: 33.3, totalDiscount: 66.6, currency: '€' },
+    ],
+    'March 2016': [
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, totalDiscount: 50, currency: '€' },
+    ],
+    'February 2010': [
+      { platform: "Playstation", edition: 'Standard Edition PS3', price: 59.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'October 2025': [
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, currency: '£', totalDiscount: 50 },
+    ],
+    'January 2019': [
+      { platform: "Steam", edition: 'Standard Edition', price: 19.99, discount: 33.3, totalDiscount: 66.6, currency: '£' },
+    ],
+    'March 2016': [
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, totalDiscount: 50, currency: '£' },
+    ],
+    'February 2010': [
+      { platform: "Playstation", edition: 'Standard Edition PS3', price: 59.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, currency: '$', totalDiscount: 50 },
+    ],
+    'January 2019': [
+      { platform: "Steam", edition: 'Standard Edition', price: 19.99, discount: 33.3, totalDiscount: 66.6, currency: '$' },
+    ],
+    'March 2016': [
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, totalDiscount: 50, currency: '$' },
+    ],
+    'February 2010': [
+      { platform: "Playstation", edition: 'Standard Edition PS3', price: 59.99, currency: '$' },
+    ],
+  },
+}
