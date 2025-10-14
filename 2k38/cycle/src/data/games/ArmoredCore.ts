436d41d08fa0359653dffb7ca900ca6d0e6f55c6
celso <celso.sjunior@outlook.com>
2025-10-14 12:30:13 -0300

    Criar ArmoredCore.ts

---
+++ 2k38/cycle/src/data/games/ArmoredCore.ts
@@ -0,0 +1,111 @@
+export const armoredcore = {
+  launchDate: '2023-08-24', 
+  Brazil: {
+    'October 2025': [
+      {
+        platform: "Steam",
+        edition: 'Digital Deluxe Edition',
+        price: 205.99,
+        discount: 48.5,
+        currency: 'R$',
+        exchangeTax: 2.57,
+      },
+      {
+        platform: "Steam",
+        edition: 'Standard Edition',
+        price: 171.89,
+        discount: 51,
+        currency: 'R$',
+        exchangeTax: 2.45,
+      },
+    ],
+    'March 2025': [
+      {
+        platform: "Playstation",
+        edition: 'Standard Edition',
+        price: 199.44,
+        discount: 43,
+        currency: 'R$',
+        basePrice: 349.9, 
+        exchangeTax: 2.84,
+      },
+    ],
+    'February 2025': [
+      {
+        platform: "Playstation",
+        edition: 'Digital Deluxe Edition',
+        price: 247.69,
+        discount: 38,
+        currency: 'R$',
+        basePrice: 399.5,
+        exchangeTax: 3.09,
+      },
+    ],
+    'October 2023': [
+      {
+        platform: "Playstation",
+        edition: 'Digital Deluxe Edition',
+        price: 399.5,
+        currency: 'R$',
+        exchangeTax: 4.99,
+      },
+      {
+        platform: "Playstation",
+        edition: 'Standard Edition',
+        price: 349.9,
+        currency: 'R$',
+        exchangeTax: 4.99,
+      },
+    ],
+  },
+  Europe: {
+    'February 2025': [
+      {
+        platform: "Playstation",
+        edition: 'Digital Deluxe Edition',
+        price: 59.39,
+        discount: 34,
+        currency: '€',
+      },
+    ],
+    'October 2023': [
+      {
+        platform: "Playstation",
+        edition: 'Digital Deluxe Edition',
+        price: 89.99,
+        currency: '€',
+      },
+      {
+        platform: "Playstation",
+        edition: 'Standard Edition',
+        price: 79.99,
+        currency: '€',
+      },
+    ],
+   },
+  UnitedStates: {
+    'February 2025': [
+      {
+        platform: "Playstation",
+        edition: 'Digital Deluxe Edition',
+        price: 49.59,
+        discount: 38,
+        currency: '$',
+      },
+    ],
+    'October 2023': [
+      {
+        platform: "Playstation",
+        edition: 'Digital Deluxe Edition',
+        price: 79.99,
+        currency: '$',
+      },
+      {
+        platform: "Playstation",
+        edition: 'Standard Edition',
+        price: 69.99,
+        currency: '$',
+      },
+    ],
+  },
+};
