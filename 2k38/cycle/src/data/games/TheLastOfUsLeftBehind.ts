ab231b32297ef3275eb45cee5e21cf2e43da557c
celso <celso.sjunior@outlook.com>
2025-10-16 16:52:49 -0300

    Criar TheLastOfUsLeftBehind.ts

---
+++ 2k38/cycle/src/data/games/TheLastOfUsLeftBehind.ts
@@ -0,0 +1,29 @@
+export const leftbehind = {
+  launchDate: '2015-05-11',
+  Japan: {
+    'April 2015': [
+      { edition: "DLC", price: 11980, currency: '¥' },
+    ],
+  },
+  Brazil: {
+    'April 2015': [
+      { edition: "DLC", price: 29.99, currency: 'R$', exchangeTax: 5.69 },
+    ],
+  },
+  Europe: {
+    'April 2015': [
+      { edition: "DLC", price: 9.99, currency: '€' },
+    ],
+  },
+
+  UnitedKingdom: {
+    'April 2015': [
+      { edition: "DLC", price: 7.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'April 2015': [
+      { edition: "DLC", price: 9.99, currency: '$' },
+    ],
+  },
+};
