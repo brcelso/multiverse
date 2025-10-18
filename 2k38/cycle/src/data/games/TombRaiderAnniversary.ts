5053fdcd042fefc76d0d61e14bd126c797574c75
celso <celso.sjunior@outlook.com>
2025-10-18 10:33:51 -0300

    Criar TombRaiderAnniversary.ts

---
+++ 2k38/cycle/src/data/games/TombRaiderAnniversary.ts
@@ -0,0 +1,75 @@
+export const tombraideranniversary = {
+  launchDate: '2007-06-05',
+  Brazil: {
+    'September 2025': [
+      { platform: "Steam", edition: 'Standard Edition', price: 4.12, currency: 'R$', exchangeTax: 4.20, discount: 89 },
+    ],
+    'September 2023': [
+      { platform: "Steam", edition: 'Standard Edition', price: 37.49, currency: 'R$', exchangeTax: 4.17, increase: 121 },
+    ],
+    'December 2017': [
+      { platform: "Steam", edition: 'Standard Edition', price: 1.86, currency: 'R$', exchangeTax: 1.89, discount: 89 },
+    ],
+    'November 2014': [
+      { platform: "Steam", edition: 'Standard Edition', price: 8.49, currency: 'R$', exchangeTax: 1.88, discount: 50 },
+      { platform: "Steam", edition: 'Standard Edition', price: 16.99, currency: 'R$', exchangeTax: 1.88 },
+    ],
+  },
+  Europe: {
+    'September 2025': [
+      { platform: "Steam", edition: 'Standard Edition', price: 0.98, currency: '€', discount: 89 },
+    ],
+    'December 2017': [
+      { platform: "Steam", edition: 'Standard Edition', price: 0.98, currency: '€', discount: 89 },
+    ],
+    'September 2013': [
+      { platform: "Steam", edition: 'Standard Edition', price: 8.99, currency: '€', totalDiscount: 10 },
+    ],
+    'November 2012': [
+      { platform: "Steam", edition: 'Standard Edition', price: 2.49, currency: '€', discount: 75 },
+    ],
+    'June 2007': [
+      { platform: "Playstation", edition: 'Standard Edition', price: 19.99, currency: '€', },
+      { platform: "Steam", edition: 'Standard Edition', price: 9.99, currency: '€', },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { platform: "Steam", edition: 'Standard Edition', price: 0.82, currency: '£', discount: 89 },
+    ],
+    'December 2017': [
+      { platform: "Steam", edition: 'Standard Edition', price: 0.76, currency: '£', discount: 89 },
+    ],
+    'September 2013': [
+      { platform: "Steam", edition: 'Standard Edition', price: 6.99, currency: '£' },
+    ],
+    'November 2012': [
+      { platform: "Steam", edition: 'Standard Edition', price: 4.19, currency: '£', discount: 65 },
+    ],
+    'June 2007': [
+      { platform: "Playstation", edition: 'Standard Edition', price: 16.99, currency: '£', },
+      { platform: "Steam", edition: 'Standard Edition', price: 6.99, currency: '£', },
+    ],
+  },
+  UnitedStates: {
+    'September 2025': [
+      { platform: "Steam", edition: 'Standard Edition', price: 0.98, currency: '$', discount: 89 },
+    ],
+    'December 2017': [
+      { platform: "Steam", edition: 'Standard Edition', price: 0.98, currency: '$', discount: 89 },
+    ],
+    'November 2014': [
+      { platform: "Steam", edition: 'Standard Edition', price: 4.49, currency: '$', discount: 50 },
+    ],
+    'September 2013': [
+      { platform: "Steam", edition: 'Standard Edition', price: 8.99, currency: '$', totalDiscount: 10 },
+    ],
+    'November 2012': [
+      { platform: "Steam", edition: 'Standard Edition', price: 2.49, currency: '$', discount: 75 },
+    ],
+    'June 2007': [
+      { platform: "Playstation", edition: 'Standard Edition', price: 19.99, currency: '$' },
+      { platform: "Steam", edition: 'Standard Edition', price: 9.99, currency: '$' },
+    ],
+  },
+};
