f6fad0b79174a58a6740b056e131a9ca4613a8be
celso <celso.sjunior@outlook.com>
2025-10-22 23:19:08 -0300

    Atualizar o ResidentEvil5.ts

--- 2k38/cycle/src/data/games/ResidentEvil5.ts
+++ 2k38/cycle/src/data/games/ResidentEvil5.ts
@@ -1,21 +1,33 @@
 export const residentevil5 = {
   launchDate: '2016-06-28',
   Brazil: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 28.72, discount: 75, currency: 'R$', exchangeTax: 5.74 },
+    ],
     'June 2016': [
-      { edition: 'Standard Edition', price: 149.00, currency: 'R$', exchangeTax: 7.45 },
+      { edition: 'Standard Edition', price: 114.90, currency: 'R$', exchangeTax: 5.74 },
     ],
   },
   Europe: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '€' },
+    ],
     'June 2016': [
       { edition: 'Standard Edition', price: 19.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 3.99, discount: 75, currency: '£' },
+    ],
     'June 2016': [
       { edition: 'Standard Edition', price: 15.99, currency: '£' },
     ],
   },
   UnitedStates: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '$' },
+    ],
     'June 2016': [
       { edition: 'Standard Edition', price: 19.99, currency: '$' },
     ],
