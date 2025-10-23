a701926cf4fa4ad606b59c794a747559f6fae931
celso <celso.sjunior@outlook.com>
2025-10-22 23:05:58 -0300

    Atualizar o CronosTheNewDawn.ts

--- 2k38/cycle/src/data/games/CronosTheNewDawn.ts
+++ 2k38/cycle/src/data/games/CronosTheNewDawn.ts
@@ -1,6 +1,9 @@
 export const cronos = {
   launchDate: '2025-09-05',
   Brazil: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 288.91, discount: 15, currency: 'R$', exchangeTax: 5.66 },
+    ],
     'September 2025': [
       { edition: 'Standard Edition', price: 289, discount: 15, currency: 'R$', exchangeTax: 4.81 },
       { edition: 'Deluxe Edition', price: 399.90, currency: 'R$', exchangeTax: 5.71 },
@@ -10,18 +13,27 @@ export const cronos = {
     ],
   },
   Europe: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 50.99, discount: 15, currency: '€' },
+    ],
     'September 2025': [
       { edition: 'Deluxe Edition', price: 69.99, currency: '€' },
       { edition: 'Standard Edition', price: 59.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 42.49, discount: 15, currency: '£' },
+    ],
     'September 2025': [
       { edition: 'Deluxe Edition', price: 54.99, currency: '£' },
       { edition: 'Standard Edition', price: 49.99, currency: '£' },
     ],
   },
   UnitedStates: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 50.99, discount: 15, currency: '$' },
+    ],
     'September 2025': [
       { edition: 'Deluxe Edition', price: 69.99, currency: '$' },
       { edition: 'Standard Edition', price: 59.99, currency: '$' },
