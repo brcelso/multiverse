a9ee4044aa9200c53d12a8930a86a4577729dada
celso <celso.sjunior@outlook.com>
2025-10-22 21:31:17 -0300

    Criar CallOfDutyModernWarfareII.ts

---
+++ 2k38/cycle/src/data/games/CallOfDutyModernWarfareII.ts
@@ -0,0 +1,35 @@
+export const callOfDutyModernWarfareII = {
+  launchDate: '2022-10-27',
+  Brazil: {
+    'October 2025': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 127.60, currency: 'R$', discount: 60, exchangeTax2: 4.55 },
+    ],
+    'October 2022': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 319.00, currency: 'R$', exchangeTax2: 4.55 },
+    ],
+  },
+  Europe: {
+    'October 2025': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', discount: 60, price: 31.99, currency: '€' },
+    ],
+    'October 2022': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 79.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'October 2025': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', discount: 60, price: 27.99, currency: '£' },
+    ],
+    'October 2022': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 69.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', discount: 60, price: 27.99, currency: '$' },
+    ],
+    'October 2022': [
+      { platform: "Playstation 5", edition: 'Cross-Gen Bundle', price: 69.99, currency: '$' },
+    ],
+  },
+};
