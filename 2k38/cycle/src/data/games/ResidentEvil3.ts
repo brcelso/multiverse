a4d2a8fcc5211b30dcb4fd67e1ca173cb3678c54
celso <celso.sjunior@outlook.com>
2025-10-22 22:57:35 -0300

    Criar ResidentEvil3.ts

---
+++ 2k38/cycle/src/data/games/ResidentEvil3.ts
@@ -0,0 +1,39 @@
+export const residentevil3 = {
+  launchDate: '2022-06-13',
+  Brazil: {
+    'October 2025': [
+      { edition: "Standard Edition", price: 49.87, discount: 75, currency: 'R$', exchangeTax: 5 },
+    ],
+    'June 2022': [
+      { edition: "Deluxe Edition", price: 207.90, currency: 'R$', exchangeTax: 4.15 },
+      { edition: "Standard Edition", price: 199.50, currency: 'R$', exchangeTax: 5 },
+    ],
+  },
+  Europe: {
+    'October 2025': [
+      { edition: "Standard Edition", price: 9.99, discount: 75, currency: '€' },
+    ],
+    'June 2022': [
+      { edition: "Deluxe Edition", price: 49.99, currency: '€' },
+      { edition: "Standard Edition", price: 39.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'October 2025': [
+      { edition: "Standard Edition", price: 8.74, discount: 75, currency: '£' },
+    ],
+    'June 2022': [
+      { edition: "Deluxe Edition", price: 44.99, currency: '£' },
+      { edition: "Standard Edition", price: 34.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { edition: "Standard Edition", price: 9.99, discount: 75, currency: '$' },
+    ],
+    'June 2022': [
+      { edition: "Deluxe Edition", price: 49.99, currency: '$' },
+      { edition: "Standard Edition", price: 39.99, currency: '$' },
+    ],
+  },
+};
