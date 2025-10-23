d4a2def5aa2cee38a75d18dc5e679b00bbd44cb7
celso <celso.sjunior@outlook.com>
2025-10-22 22:00:41 -0300

    Atualizar o DaysGone.ts

--- 2k38/cycle/src/data/games/DaysGone.ts
+++ 2k38/cycle/src/data/games/DaysGone.ts
@@ -2,6 +2,8 @@ export const daysgone = {
   launchDate: '2019-04-26',
   Brazil: {
     'October 2025': [
+      { platform: "Playstation", edition: 'Remastered', price: 187.12, discount: 25, currency: 'R$', exchangeTax: 5 },
+      { platform: "Playstation | Steam", edition: 'Standard Edition PS4', price: 158.63, discount: 31, currency: 'R$', exchangeTax: 5.74 },
       { platform: "Steam", edition: 'Standard Edition', price: 99.75, discount: 50, currency: 'R$', exchangeTax: 5 },
     ],
     'December 2024': [
@@ -18,6 +20,11 @@ export const daysgone = {
     ],
   },
   Europe: {
+    'October 2025': [
+      { platform: "Playstation", edition: 'Remastered', price: 37.49, discount: 25, currency: '€' },
+      { platform: "Playstation | Steam", edition: 'Standard Edition PS4', price: 27.59, discount: 31, currency: '€' },
+      { platform: "Steam", edition: 'Standard Edition', discount: 50, price: 19.99, currency: '€' },
+    ],
     'April 2019': [
       { platform: "Playstation", edition: 'Remastered', price: 49.99, currency: '€' },
       { platform: "Playstation", edition: 'Digital Deluxe Edition PS4', price: 49.99, currency: '€' },
@@ -25,6 +32,11 @@ export const daysgone = {
     ],
   },
   UnitedKingdom: {
+    'October 2025': [
+      { platform: "Playstation", edition: 'Remastered', price: 33.74, discount: 25, currency: '£' },
+      { platform: "Playstation | Steam", edition: 'Standard Edition PS4', price: 24.14, discount: 31, currency: '£' },
+      { platform: "Steam", edition: 'Standard Edition', discount: 50, price: 19.99, currency: '£' },
+    ],
     'April 2019': [
       { platform: "Playstation", edition: 'Remastered', price: 44.99, currency: '£' },
       { platform: "Playstation", edition: 'Digital Deluxe Edition PS4', price: 44.99, currency: '£' },
@@ -33,6 +45,8 @@ export const daysgone = {
   },
   UnitedStates: {
     'October 2025': [
+      { platform: "Playstation", edition: 'Remastered', price: 37.49, discount: 25, currency: '$' },
+      { platform: "Playstation | Steam", edition: 'Standard Edition PS4', price: 27.59, discount: 31, currency: '$' },
       { platform: "Steam", edition: 'Standard Edition', discount: 50, price: 19.99, currency: '$' },
     ],
     'December 2024': [
