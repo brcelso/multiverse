c0913449a29a79c1b59e58d6ca654c28f061745a
Celso <celso.sjunior@outlook.com>
2025-10-23 09:06:26 -0300

    crash 4

--- 2k38/cycle/src/data/games/Crash4.ts
+++ 2k38/cycle/src/data/games/Crash4.ts
@@ -1,13 +1,32 @@
 export const crash4ItsAboutTime = {
   launchDate: '2020-10-02',
-
   Brazil: {
+    'September 2025': [
+      { edition: 'Standard Edition', price: 82.46, currency: 'R$', exchangeTax2: 4.16, discount: 67 },
+    ],
     'February 2025': [
-      { edition: 'Standard Edition', price: 82.46, currency: 'R$', discount: 67 },
-      { edition: 'Standard Edition', price: 249.9, currency: 'R$' },
+      { edition: 'Standard Edition', price: 82.46, currency: 'R$', exchangeTax2: 4.16, discount: 67 },
+      { edition: 'Standard Edition', price: 249.9, currency: 'R$', exchangeTax2: 4.16 },
     ],
   },
-
+  Europe: {
+    'September 2025': [
+      { edition: 'Standard Edition', price: 23.09, currency: '€', discount: 67 },
+    ],
+    'February 2025': [
+      { edition: 'Standard Edition', price: 23.09, currency: '€', discount: 67 },
+      { edition: 'Standard Edition', price: 69.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'September 2025': [
+      { edition: 'Standard Edition', price: 19.79, currency: '£', discount: 67 },
+    ],
+    'February 2025': [
+      { edition: 'Standard Edition', price: 19.79, currency: '£', discount: 67 },
+      { edition: 'Standard Edition', price: 59.99, currency: '£' },
+    ],
+  },
   UnitedStates: {
     'September 2025': [
       { edition: 'Standard Edition', price: 19.79, currency: '$', discount: 67 },
