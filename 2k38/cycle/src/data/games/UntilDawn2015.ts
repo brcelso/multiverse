e52bbea9d7bc38041257de67bd068e25e8b9adbd
celso <celso.sjunior@outlook.com>
2025-10-22 22:27:26 -0300

    Criar UntilDawn2015.ts

---
+++ 2k38/cycle/src/data/games/UntilDawn2015.ts
@@ -0,0 +1,35 @@
+export const untilDawn2015 = {
+  launchDate: '2015-08-25',
+  Brazil: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 49.75, currency: 'R$', exchangeTax2: 5, discount: 50 },
+    ],
+    'August 2015': [
+      { edition: 'Standard Edition', price: 99.50, currency: 'R$', exchangeTax2: 5 },
+    ],
+  },
+  Europe: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 9.99, currency: '€', discount: 50 },
+    ],
+    'August 2015': [
+      { edition: 'Standard Edition', price: 19.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 7.99, currency: '£', discount: 50 },
+    ],
+    'August 2015': [
+      { edition: 'Standard Edition', price: 15.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 9.99, currency: '$', discount: 50 },
+    ],
+    'August 2015': [
+      { edition: 'Standard Edition', price: 19.99, currency: '$' },
+    ],
+  },
+};
