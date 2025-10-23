60518a65c75dbd0666fd31f11d180c51c4e74f56
Celso <celso.sjunior@outlook.com>
2025-10-23 12:15:06 -0300

    updated detroit

--- 2k38/cycle/src/data/games/DetroitBecomeHuman.ts
+++ 2k38/cycle/src/data/games/DetroitBecomeHuman.ts
@@ -2,62 +2,44 @@ export const detroitbecomehuman = {
   launchDate: '2018-05-25',
   Brazil: {
     'October 2025': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 71.50, currency: 'R$', exchangeTax2: 3.57, totalDiscount: 50 },
-      { platform: "Steam", edition: 'Standard Edition', price: 23.99, totalDiscount: 80, currency: 'R$', exchangeTax2: 3 },
-],
-    'January 2019': [
-      { platform: "Steam", edition: 'Standard Edition', price: 119.99, totalDiscount: 33.3, currency: 'R$', exchangeTax2: 3 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 35.75, 
+        discount: 50, currency: 'R$', exchangeTax2: 3.57 },
     ],
-    'March 2016': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 149.90, totalDiscount: 25, currency: 'R$', exchangeTax2: 5 },
-    ],
-    'February 2010': [
-      { platform: "Playstation", edition: 'Standard Edition PS3', price: 199.99, currency: 'R$', exchangeTax2: 3.33 },
+    'May 2018': [
+      { platform: "Playstation", edition: 'Quantic Dream Collection', price: 229.90, 
+        currency: 'R$', exchangeTax2: 5.74 },
+      { platform: "Playstation", edition: 'Deluxe Edition', price: 169.90, 
+        currency: 'R$', exchangeTax2: 5.66 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 71.50, 
+        currency: 'R$', exchangeTax2: 3.57 },
     ],
   },
   Europe: {
     'October 2025': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 19.99, currency: '€', totalDiscount: 50 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 14.99, discount: 50, currency: '€' },
     ],
-    'January 2019': [
-      { platform: "Steam", edition: 'Standard Edition', price: 19.99, discount: 33.3, totalDiscount: 66.6, currency: '€' },
-    ],
-    'March 2016': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, totalDiscount: 50, currency: '€' },
-    ],
-    'February 2010': [
-      { platform: "Playstation", edition: 'Standard Edition PS3', price: 59.99, currency: '€' },
+    'May 2018': [
+      { platform: "Playstation", edition: 'Deluxe Edition', price: 39.99, currency: '€' },
+      { platform: "Playstation", edition: 'Standard Edition', price: 29.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
     'October 2025': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 16.99, currency: '£', totalDiscount: 50 },
-      { platform: "Steam", edition: 'Standard Edition', price: 6.79, discount: 80, currency: '£' },
+      { platform: "Playstation", edition: 'Standard Edition', price: 12.49, discount: 50, currency: '£' },
     ],
-    'January 2019': [
-      { platform: "Steam", edition: 'Standard Edition', price: 33.99, currency: '£' },
-    ],
-    'March 2016': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, totalDiscount: 50, currency: '£' },
-    ],
-    'February 2010': [
-      { platform: "Playstation", edition: 'Standard Edition PS3', price: 59.99, currency: '£' },
+    'May 2018': [
+      { platform: "Playstation", edition: 'Deluxe Edition', price: 34.99, currency: '£' },
+      { platform: "Playstation", edition: 'Standard Edition', price: 24.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'October 2025': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 19.99, currency: '$', totalDiscount: 50 },
-      { platform: "Steam", edition: 'Standard Edition', price: 7.99, currency: '$', totalDiscount: 80 },
+      { platform: "Playstation", edition: 'Standard Edition', price: 9.99, discount: 50, currency: '$' },
     ],
-    'January 2019': [
-      { platform: "Steam", edition: 'Standard Edition', price: 19.99, discount: 33.3, totalDiscount: 66.6, currency: '$' },
-      { platform: "Steam", edition: 'Standard Edition', price: 39.99, currency: '$' },
-    ],
-    'March 2016': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, totalDiscount: 50, currency: '$' },
-    ],
-    'February 2010': [
-      { platform: "Playstation", edition: 'Standard Edition PS3', price: 59.99, currency: '$' },
+    'May 2018': [
+      { platform: "Playstation", edition: 'Quantic Dream Collection', price: 39.99, currency: '$' },
+      { platform: "Playstation", edition: 'Deluxe Edition', price: 29.99, currency: '$' },
+      { platform: "Playstation", edition: 'Standard Edition', price: 19.99, currency: '$' },
     ],
   },
 }
