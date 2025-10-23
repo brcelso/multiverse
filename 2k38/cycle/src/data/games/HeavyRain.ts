77a181c74699846bb31c217fe40f63aebbb1d20e
celso <celso.sjunior@outlook.com>
2025-10-22 23:12:43 -0300

    Atualizar o HeavyRain.ts

--- 2k38/cycle/src/data/games/HeavyRain.ts
+++ 2k38/cycle/src/data/games/HeavyRain.ts
@@ -2,13 +2,13 @@ export const heavyrain = {
   launchDate: '2010-02-24',
   Brazil: {
     'October 2025': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 99.99, currency: 'R$', exchangeTax2: 3.33, totalDiscount: 50 },
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 39.99, currency: 'R$', exchangeTax2: 3.33, discount: 60 },
     ],
     'January 2019': [
       { platform: "Steam", edition: 'Standard Edition', price: 59.99, totalDiscount: 66.6, currency: 'R$', exchangeTax2: 3 },
     ],
     'March 2016': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 149.90, totalDiscount: 25, currency: 'R$', exchangeTax2: 5 },
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 99.99, totalDiscount: 25, currency: 'R$', exchangeTax2: 5 },
     ],
     'February 2010': [
       { platform: "Playstation", edition: 'Standard Edition PS3', price: 199.99, currency: 'R$', exchangeTax2: 3.33 },
@@ -16,7 +16,7 @@ export const heavyrain = {
   },
   Europe: {
     'October 2025': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, currency: '€', totalDiscount: 50 },
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 11.99, currency: '€', discount: 60 },
     ],
     'January 2019': [
       { platform: "Steam", edition: 'Standard Edition', price: 19.99, discount: 33.3, totalDiscount: 66.6, currency: '€' },
@@ -30,13 +30,13 @@ export const heavyrain = {
   },
   UnitedKingdom: {
     'October 2025': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, currency: '£', totalDiscount: 50 },
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 9.99, currency: '£', discount: 60 },
     ],
     'January 2019': [
       { platform: "Steam", edition: 'Standard Edition', price: 19.99, discount: 33.3, totalDiscount: 66.6, currency: '£' },
     ],
     'March 2016': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, totalDiscount: 50, currency: '£' },
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 24.99, totalDiscount: 50, currency: '£' },
     ],
     'February 2010': [
       { platform: "Playstation", edition: 'Standard Edition PS3', price: 59.99, currency: '£' },
@@ -44,7 +44,7 @@ export const heavyrain = {
   },
   UnitedStates: {
     'October 2025': [
-      { platform: "Playstation", edition: 'Standard Edition PS4', price: 29.99, currency: '$', totalDiscount: 50 },
+      { platform: "Playstation", edition: 'Standard Edition PS4', price: 11.99, currency: '$', discount: 60 },
     ],
     'January 2019': [
       { platform: "Steam", edition: 'Standard Edition', price: 19.99, discount: 33.3, totalDiscount: 66.6, currency: '$' },
