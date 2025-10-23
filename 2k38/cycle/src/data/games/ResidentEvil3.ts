99d6e3ab346b1da30d3ee58095c262386d4a1ea7
celso <celso.sjunior@outlook.com>
2025-10-22 22:59:04 -0300

    Atualizar o ResidentEvil3.ts

--- 2k38/cycle/src/data/games/ResidentEvil3.ts
+++ 2k38/cycle/src/data/games/ResidentEvil3.ts
@@ -2,11 +2,10 @@ export const residentevil3 = {
   launchDate: '2022-06-13',
   Brazil: {
     'October 2025': [
-      { edition: "Standard Edition", price: 49.87, discount: 75, currency: 'R$', exchangeTax: 5 },
+      { edition: "Standard Edition", price: 48.62, discount: 75, currency: 'R$', exchangeTax: 4.86 },
     ],
     'June 2022': [
-      { edition: "Deluxe Edition", price: 207.90, currency: 'R$', exchangeTax: 4.15 },
-      { edition: "Standard Edition", price: 199.50, currency: 'R$', exchangeTax: 5 },
+      { edition: "Standard Edition", price: 194.50, currency: 'R$', exchangeTax: 4.86 },
     ],
   },
   Europe: {
@@ -14,7 +13,6 @@ export const residentevil3 = {
       { edition: "Standard Edition", price: 9.99, discount: 75, currency: '€' },
     ],
     'June 2022': [
-      { edition: "Deluxe Edition", price: 49.99, currency: '€' },
       { edition: "Standard Edition", price: 39.99, currency: '€' },
     ],
   },
@@ -23,7 +21,6 @@ export const residentevil3 = {
       { edition: "Standard Edition", price: 8.74, discount: 75, currency: '£' },
     ],
     'June 2022': [
-      { edition: "Deluxe Edition", price: 44.99, currency: '£' },
       { edition: "Standard Edition", price: 34.99, currency: '£' },
     ],
   },
@@ -32,7 +29,6 @@ export const residentevil3 = {
       { edition: "Standard Edition", price: 9.99, discount: 75, currency: '$' },
     ],
     'June 2022': [
-      { edition: "Deluxe Edition", price: 49.99, currency: '$' },
       { edition: "Standard Edition", price: 39.99, currency: '$' },
     ],
   },
