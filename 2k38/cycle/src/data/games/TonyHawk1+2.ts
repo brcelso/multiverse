29090540daa9b0550af41f59cdf46af3f147a9ff
Celso <celso.sjunior@outlook.com>
2025-10-23 09:16:06 -0300

    tony 1+2

--- 2k38/cycle/src/data/games/TonyHawk1+2.ts
+++ 2k38/cycle/src/data/games/TonyHawk1+2.ts
@@ -1,27 +1,67 @@
 export const tonyHawk12 = {
   launchDate: '2020-09-04',
-
-  UnitedStates: {
+  Brasil: {
+    'October 2025': [
+      { edition: 'Cross-Gen Deluxe Bundle', price: 91.60, currency: 'R$', exchangeTax2: 4.58, discount: 60 },
+      { edition: 'Standard Edition', price: 79.70, currency: 'R$', exchangeTax2: 4.97, discount: 60 },
+    ],
     'February 2025': [
-      { edition: 'Standard Edition', price: 19.99, currency: '$', discount: 60 },
+      { edition: 'Standard Edition', price: 79.70, currency: 'R$', exchangeTax2: 4.97, discount: 60 },
     ],
     'March 2025': [
-      { edition: 'Standard Edition', price: 19.99, currency: '$', discount: 60 },
+      { edition: 'Standard Edition', price: 79.70, currency: 'R$', exchangeTax2: 4.97, discount: 60 },
     ],
     'September 2020': [
-      { edition: 'Standard Edition', price: 49.99, currency: '$' },
+      { edition: 'Cross-Gen Deluxe Bundle', price: 229.00, currency: 'R$', exchangeTax2: 4.58 },
+      { edition: 'Standard Edition', price: 199.00, currency: 'R$', exchangeTax2: 4.97 },
     ],
   },
-
-  UnitedKingdom: {
+  Europe: {
+    'October 2025': [
+      { edition: 'Cross-Gen Deluxe Bundle', price: 21.99, currency: '€', discount: 60 },
+      { edition: 'Standard Edition', price: 17.99, currency: '€', discount: 60 },
+    ],
     'February 2025': [
-      { edition: 'Standard Edition', price: 19.99, currency: '£', discount: 60 },
+      { edition: 'Standard Edition', price: 17.99, currency: '€', discount: 60 },
     ],
     'March 2025': [
-      { edition: 'Standard Edition', price: 19.99, currency: '£', discount: 60 },
+      { edition: 'Standard Edition', price: 17.99, currency: '€', discount: 60 },
     ],
     'September 2020': [
-      { edition: 'Standard Edition', price: 49.99, currency: '£' },
+      { edition: 'Cross-Gen Deluxe Bundle', price: 54.99, currency: '€' },
+      { edition: 'Standard Edition', price: 44.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'October 2025': [
+      { edition: 'Cross-Gen Deluxe Bundle', price: 19.99, currency: '£', discount: 60 },
+      { edition: 'Standard Edition', price: 15.99, currency: '£', discount: 60 },
+    ],
+    'February 2025': [
+      { edition: 'Standard Edition', price: 15.99, currency: '£', discount: 60 },
+    ],
+    'March 2025': [
+      { edition: 'Standard Edition', price: 15.99, currency: '£', discount: 60 },
+    ],
+    'September 2020': [
+      { edition: 'Cross-Gen Deluxe Bundle', price: 49.99, currency: '£' },
+      { edition: 'Standard Edition', price: 39.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { edition: 'Cross-Gen Deluxe Bundle', price: 19.99, currency: '$', discount: 60 },
+      { edition: 'Standard Edition', price: 15.99, currency: '$', discount: 60 },
+    ],
+    'February 2025': [
+      { edition: 'Standard Edition', price: 15.99, currency: '$', discount: 60 },
+    ],
+    'March 2025': [
+      { edition: 'Standard Edition', price: 15.99, currency: '$', discount: 60 },
+    ],
+    'September 2020': [
+      { edition: 'Cross-Gen Deluxe Bundle', price: 49.99, currency: '$' },
+      { edition: 'Standard Edition', price: 39.99, currency: '$' },
     ],
   },
 };
