52e8c746ae8cb339f2a9c99a6eb4e1524607b7cb
celso <celso.sjunior@outlook.com>
2025-10-22 23:23:04 -0300

    Criar ResidentEvil4old.ts

---
+++ 2k38/cycle/src/data/games/ResidentEvil4old.ts
@@ -0,0 +1,35 @@
+export const residentevil4old = {
+  launchDate: '2016-08-30',
+  Brazil: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 37.49, discount: 75, currency: 'R$', exchangeTax: 7.50 },
+    ],
+    'June 2016': [
+      { edition: 'Standard Edition', price: 149.99, currency: 'R$', exchangeTax: 7.50 },
+    ],
+  },
+  Europe: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '€' },
+    ],
+    'June 2016': [
+      { edition: 'Standard Edition', price: 19.99, currency: '€' },
+    ],
+  },
+  UnitedKingdom: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 3.99, discount: 75, currency: '£' },
+    ],
+    'June 2016': [
+      { edition: 'Standard Edition', price: 15.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 4.99, discount: 75, currency: '$' },
+    ],
+    'June 2016': [
+      { edition: 'Standard Edition', price: 19.99, currency: '$' },
+    ],
+  },
+};
