e7dfd0f874c4fb4c2a4b1ad2fd94c55fd5abcdc4
celso <celso.sjunior@outlook.com>
2025-10-22 21:52:39 -0300

    Atualizar o DoomTheDarkAges.ts

--- 2k38/cycle/src/data/games/DoomTheDarkAges.ts
+++ 2k38/cycle/src/data/games/DoomTheDarkAges.ts
@@ -1,7 +1,9 @@
 export const doomTheDarkAges = {
   launchDate: '2025-05-13',
-
   Brazil: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 234.43, discount: 33, currency: 'R$', exchangeTax: 4.99 },
+    ],
     'July 2025': [
       { edition: 'Premium Edition', price: 374.99, discount: 25, currency: 'R$', exchangeTax: 4.99 },
       { edition: 'Standard Edition', price: 262.42, discount: 25, currency: 'R$', exchangeTax: 4.99 },
@@ -11,30 +13,23 @@ export const doomTheDarkAges = {
       { edition: 'Standard Edition', price: 349.90, currency: 'R$', exchangeTax: 4.99 },
     ],
   },
-
-  UnitedStates: {
-    'July 2025': [
-      { edition: 'Premium Edition', price: 74.99, discount: 25, currency: '$' },
-      { edition: 'Standard Edition', price: 52.49, discount: 25, currency: '$' },
-    ],
-    'May 2025': [
-      { edition: 'Premium Edition', price: 99.99, currency: '$' },
-      { edition: 'Standard Edition', price: 69.99, currency: '$' },
-    ],
-  },
-
   Europe: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 53.59, discount: 33, currency: '€' },
+    ],
     'July 2025': [
       { edition: 'Deluxe Edition', price: 82.49, discount: 25, currency: '€' },
       { edition: 'Standard Edition', price: 59.99, discount: 25, currency: '€' },
     ],
     'May 2025': [
       { edition: 'Deluxe Edition', price: 109.99, currency: '€' },
-      { edition: 'Standard Edition', price: 59.99, currency: '€' },
+      { edition: 'Standard Edition', price: 79.99, currency: '€' },
     ],
   },
-
   UnitedKingdom: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 46.89, discount: 33, currency: '£' },
+    ],
     'July 2025': [
       { edition: 'Premium Edition', price: 74.99, discount: 25, currency: '£' },
       { edition: 'Standard Edition', price: 52.49, discount: 25, currency: '£' },
@@ -42,6 +37,19 @@ export const doomTheDarkAges = {
     'May 2025': [
       { edition: 'Premium Edition', price: 99.99, currency: '£' },
       { edition: 'Standard Edition', price: 69.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { edition: 'Standard Edition', price: 46.89, discount: 33, currency: '$' },
+    ],
+    'July 2025': [
+      { edition: 'Premium Edition', price: 74.99, discount: 25, currency: '$' },
+      { edition: 'Standard Edition', price: 52.49, discount: 25, currency: '$' },
+    ],
+    'May 2025': [
+      { edition: 'Premium Edition', price: 99.99, currency: '$' },
+      { edition: 'Standard Edition', price: 69.99, currency: '$' },
     ],
   },
 };
