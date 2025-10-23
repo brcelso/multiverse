08f9d30b98d658874bd12fc2ccdfbfa64c19da80
celso <celso.sjunior@outlook.com>
2025-10-22 21:10:17 -0300

    Atualizar o Destiny2.ts

--- 2k38/cycle/src/data/games/Destiny2.ts
+++ 2k38/cycle/src/data/games/Destiny2.ts
@@ -2,36 +2,51 @@ export const destiny2 = {
   launchDate: '2025-07-15',
   Brazil: {
     'October 2025': [
-      { edition: 'Year of Prophecy', price: 267.99, discount: 33, currency: 'R$', exchangeTax: 5 },
+      { edition: 'Year of Prophecy Ultimate Edition', price: 267.99, discount: 33, currency: 'R$', exchangeTax: 4 },
+      { edition: 'Year of Prophecy', price: 223.99, discount: 30, currency: 'R$', exchangeTax: 4 },
     ],
     'September 2025': [
-      { edition: 'Year of Prophecy', price: 299.99, discount: 25, currency: 'R$', exchangeTax: 5 },
+      { edition: 'Year of Prophecy', price: 299.99, discount: 25, currency: 'R$', exchangeTax: 4 },
     ],
     'July 2025': [
-      { edition: 'Year of Prophecy', price: 399.99, currency: 'R$', exchangeTax: 5 },
+      { edition: 'Year of Prophecy Ultimate Edition', price: 399.99, currency: 'R$', exchangeTax: 4 },
+      { edition: 'Year of Prophecy', price: 319.99, currency: 'R$', exchangeTax: 4 },
     ],
   },
   Europe: {
+    'October 2025': [
+      { edition: 'Year Of Prophecy Ultimate Edition', price: 66.99, discount: 33, currency: '€' },
+    ],
     'September 2025': [
       { edition: 'Year Of Prophecy', price: 59.99, discount: 25, currency: '€' },
     ],
     'July 2025': [
+      { edition: 'Year Of Prophecy Ultimate Edition', price: 99.99, currency: '€' },
       { edition: 'Year Of Prophecy', price: 79.99, currency: '€' },
     ],
   },
   UnitedKingdom: {
+    'October 2025': [
+      { edition: 'Year Of Prophecy Ultimate Edition', price: 56.94, discount: 33, currency: '£' },
+    ],
     'September 2025': [
       { edition: 'Year Of Prophecy', price: 59.99, discount: 25, currency: '£' },
     ],
     'July 2025': [
-      { edition: 'Year Of Prophecy', price: 79.99, currency: '£' },
+      { edition: 'Year Of Prophecy Ultimate Edition', price: 84.99, currency: '£' },
+      { edition: 'Year Of Prophecy', price: 69.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'September 2025': [
+      { edition: 'Year Of Prophecy Ultimate Edition', price: 66.99, discount: 33, currency: '$' },
+      { edition: 'Year Of Prophecy', price: 55.99, discount: 30, currency: '$' },
+    ],
+    'September 2025': [
       { edition: 'Year Of Prophecy', price: 59.99, discount: 25, currency: '$' },
     ],
     'July 2025': [
+      { edition: 'Year Of Prophecy Ultimate Edition', price: 99.99, currency: '$' },
       { edition: 'Year Of Prophecy', price: 79.99, currency: '$' },
     ],
   },
