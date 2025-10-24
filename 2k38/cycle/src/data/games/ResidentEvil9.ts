abfb5b84f74113efe8a8b9f89d9b0df9211acb78
celso <celso.sjunior@outlook.com>
2025-10-23 23:03:44 -0300

    Atualizar o ResidentEvil9.ts

--- 2k38/cycle/src/data/games/ResidentEvil9.ts
+++ 2k38/cycle/src/data/games/ResidentEvil9.ts
@@ -1,38 +1,40 @@
 export const residentevil9 = {
   launchDate: '2026-02-27',
-
-  Japan: {
-    'June 2025': [
-      { edition: "Deluxe Edition", price: 9980, currency: '¥' },
-      { edition: "Standard Edition", price: 8980, currency: '¥' },
-    ],
-  },
-
-  Brazil: {
-    'June 2025': [
-      { edition: "Deluxe Edition", price: 665.90, currency: 'R$', exchangeTax: 6.66 },
-      { edition: "Standard Edition", price: 565.90, currency: 'R$', exchangeTax: 6.66 },
-    ],
-  },
-
-  UnitedStates: {
-    'June 2025': [
+  Argentina: {
+    'October 2025': [
       { edition: "Deluxe Edition", price: 99.99, currency: '$' },
       { edition: "Standard Edition", price: 84.99, currency: '$' },
     ],
   },
-
+  Brazil: {
+    'October 2025': [
+      { edition: "Deluxe Edition", price: 665.90, currency: 'R$', exchangeTax: 6.66 },
+      { edition: "Standard Edition", price: 565.90, currency: 'R$', exchangeTax: 6.66 },
+      { platform: "Switch", edition: "Standard Edition", price: 339.00, currency: 'R$', exchangeTax: 6.66 },
+    ],
+  },
   Europe: {
-    'June 2025': [
+    'October 2025': [
       { edition: "Deluxe Edition", price: 89.99, currency: '€' },
       { edition: "Standard Edition", price: 79.99, currency: '€' },
     ],
   },
-
+  Japan: {
+    'October 2025': [
+      { edition: "Deluxe Edition", price: 9980, currency: '¥' },
+      { edition: "Standard Edition", price: 8980, currency: '¥' },
+    ],
+  },
   UnitedKingdom: {
-    'June 2025': [
+    'October 2025': [
       { edition: "Deluxe Edition", price: 79.99, currency: '£' },
       { edition: "Standard Edition", price: 69.99, currency: '£' },
+    ],
+  },
+  UnitedStates: {
+    'October 2025': [
+      { edition: "Deluxe Edition", price: 99.99, currency: '$' },
+      { edition: "Standard Edition", price: 84.99, currency: '$' },
     ],
   },
 };
