5a50058b3a5007686f60e8ba5a99cd0d9633faf6
celso <celso.sjunior@outlook.com>
2025-11-04 08:28:06 -0300

    Update TombRaiderRemastered.ts

--- 2k38/cycle/src/data/games/TombRaiderRemastered.ts
+++ 2k38/cycle/src/data/games/TombRaiderRemastered.ts
@@ -1,6 +1,9 @@
 export const tombRaiderRemastered = {
   launchDate: '2024-02-14',
   Brazil: {
+    'November 2025': [
+      { platform: "Steam", edition: 'Remastered', price: 35.59, discount: 60, currency: 'R$', exchangeTax2: 2.96 }
+    ],
     'October 2025': [
       { edition: 'Remastered Bundle', price: 117.96, currency: 'R$', discount: 60, exchangeTax: 5.36 },
       { edition: 'Remastered Bundle', price: 147.45, currency: 'R$', discount: 50, exchangeTax: 5.36 },
@@ -13,6 +16,7 @@ export const tombRaiderRemastered = {
     'February 2024': [
       { edition: 'Remastered', price: 143.91, currency: 'R$', discount: 10, exchangeTax: 5.33 },
       { edition: 'Remastered', price: 159.99, currency: 'R$', exchangeTax: 5.33 },
+      { platform: "Steam", edition: 'Remastered', price: 88.99, currency: 'R$', exchangeTax2: 2.96 }
     ],
   },
   Europe: {
@@ -46,6 +50,9 @@ export const tombRaiderRemastered = {
     ],
   },  
   UnitedStates: {
+  'November 2025': [
+      { platform: "Steam", edition: 'Remastered', price: 11.99, discount: 60, currency: '$' }
+    ],
     'October 2025': [
       { edition: 'Remastered Bundle', price: 21.99, currency: '$', discount: 60 },
       { edition: 'Remastered Bundle', price: 27.49, currency: '$', discount: 50 },
@@ -58,6 +65,8 @@ export const tombRaiderRemastered = {
     'February 2024': [
       { edition: 'Remastered', price: 26.99, currency: '$', discount: 10 },
       { edition: 'Remastered', price: 29.99, currency: '$' },
+
+
     ],
   },
 };
