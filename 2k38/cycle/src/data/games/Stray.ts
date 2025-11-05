25dfc69feb21e1a766c1e287f76f3c7cefd4822d
celso <celso.sjunior@outlook.com>
2025-11-04 21:38:27 -0300

    Update Stray.ts

--- 2k38/cycle/src/data/games/Stray.ts
+++ 2k38/cycle/src/data/games/Stray.ts
@@ -3,9 +3,17 @@ export const stray = {
   Brazil: {
     'October 2025': [
       { edition: 'Standard Edition', price: 89.70, discount: 40, currency: 'R$', exchangeTax: 5 },
+      { edition: 'Standard Edition', price: 47.99, discount: 40, currency: 'R$', exchangeTax: 2.66 },
+    ],
+    'January 2024': [
+      { edition: 'Standard Edition', price: 79.99, currency: 'R$', exchangeTax: 2.66 },
+    ],
+    'November 2023': [
+      { edition: 'Standard Edition', price: 46.19, currency: 'R$', discount: 34, exchangeTax: 2.33 },
     ],
     'July 2022': [
       { edition: 'Standard Edition', price: 149.50, currency: 'R$', exchangeTax: 5 },
+      { edition: 'Standard Edition', price: 69.99, currency: 'R$', exchangeTax: 2.33 },
     ],
   },
   Europe: {
@@ -27,6 +35,9 @@ export const stray = {
   UnitedStates: {
     'October 2025': [
       { edition: 'Standard Edition', price: 17.99, discount: 40, currency: '$' },
+    ],
+    'November 2023': [
+      { edition: 'Standard Edition', price: 19.79, discount: 34, currency: '$' },
     ],
     'July 2022': [
       { edition: 'Standard Edition', price: 29.99, currency: '$' },
