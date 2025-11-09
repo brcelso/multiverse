6ebfe0b6431aefbcc8ddcf0d59058081bb289ba5
celso <celso.sjunior@outlook.com>
2025-11-09 15:39:33 -0300

    Update UnchartedLegacy.ts

--- 2k38/cycle/src/data/games/UnchartedLegacy.ts
+++ 2k38/cycle/src/data/games/UnchartedLegacy.ts
@@ -4,34 +4,42 @@ export const unchartedlegacy = {
     'October 2025': [
       { platform: "Steam", edition: 'Legacy Of Thieves Collection', price: 79.96, currency: 'R$', discount: 60, exchangeTax2: 3.99 },
       { platform: "Playstation", edition: 'Legacy Of Thieves Collection', price: 113.96, currency: 'R$', discount: 60, exchangeTax2: 5.69 }
+      { platform: "Playstation", edition: 'Nathan Drake Collection', price: 23.97, currency: 'R$', discount: 70, exchangeTax2: 4 }
     ],
     'October 2022': [
       { platform: "Steam", edition: 'Legacy Of Thieves Collection', price: 199.90, currency: 'R$', exchangeTax2: 3.99 },
       { platform: "Playstation", edition: 'Legacy Of Thieves Collection', price: 284.90, currency: 'R$', exchangeTax2: 5.69 }
+      { platform: "Playstation", edition: 'Nathan Drake Collection', price: 79.90, currency: 'R$', exchangeTax2: 4 }
     ],
   },
   Europe: {
     'October 2025': [
       { edition: 'Legacy Of Thieves Collection', price: 19.99, currency: '€', discount: 60 }
+      { edition: 'Nathan Drake Collection', price: 5.99, discount: 70, currency: '€', },
     ],
     'October 2022': [
       { edition: 'Legacy Of Thieves Collection', price: 49.99, currency: '€' },
+      { edition: 'Nathan Drake Collection', price: 19.99, currency: '€', },
     ],
   },
   UnitedKingdom: {
     'October 2025': [
       { edition: 'Legacy Of Thieves Collection', price: 17.99, currency: '£', discount: 60 },
+      { edition: 'Nathan Drake Collection', price: 4.79, discount: 70, currency: '£' },
     ],
     'October 2022': [
       { edition: 'Legacy Of Thieves Collection', price: 44.99, currency: '£' },
+      { edition: 'Nathan Drake Collection', price: 15.99, currency: '£' },
     ],
   },
   UnitedStates: {
     'October 2025': [
       { edition: 'Legacy Of Thieves Collection', price: 19.99, currency: '$', discount: 60 },
+      { edition: 'Nathan Drake Collection', price: 5.99, discount: 70, currency: '$' },
     ],
     'October 2022': [
       { edition: 'Legacy Of Thieves Collection', price: 49.99, currency: '$' },
+      { edition: 'Nathan Drake Collection', price: 19.99, currency: '$' },
     ],
   },
 };
