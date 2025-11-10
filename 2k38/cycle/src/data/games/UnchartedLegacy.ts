80afd423f16f180ab6615e294753f613baecc9f2
celso <celso.sjunior@outlook.com>
2025-11-09 23:49:59 -0300

    Update UnchartedLegacy.ts

--- 2k38/cycle/src/data/games/UnchartedLegacy.ts
+++ 2k38/cycle/src/data/games/UnchartedLegacy.ts
@@ -3,12 +3,12 @@ export const unchartedlegacy = {
   Brazil: {
     'October 2025': [
       { platform: "Steam", edition: 'Legacy Of Thieves Collection', price: 79.96, currency: 'R$', discount: 60, exchangeTax2: 3.99 },
-      { platform: "Playstation", edition: 'Legacy Of Thieves Collection', price: 113.96, currency: 'R$', discount: 60, exchangeTax2: 5.69 }
+      { platform: "Playstation", edition: 'Legacy Of Thieves Collection', price: 113.96, currency: 'R$', discount: 60, exchangeTax2: 5.69 },
       { platform: "Playstation", edition: 'Nathan Drake Collection', price: 23.97, currency: 'R$', discount: 70, exchangeTax2: 4 }
     ],
     'October 2022': [
       { platform: "Steam", edition: 'Legacy Of Thieves Collection', price: 199.90, currency: 'R$', exchangeTax2: 3.99 },
-      { platform: "Playstation", edition: 'Legacy Of Thieves Collection', price: 284.90, currency: 'R$', exchangeTax2: 5.69 }
+      { platform: "Playstation", edition: 'Legacy Of Thieves Collection', price: 284.90, currency: 'R$', exchangeTax2: 5.69 },
       { platform: "Playstation", edition: 'Nathan Drake Collection', price: 79.90, currency: 'R$', exchangeTax2: 4 }
     ],
   },
